66d03abd1fdcf1178a2a973e0aee05f963cfb6ac
Carterpersall <carterpersall@gmail.com>
2022-05-19 21:15:05 -0500

    Actually add \b
    Forgot to save before previous commit  : |

--- winutil-test.ps1
+++ winutil-test.ps1
@@ -28,18 +28,18 @@
     }
 
     If($env:args){Write-Host "Arguments Detected, Running Args";
-        If($env:args -match "InstallUpgrade"){Invoke-Runspace $InstallUpgrade}
-        If($env:args -match "UndoTweaks"){Invoke-Runspace $undotweaks}
-        If($env:args -match "PanelControl"){cmd /c control}
-        If($env:args -match "PanelNetwork"){cmd /c ncpa.cpl}
-        If($env:args -match "PanelPower"){cmd /c powercfg.cpl}
-        If($env:args -match "PanelSound"){cmd /c mmsys.cpl}
-        If($env:args -match "PanelSystem"){cmd /c sysdm.cpl}
-        If($env:args -match "PanelUser"){cmd /c "control userpasswords2"}
-        If($env:args -match "DefaultUpdates"){Invoke-Runspace $Updatesdefault}
-        If($env:args -match "DisableUpdates"){Invoke-Runspace $Updatesdisable}
-        If($env:args -match "EnableSecurity"){Invoke-Runspace $Updatessecurity}
-        If($env:args -match "QuitAfter"){Break}
+        If($env:args -match '\bInstallUpgrade\b'){Invoke-Runspace $InstallUpgrade}
+        If($env:args -match '\bUndoTweaks\b'){Invoke-Runspace $undotweaks}
+        If($env:args -match '\bPanelControl\b'){cmd /c control}
+        If($env:args -match '\bPanelNetwork\b'){cmd /c ncpa.cpl}
+        If($env:args -match '\bPanelPower\b'){cmd /c powercfg.cpl}
+        If($env:args -match '\bPanelSound\b'){cmd /c mmsys.cpl}
+        If($env:args -match '\bPanelSystem\b'){cmd /c sysdm.cpl}
+        If($env:args -match '\bPanelUser\b'){cmd /c "control userpasswords2"}
+        If($env:args -match '\bDefaultUpdates\b'){Invoke-Runspace $Updatesdefault}
+        If($env:args -match '\bDisableUpdates\b'){Invoke-Runspace $Updatesdisable}
+        If($env:args -match '\bEnableSecurity\b'){Invoke-Runspace $Updatessecurity}
+        If($env:args -match '\bQuitAfter\b'){Break}
     }
 
     $inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window'
