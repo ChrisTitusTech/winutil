5684be8a33e36bb58e69158991cc747c01347c89
PWall <34860495+pwall2222@users.noreply.github.com>
2022-06-04 13:42:41 +0200

    Added  -NoRestart to WindowsOptionalFeature

--- winutil.ps1
+++ winutil.ps1
@@ -1150,36 +1150,36 @@ $WPFundoall.Add_Click({
 $WPFFeatureInstall.Add_Click({
 
         If ( $WPFFeaturesdotnet.IsChecked -eq $true ) {
-            Enable-WindowsOptionalFeature -Online -FeatureName "NetFx4-AdvSrvs" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" -All
+            Enable-WindowsOptionalFeature -Online -FeatureName "NetFx4-AdvSrvs" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" -All -NoRestart
         }
         If ( $WPFFeatureshyperv.IsChecked -eq $true ) {
-            Enable-WindowsOptionalFeature -Online -FeatureName "HypervisorPlatform" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-All" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Tools-All" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Management-PowerShell" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Hypervisor" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Services" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Management-Clients" -All
+            Enable-WindowsOptionalFeature -Online -FeatureName "HypervisorPlatform" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-All" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Tools-All" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Management-PowerShell" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Hypervisor" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Services" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Management-Clients" -All -NoRestart
             cmd /c bcdedit /set hypervisorschedulertype classic
             Write-Host "HyperV is now installed and configured. Please Reboot before using."
         } 
         If ( $WPFFeatureslegacymedia.IsChecked -eq $true ) {
-            Enable-WindowsOptionalFeature -Online -FeatureName "WindowsMediaPlayer" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "MediaPlayback" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "DirectPlay" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "LegacyComponents" -All
+            Enable-WindowsOptionalFeature -Online -FeatureName "WindowsMediaPlayer" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "MediaPlayback" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "DirectPlay" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "LegacyComponents" -All -NoRestart
         }
         If ( $WPFFeaturewsl.IsChecked -eq $true ) {
-            Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" -All
+            Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" -All -NoRestart
             Write-Host "WSL is now installed and configured. Please Reboot before using."
         }
         If ( $WPFFeaturenfs.IsChecked -eq $true ) {
-            Enable-WindowsOptionalFeature -Online -FeatureName "ServicesForNFS-ClientOnly" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "ClientForNFS-Infrastructure" -All
-            Enable-WindowsOptionalFeature -Online -FeatureName "NFS-Administration" -All
+            Enable-WindowsOptionalFeature -Online -FeatureName "ServicesForNFS-ClientOnly" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "ClientForNFS-Infrastructure" -All -NoRestart
+            Enable-WindowsOptionalFeature -Online -FeatureName "NFS-Administration" -All -NoRestart
             nfsadmin client stop
             Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\ClientForNFS\CurrentVersion\Default" -Name "AnonymousUID" -Type DWord -Value 0
             Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\ClientForNFS\CurrentVersion\Default" -Name "AnonymousGID" -Type DWord -Value 0
