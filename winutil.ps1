ffdca7373968a1fe8434f48e7a01f7ff35a448f3
Sarah <justsweetluna@gmail.com>
2022-09-25 21:35:19 +0200

    added check for checking if path exists for TPM

--- winutil.ps1
+++ winutil.ps1
@@ -673,6 +673,9 @@ $WPFtweaksbutton.Add_Click({
         }
         If ( $WPFMiscTweaksDisableTPMCheck.IsChecked -eq $true ) {
             Write-Host "Disabling TPM Check..."
+                If (!(Test-Path "HKLM:\SYSTEM\Setup\MoSetup")) {
+                New-Item -Path "HKLM:\SYSTEM\Setup\MoSetup" -Force | Out-Null
+            }
             Set-ItemProperty -Path "HKLM:\SYSTEM\Setup\MoSetup" -Name "AllowUpgradesWithUnsupportedTPM" -Type DWord -Value 1
             $WPFMiscTweaksDisableTPMCheck.IsChecked = $false
         }
