f9d538c1031ef78db753d22973e44682c23bdba8
padsalatushal <padsalatushal@gmail.com>
2022-09-30 00:15:46 +0530

    Added Internet Download Manager for Install

--- winutil.ps1
+++ winutil.ps1
@@ -236,6 +236,10 @@ $WPFinstall.Add_Click({
             $wingetinstall.Add("Microsoft.WindowsTerminal")
             $WPFInstallterminal.IsChecked = $false
         }
+        If ( $WPFInstallidm.IsChecked -eq $true ) { 
+            $wingetinstall.Add("Tonec.InternetDownloadManager")
+            $WPFInstallidm.IsChecked = $false
+        }
         If ( $WPFInstallalacritty.IsChecked -eq $true ) { 
             $wingetinstall.Add("Alacritty.Alacritty")
             $WPFInstallalacritty.IsChecked = $false
