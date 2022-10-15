76c38b5a028eb0970a8b8ce6302523074b20a328
DeveloperDurp <developerdurp@durp.info>
2022-10-15 11:16:00 -0500

    moved all lagacy scripts to json

--- winutil-test.ps1
+++ winutil-test.ps1
@@ -695,78 +695,12 @@ $xaml.SelectNodes("//*[@Name]") | ForEach-Object {$sync["$("$($psitem.Name)")"] 
 
             $ScriptsToRun | ForEach-Object {
                 $Scriptblock = [scriptblock]::Create($psitem)
+                Invoke-Command -ScriptBlock $Scriptblock
             }
     
             Write-Logs -Level INFO -Message "Finished Scripts" -LogPath $sync.logfile
         }
 
-        #Legacy tweaks not converted to json
-        #old code that didn't work inside json file cleanly. Will investigate ways to get around this
-
-        Foreach($tweak in $tweakstorun){
-
-            #if ($tweak -eq "EssTweaksOO"){
-            #    Import-Module BitsTransfer
-            #    Start-BitsTransfer -Source "https://raw.githubusercontent.com/ChrisTitusTech/win10script/master/ooshutup10.cfg" -Destination ooshutup10.cfg
-            #    Start-BitsTransfer -Source "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -Destination OOSU10.exe
-            #    ./OOSU10.exe ooshutup10.cfg /quiet
-            #}        
-            if ($tweak -eq "EssTweaksRP"){
-                Enable-ComputerRestore -Drive "C:\"
-                Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
-            }        
-            if ($tweak -eq "EssTweaksStorage"){
-                Remove-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy" -Recurse -ErrorAction SilentlyContinue
-            }
-            if ($tweak -eq "EssTweaksTele"){
-
-                Write-Host "Enabling F8 boot menu options..."
-                bcdedit /set `{current`} bootmenupolicy Legacy | Out-Null
-
-                # Task Manager Details
-                If ((get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name CurrentBuild).CurrentBuild -lt 22557) {
-                    Write-Host "Showing task manager details..."
-                    $taskmgr = Start-Process -WindowStyle Hidden -FilePath taskmgr.exe -PassThru
-                    Do {
-                        Start-Sleep -Milliseconds 100
-                        $preferences = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\TaskManager" -Name "Preferences" -ErrorAction SilentlyContinue
-                    } Until ($preferences)
-                    Stop-Process $taskmgr
-                    $preferences.Preferences[28] = 0
-                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\TaskManager" -Name "Preferences" -Type Binary -Value $preferences.Preferences
-                } else {Write-Host "Task Manager patch not run in builds 22557+ due to bug"}
-
-                Write-Host "Hiding 3D Objects icon from This PC..."
-                Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}" -Recurse -ErrorAction SilentlyContinue  
-                
-                # Group svchost.exe processes
-                $ram = (Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1kb
-                Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name "SvcHostSplitThresholdInKB" -Type DWord -Value $ram -Force                
-
-                Write-Host "Removing AutoLogger file and restricting directory..."
-                $autoLoggerDir = "$env:PROGRAMDATA\Microsoft\Diagnosis\ETLLogs\AutoLogger"
-                If (Test-Path "$autoLoggerDir\AutoLogger-Diagtrack-Listener.etl") {
-                    Remove-Item "$autoLoggerDir\AutoLogger-Diagtrack-Listener.etl"
-                }
-                icacls $autoLoggerDir /deny SYSTEM:`(OI`)`(CI`)F | Out-Null
-            }
-            if ($tweak -eq "MiscTweaksLapNum"){
-                If (!(Test-Path "HKU:")) {
-                    New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS | Out-Null
-                }
-                Set-ItemProperty -Path "HKU:\.DEFAULT\Control Panel\Keyboard" -Name "InitialKeyboardIndicators" -Type DWord -Value 0
-            }
-            if ($tweak -eq "MiscTweaksNum"){
-                If (!(Test-Path "HKU:")) {
-                    New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS | Out-Null
-                }
-                Set-ItemProperty -Path "HKU:\.DEFAULT\Control Panel\Keyboard" -Name "InitialKeyboardIndicators" -Type DWord -Value 2
-            }
-            if ($tweak -eq "MiscTweaksDisplay"){
-                Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "UserPreferencesMask" -Type Binary -Value ([byte[]](144,18,3,128,16,0,0,0))
-            }
-        }
-        
         Write-Logs -Level INFO -Message "Tweaks finished" -LogPath $sync.logfile
         
         if($sync){
