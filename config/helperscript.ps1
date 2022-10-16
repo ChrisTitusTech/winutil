2d878c5fdef15b747a2c6364998915eea60cb4b9
DeveloperDurp <developerdurp@durp.info>
2022-10-16 08:33:37 -0500

    added tweaks to helper file

--- config/helperscript.ps1
+++ config/helperscript.ps1
@@ -143,3 +143,177 @@ $jsonfile = Get-Content ./config/preset.json | ConvertFrom-Json
 Add-Member -InputObject $jsonfile -MemberType NoteProperty -Name $NameofButton -Value $commands
 
 $jsonfile | ConvertTo-Json | Out-File ./config/preset.json
+
+<#
+    tweaks.json
+    -----------------
+    This file holds all the tweaks needed to make modifications to windows. This file is the most complicated so modify with care.
+
+    The structure of the json is as follows
+
+{
+    "Name of button": {
+        "registry" : [
+            {
+                "Path":  "Path in registry",
+                "Name":  "Name of Registry key",
+                "Type": "Item type",
+                "Value":  "Value to modify", 
+                "OriginalValue": "value to reset"
+            }
+        ],
+        "service" : [
+            {
+                "Name":  "Name of service",
+                "StartupType":  "Startup type to set", 
+                "OriginalType": "Startup type to reset"
+            }
+        ],
+        "ScheduledTask" : [
+            {
+                "Name":  "Path to scheduled task",
+                "State":  "State to set", 
+                "OriginalState": "State to reset"
+            }
+        ],
+        "appx": [
+            List of appx,
+            files to uninstall
+        ],
+        "InvokeScript": [
+            "Script to make modifications not possible with the above types
+            Special care needs to be taken here as converting from json to a scriptblock
+            can cause weird issues. Please look at the example below to get an idea of how things should work"                 
+        ],
+        "UndoScript": [
+            "Same as above however is meant to undo what you did above"                 
+        ]
+    }
+}
+
+Example:
+
+{
+    EssTweaksAH": {
+        "registry" : [
+            {
+                "Path":  "HKLM:\\SOFTWARE\\Policies\\Microsoft\\Windows\\System",
+                "Name":  "EnableActivityFeed",
+                "Type": "DWord",
+                "Value":  "0", 
+                "OriginalValue": "1"
+            },
+            {
+                "Path":  "HKLM:\\SOFTWARE\\Policies\\Microsoft\\Windows\\System",
+                "Name":  "PublishUserActivities",
+                "Type": "DWord",
+                "Value":  "0", 
+                "OriginalValue": "1"
+            }
+        ]
+    },
+    "EssTweaksHome": {
+        "service" : [
+            {
+                "Name":  "HomeGroupListener",
+                "StartupType":  "Manual", 
+                "OriginalType": "Automatic"
+            },
+            {
+                "Name":  "HomeGroupProvider",
+                "StartupType":  "Manual", 
+                "OriginalType": "Automatic"
+            }
+        ]
+    },
+    "EssTweaksTele": {
+        "ScheduledTask" : [
+            {
+                "Name":  "Microsoft\\Windows\\Application Experience\\Microsoft Compatibility Appraiser",
+                "State":  "Disabled", 
+                "OriginalState": "Enabled"
+            },                    
+            {
+                "Name":  "Microsoft\\Windows\\Application Experience\\ProgramDataUpdater",
+                "State":  "Disabled", 
+                "OriginalState": "Enabled"
+            }
+        ]
+    },
+    "EssTweaksDeBloat": {
+        "appx": [
+            "Microsoft.Microsoft3DViewer",
+            "Microsoft.AppConnector"
+        ]
+    },
+    "EssTweaksOO": {
+        "InvokeScript": [
+            "Import-Module BitsTransfer
+            Start-BitsTransfer -Source \"https://raw.githubusercontent.com/ChrisTitusTech/win10script/master/ooshutup10.cfg\" -Destination C:\\Windows\\Temp\\ooshutup10.cfg
+            Start-BitsTransfer -Source \"https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe\" -Destination C:\\Windows\\Temp\\OOSU10.exe
+            C:\\Windows\\Temp\\OOSU10.exe C:\\Windows\\Temp\\ooshutup10.cfg /quiet"                 
+        ]
+    }
+}
+
+#>
+
+#Modify the variables and run his code. It will import the current file and add your addition. From there you can create a pull request.
+#Make sure to uncomment the sections you which to add.
+
+$NameofButton = "EssTweaksTele1"
+
+#$Registry = @(
+#    #to add more repeat this seperated by a comma
+#    @{
+#        Path = ""
+#        Name = ""
+#        Type = ""
+#        Value = ""
+#        OriginalValue = ""
+#    }
+#)
+
+#$Service = @(
+#    #to add more repeat this seperated by a comma
+#    @{
+#        Name = ""
+#        StartupType = ""
+#        OriginalType = ""
+#    }
+#)
+
+#$ScheduledTask = @(
+#    #to add more repeat this seperated by a comma
+#    @{
+#        Name = ""
+#        State = ""
+#        OriginalState = ""
+#    }
+#)
+
+#$Appx = @(
+#    ""
+#)
+
+#$InvokeScript = @(
+#    "" 
+#)
+
+#$UndoScript = @(
+#    "" 
+#)
+
+$ButtonToAdd = New-Object psobject
+$jsonfile = Get-Content ./config/tweaks.json | ConvertFrom-Json
+
+if($Registry){Add-Member -InputObject $ButtonToAdd -MemberType NoteProperty -Name "registry" -Value $Registry}
+if($Service){Add-Member -InputObject $ButtonToAdd -MemberType NoteProperty -Name "service" -Value $Service}
+if($ScheduledTask){Add-Member -InputObject $ButtonToAdd -MemberType NoteProperty -Name "ScheduledTask" -Value $ScheduledTask}
+if($Appx){Add-Member -InputObject $ButtonToAdd -MemberType NoteProperty -Name "Appx" -Value $Appx}
+if($InvokeScript){Add-Member -InputObject $ButtonToAdd -MemberType NoteProperty -Name "InvokeScript" -Value $InvokeScript}
+if($UndoScript){Add-Member -InputObject $ButtonToAdd -MemberType NoteProperty -Name "UndoScript" -Value $UndoScript}
+
+Add-Member -InputObject $jsonfile -MemberType NoteProperty -Name $NameofButton -Value $ButtonToAdd
+
+($jsonfile | ConvertTo-Json -Depth 5).replace('\r\n',"`r`n") | Out-File ./config/tweaks.json
