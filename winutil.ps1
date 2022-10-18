6da3535c5cbd1b4217bea0fd910e2d6d825f03db
Chris Titus <dfm.titus@gmail.com>
2022-10-18 16:39:14 -0500

    syntax fix

--- winutil.ps1
+++ winutil.ps1
@@ -584,7 +584,7 @@ Version 0.1
             Write-Logs -Level INFO -Message "Starting ScheduledTask Modification" -LogPath $sync.logfile
 
             $ScheduledTaskToModify | ForEach-Object {
-							if ($(Get-ScheduledTask | where{$_.TaskName -eq "$($psitem.name)"))
+							if ($(Get-ScheduledTask | where{$_.TaskName -eq "$($psitem.name)"}))
                 Try{
                     if($($psitem.State) -eq "Disabled"){
                         Disable-ScheduledTask -TaskName "$($psitem.name)" -ErrorAction Stop | Out-Null
