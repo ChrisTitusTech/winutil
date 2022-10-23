679039f368a041d94c10efdc40960db1cb8ccf4b
Chris Titus <dfm.titus@gmail.com>
2022-10-23 00:01:38 -0500

    Switch to PSGallery winget-installer

--- winutil.ps1
+++ winutil.ps1
@@ -545,33 +545,25 @@ $WPFinstall.Add_Click({
             Write-Host "Winget Already Installed"
         }
         else {
-            if (((((Get-ComputerInfo).OSName.IndexOf("LTSC")) -ne -1) -or ((Get-ComputerInfo).OSName.IndexOf("Server") -ne -1)) -and (((Get-ComputerInfo).WindowsVersion) -ge "1809")) {
-                #Checks if Windows edition is LTSC/Server 2019+
-                #Manually Installing Winget
+            #Gets the computer's information
+            $ComputerInfo = Get-ComputerInfo
+
+            #Gets the Windows Edition
+            $OSName = if ($ComputerInfo.OSName) {
+                $ComputerInfo.OSName
+            }else {
+                $ComputerInfo.WindowsProductName
+            }
+
+            if (((($OSName.IndexOf("LTSC")) -ne -1) -or ($OSName.IndexOf("Server") -ne -1)) -and (($ComputerInfo.WindowsVersion) -ge "1809")) {
+                
                 Write-Host "Running Alternative Installer for LTSC/Server Editions"
 
-                #Download Needed Files
-                Write-Host "Downloading Needed Files..."
-                Start-BitsTransfer -Source "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -Destination "$env:TEMP\Microsoft.VCLibs.x64.14.00.Desktop.appx"
-                Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Destination "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-                Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/b0a0692da1034339b76dce1c298a1e42_License1.xml" -Destination "$env:TEMP\b0a0692da1034339b76dce1c298a1e42_License1.xml"
-
-                #Installing Packages
-                Write-Host "Installing Packages..."
-                Add-AppxProvisionedPackage -Online -PackagePath "$env:TEMP\Microsoft.VCLibs.x64.14.00.Desktop.appx" -SkipLicense
-                Add-AppxProvisionedPackage -Online -PackagePath "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -LicensePath "$env:TEMP\b0a0692da1034339b76dce1c298a1e42_License1.xml"
-                Write-Host "winget Installed (Reboot might be required before winget will work)"
-
-                #Sleep for 5 seconds to maximize chance that winget will work without reboot
-                Write-Host "Pausing for 5 seconds to maximize chance that winget will work without reboot"
-                Start-Sleep -s 5
-
-                #Removing no longer needed Files
-                Write-Host "Removing no longer needed Files..."
-                Remove-Item -Path "$env:TEMP\Microsoft.VCLibs.x64.14.00.Desktop.appx" -Force
-                Remove-Item -Path "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force
-                Remove-Item -Path "$env:TEMP\b0a0692da1034339b76dce1c298a1e42_License1.xml" -Force
-                Write-Host "Removed Files that are no longer needed"
+                # Switching to winget-install from PSGallery from asheroto
+                # Source: https://github.com/asheroto/winget-installer
+                
+                Start-Process powershell.exe -Verb RunAs -ArgumentList "-command irm https://raw.githubusercontent.com/asheroto/winget-installer/master/winget-install.ps1 | iex | Out-Host" -WindowStyle Normal
+                
             }
             elseif (((Get-ComputerInfo).WindowsVersion) -lt "1809") {
                 #Checks if Windows Version is too old for winget
