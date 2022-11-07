2ba0d22b435578c0668c2d8273cef1c581d7250f
Gökhan Türk <ensar.gokhan@hotmail.com>
2022-11-07 10:36:41 +0300

    Silent install command for Nuget Provider
    I added silent install command for Nuget Provider. Now winget installation is completely silent.

--- winget.ps1
+++ winget.ps1
@@ -1,3 +1,4 @@
+Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-Script -Name winget-install -Force
 winget-install.ps1
