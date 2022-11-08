86da6ed2f397ee3155b0ce671a0a04a72ad929d2
lavavex <27239435+lavavex@users.noreply.github.com>
2022-11-08 16:43:02 -0600

    fix typo

--- winutil.ps1
+++ winutil.ps1
@@ -1055,6 +1055,8 @@ $WPFAddUltPerf.Add_Click({
         powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
         powercfg /setactive e9a42b02-d5df-448d-aa00-03f14749eb61
         Write-Host "Profile added and enabled"
+     }
+)
     
 $WPFEnableDarkMode.Add_Click({
         Write-Host "Enabling Dark Mode"
