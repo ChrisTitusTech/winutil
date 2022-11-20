7e8db7a72a8f04452a037ab7c23043c25536a8f2
DeveloperDurp <developerdurp@durp.info>
2022-11-20 10:53:25 -0600

    test pipeline

--- pester/winutil.Tests.ps1
+++ pester/winutil.Tests.ps1
@@ -41,6 +41,24 @@
 
 #endregion Load Variables needed for testing 
 
+#===========================================================================
+# Tests - Config Files
+#===========================================================================
+
+Describe "Config Files" {
+    Context "Application installs" {
+        It "Imports with no errors" {
+            $global:configs.Applications | should -Not -BeNullOrEmpty
+        }
+        $global:configs.applications.install | Get-Member -MemberType NoteProperty  | ForEach-Object {
+            $TestCase = @{ name = $psitem.name }
+            It "$($psitem.name) should include Winget Install" -TestCases $TestCase{
+                param($name)
+                $null -eq $global:configs.applications.install.$name.winget | should -Befalse -because "$name Did not include a Winget Install"
+            } 
+        }
+    } 
+}
 
 #===========================================================================
 # Tests - GUI
@@ -62,9 +80,6 @@ Describe "GUI" {
         }
         It "Title should match XML" {
             $global:Form.title | should -Be $global:XAML.window.Title
-        }
-        It "Features should be $Global:GUIFeatureCount" {
-            (get-variable | Where-Object {$psitem.name -like "*feature*" -and $psitem.value.GetType().name -eq "CheckBox"}).count | should -Be $Global:GUIFeatureCount
         }
         It "Applications config should contain an application for each GUI checkbox" {
 
