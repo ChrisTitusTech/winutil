65e30656833919bb27cd3e5903f0646837455819
DeveloperDurp <developerdurp@durp.info>
2022-11-26 09:27:54 -0600

    testing theroy

--- Scripts/generate_winutil.ps1
+++ Scripts/generate_winutil.ps1
@@ -8,15 +8,14 @@ $configs = @{}
 ) | ForEach-Object {
     $configs["$PSItem"] = Get-Content .\config\$PSItem.json -Encoding utf8 -Raw
 }
-$winutil = Get-Content ./winutil.ps1 -Encoding utf8 -Raw    
+
+$winutil = Get-Content ./scripts/main.ps1 -Encoding utf8 -Raw    
 
 @"
-
 `$inputXML = '$XAML'
 `$preset = '$($configs.preset)' | convertfrom-json
 `$tweaks = '$($configs.tweaks)' | convertfrom-json
 `$applications = '$($configs.applications)' | convertfrom-json
 `$feature = '$($configs.feature)' | convertfrom-json
 $winutil
-
-"@ | Out-File winutil-generated.ps1
+"@ | Out-File winutil.ps1
