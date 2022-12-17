2e61c61bb9fb4c34b08567719c3afd867dc0f5cc
jstok84 <jstok84@gmail.com>
2022-12-17 23:32:28 +0100

    Update check for Chocolatey  (#520)
    * Update winutil.ps1
    
    * Update check for Chocolatey
    
    Let user decide if he wants to install Chocolatey

--- winutil.ps1
+++ winutil.ps1
@@ -17,9 +17,13 @@ $inputXML = (new-object Net.WebClient).DownloadString("https://raw.githubusercon
 if ((Get-Command -Name choco -ErrorAction Ignore) -and ($chocoVersion = (Get-Item "$env:ChocolateyInstall\choco.exe" -ErrorAction Ignore).VersionInfo.ProductVersion)) {
     Write-Output "Chocolatey Version $chocoVersion is already installed"
 }else {
-    Write-Output "Seems Chocolatey is not installed, installing now"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    powershell choco feature enable -n allowGlobalConfirmation
+    Write-Output "Seems Chocolatey is not installed, installing now?"
+    #Let user decide if he wants to install Chocolatey
+    $confirmation = Read-Host "Are you Sure You Want To Proceed:(y/n)"
+      if ($confirmation -eq 'y') {
+          Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+          powershell choco feature enable -n allowGlobalConfirmation
+      }
 }
 
 #Load config files to hashtable
