ce76e473f12f2bf38adb0a602a400d9965836fb0
jstok84 <jstok84@gmail.com>
2022-12-17 11:00:02 +0100

    Update winutil.ps1

--- winutil.ps1
+++ winutil.ps1
@@ -18,8 +18,12 @@ if ((Get-Command -Name choco -ErrorAction Ignore) -and ($chocoVersion = (Get-Ite
     Write-Output "Chocolatey Version $chocoVersion is already installed"
 }else {
     Write-Output "Seems Chocolatey is not installed, installing now"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    powershell choco feature enable -n allowGlobalConfirmation
+    $confirmation = Read-Host "Are you Sure You Want To Proceed:(y/n)"
+      if ($confirmation -eq 'y') {
+          Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+          powershell choco feature enable -n allowGlobalConfirmation
+      }
+    
 }
 
 #Load config files to hashtable
