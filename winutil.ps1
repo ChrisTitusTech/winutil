440e648a8324892cd7e8827c031f0236300debf3
DeveloperDurp <DeveloperDurp@durp.info>
2023-01-02 20:07:27 -0600

    add logging to update

--- winutil.ps1
+++ winutil.ps1
@@ -270,6 +270,7 @@ Function Update-ProgramWinget {
 
         $host.ui.RawUI.WindowTitle = """Winget Install"""
 
+        Start-Transcript $ENV:TEMP\winget-update.log -Append
         winget upgrade --all
 
         Pause
