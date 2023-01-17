7a6d9d112072ebc40305707445346f1ed1659c41
DeveloperDurp <DeveloperDurp@durp.info>
2023-01-17 16:35:58 -0600

    Move WPFEssTweaksDVR to function

--- winutil.ps1
+++ winutil.ps1
@@ -573,18 +573,10 @@ $WPFtweaksbutton.Add_Click({
         }
 
         If ( $WPFEssTweaksDVR.IsChecked -eq $true ) {
-            If (!(Test-Path "HKCU:\System\GameConfigStore")) {
-                New-Item -Path "HKCU:\System\GameConfigStore" -Force
-            }
-            Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name "GameDVR_DXGIHonorFSEWindowsCompatible" -Type DWord -Value 1
-            Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name "GameDVR_HonorUserFSEBehaviorMode" -Type DWord -Value 1
-            Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name "GameDVR_EFSEFeatureFlags" -Type DWord -Value 0
-            Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name "GameDVR_Enabled" -Type DWord -Value 0
-            Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name "GameDVR_FSEBehavior" -Type DWord -Value 2
-            If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR")) {
-                New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" -Force
-            }
-            Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" -Name "AllowGameDVR" -Type DWord -Value 0
+
+            Write-Host "Disabling GameDVR..."
+            Invoke-WinTweaks "WPFEssTweaksDVR"
+    
             $WPFEssTweaksDVR.IsChecked = $false
         }
 
