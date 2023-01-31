258675061ca1a1298ebdd1f3673a10e9dc8ff107
DeveloperDurp <developerdurp@durp.info>
2023-01-30 19:32:54 -0600

    WPFEssTweaksLoc

--- winutil.ps1
+++ winutil.ps1
@@ -631,14 +631,8 @@ $WPFtweaksbutton.Add_Click({
     }
     If ( $WPFEssTweaksLoc.IsChecked -eq $true ) {
         Write-Host "Disabling Location Tracking..."
-        If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location")) {
-            New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Force | Out-Null
-        }
-        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name "Value" -Type String -Value "Deny"
-        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type DWord -Value 0
-        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\lfsvc\Service\Configuration" -Name "Status" -Type DWord -Value 0
         Write-Host "Disabling automatic Maps updates..."
-        Set-ItemProperty -Path "HKLM:\SYSTEM\Maps" -Name "AutoUpdateEnabled" -Type DWord -Value 0
+        Invoke-WinTweaks WPFEssTweaksLoc
         $WPFEssTweaksLoc.IsChecked = $false
     }
     If ( $WPFMiscTweaksDisableTPMCheck.IsChecked -eq $true ) {
