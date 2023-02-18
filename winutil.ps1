7dd0be84190500e56c2aa80ed1f405d7c3435239
DeveloperDurp <developerdurp@durp.info>
2023-02-18 09:32:38 -0600

    WPFEssTweaksDeBloat

--- winutil.ps1
+++ winutil.ps1
@@ -28,12 +28,17 @@ Start-Transcript $ENV:TEMP\Winutil.log -Append
 
 # variable to sync between runspaces
 $sync = [Hashtable]::Synchronized(@{})
+$sync.IsDev = $true
 $sync.BranchToUse = $BranchToUse
 $sync.PSScriptRoot = $PSScriptRoot
 if (!$sync.PSScriptRoot){$sync.PSScriptRoot = (Get-Location).Path}
 
-$inputXML = Get-Content "$($sync.PSScriptRoot)\MainWindow.xaml" #uncomment for development
-#$inputXML = (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTitusTech/winutil/$BranchToUse/MainWindow.xaml") #uncomment for Production
+if($sync.IsDev -eq $true){
+    $inputXML = Get-Content "$($sync.PSScriptRoot)\MainWindow.xaml"
+}
+Else{
+    $inputXML = (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTitusTech/winutil/$BranchToUse/MainWindow.xaml")
+}
 
 $inputXML = $inputXML -replace 'mc:Ignorable="d"', '' -replace "x:N", 'N' -replace '^<Win.*', '<Window'
 [void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
@@ -430,6 +435,11 @@ function Invoke-WinTweaks {
             Set-WinUtilService -Name $psitem.Name -StartupType $psitem.StartupType
         }
     }
+    if($sync.configs.tweaks.$CheckBox.appx){
+        $sync.configs.tweaks.$CheckBox.appx | ForEach-Object {
+            Remove-WinUtilAPPX -Name $psitem
+        }
+    }
     if($sync.configs.tweaks.$CheckBox.InvokeScript){
         $sync.configs.tweaks.$CheckBox.InvokeScript | ForEach-Object {
             $Scriptblock = [scriptblock]::Create($psitem)
@@ -545,6 +555,31 @@ function Set-WinUtilScheduledTask {
     }
     Catch{
         Write-Warning "Unable to run script for $name due to unhandled exception"
+        Write-Warning $psitem.Exception.StackTrace 
+    }
+}
+
+function Remove-WinUtilAPPX {
+    param (
+        $Name
+    )
+
+    Try{
+        Write-Host "Removing $Name"
+        Get-AppxPackage "*$Name*" | Remove-AppxPackage -ErrorAction SilentlyContinue
+        Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like "*$Name*" | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+    }
+    Catch [System.Exception] {
+        if($psitem.Exception.Message -like "*The requested operation requires elevation*"){
+            Write-Warning "Unable to uninstall $name due to a Security Exception"
+        }
+        Else{
+            Write-Warning "Unable to uninstall $name due to unhandled exception"
+            Write-Warning $psitem.Exception.StackTrace 
+        }
+    }
+    Catch{
+        Write-Warning "Unable to uninstall $name due to unhandled exception"
         Write-Warning $psitem.Exception.StackTrace 
     }
 }
@@ -826,182 +861,12 @@ $WPFtweaksbutton.Add_Click({
     }
     If ( $WPFEssTweaksRemoveEdge.IsChecked -eq $true ) {
         Write-Host "Removing Microsoft Edge..."
-        Invoke-WebRequest -useb https://raw.githubusercontent.com/ChrisTitusTech/winutil/$BranchToUse/Edge_Removal.bat | Invoke-Expression
+        Invoke-WinTweaks WPFEssTweaksRemoveEdge
         $WPFEssTweaksRemoveEdge.IsChecked = $false
     }
     If ( $WPFEssTweaksDeBloat.IsChecked -eq $true ) {
-        $Bloatware = @(
-            #Unnecessary Windows 10 AppX Apps
-            "3DBuilder"
-            "Microsoft3DViewer"
-            "AppConnector"
-            "BingFinance"
-            "BingNews"
-            "BingSports"
-            "BingTranslator"
-            "BingWeather"
-            "BingFoodAndDrink"
-            "BingHealthAndFitness"
-            "BingTravel"
-            "MinecraftUWP"
-            "GamingServices"
-            # "WindowsReadingList"
-            "GetHelp"
-            "Getstarted"
-            "Messaging"
-            "Microsoft3DViewer"
-            "MicrosoftSolitaireCollection"
-            "NetworkSpeedTest"
-            "News"
-            "Lens"
-            "Sway"
-            "OneNote"
-            "OneConnect"
-            "People"
-            "Print3D"
-            "SkypeApp"
-            "Todos"
-            "Wallet"
-            "Whiteboard"
-            "WindowsAlarms"
-            "windowscommunicationsapps"
-            "WindowsFeedbackHub"
-            "WindowsMaps"
-            "WindowsPhone"
-            "WindowsSoundRecorder"
-            "XboxApp"
-            "ConnectivityStore"
-            "CommsPhone"
-            "ScreenSketch"
-            "TCUI"
-            "XboxGameOverlay"
-            "XboxGameCallableUI"
-            "XboxSpeechToTextOverlay"
-            "MixedReality.Portal"
-            "ZuneMusic"
-            "ZuneVideo"
-            #"YourPhone"
-            "Getstarted"
-            "MicrosoftOfficeHub"
-
-            #Sponsored Windows 10 AppX Apps
-            #Add sponsored/featured apps to remove in the "*AppName*" format
-            "EclipseManager"
-            "ActiproSoftwareLLC"
-            "AdobeSystemsIncorporated.AdobePhotoshopExpress"
-            "Duolingo-LearnLanguagesforFree"
-            "PandoraMediaInc"
-            "CandyCrush"
-            "BubbleWitch3Saga"
-            "Wunderlist"
-            "Flipboard"
-            "Twitter"
-            "Facebook"
-            "Royal Revolt"
-            "Sway"
-            "Speed Test"
-            "Dolby"
-            "Viber"
-            "ACGMediaPlayer"
-            "Netflix"
-            "OneCalendar"
-            "LinkedInforWindows"
-            "HiddenCityMysteryofShadows"
-            "Hulu"
-            "HiddenCity"
-            "AdobePhotoshopExpress"
-            "HotspotShieldFreeVPN"
-
-            #Optional: Typically not removed but you can if you need to
-            "Advertising"
-            #"MSPaint"
-            #"MicrosoftStickyNotes"
-            #"Windows.Photos"
-            #"WindowsCalculator"
-            #"WindowsStore"
-
-            # HPBloatware Packages
-            "HPJumpStarts"
-            "HPPCHardwareDiagnosticsWindows"
-            "HPPowerManager"
-            "HPPrivacySettings"
-            "HPSupportAssistant"
-            "HPSureShieldAI"
-            "HPSystemInformation"
-            "HPQuickDrop"
-            "HPWorkWell"
-            "myHP"
-            "HPDesktopSupportUtilities"
-            "HPQuickTouch"
-            "HPEasyClean"
-            "HPSystemInformation"
-        )
-
-        ## Teams Removal - Source: https://github.com/asheroto/UninstallTeams
-        function getUninstallString($match) {
-            return (Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object { $_.DisplayName -like "*$match*" }).UninstallString
-        }
-
-        $TeamsPath = [System.IO.Path]::Combine($env:LOCALAPPDATA, 'Microsoft', 'Teams')
-        $TeamsUpdateExePath = [System.IO.Path]::Combine($TeamsPath, 'Update.exe')
-
-        Write-Host "Stopping Teams process..."
-        Stop-Process -Name "*teams*" -Force -ErrorAction SilentlyContinue
-
-        Write-Host "Uninstalling Teams from AppData\Microsoft\Teams"
-        if ([System.IO.File]::Exists($TeamsUpdateExePath)) {
-            # Uninstall app
-            $proc = Start-Process $TeamsUpdateExePath "-uninstall -s" -PassThru
-            $proc.WaitForExit()
-        }
-
-        Write-Host "Removing Teams AppxPackage..."
-        Get-AppxPackage "*Teams*" | Remove-AppxPackage -ErrorAction SilentlyContinue
-        Get-AppxPackage "*Teams*" -AllUsers | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
-
-        Write-Host "Deleting Teams directory"
-        if ([System.IO.Directory]::Exists($TeamsPath)) {
-            Remove-Item $TeamsPath -Force -Recurse -ErrorAction SilentlyContinue
-        }
-
-        Write-Host "Deleting Teams uninstall registry key"
-        # Uninstall from Uninstall registry key UninstallString
-        $us = getUninstallString("Teams");
-        if ($us.Length -gt 0) {
-            $us = ($us.Replace("/I", "/uninstall ") + " /quiet").Replace("  ", " ")
-            $FilePath = ($us.Substring(0, $us.IndexOf(".exe") + 4).Trim())
-            $ProcessArgs = ($us.Substring($us.IndexOf(".exe") + 5).Trim().replace("  ", " "))
-            $proc = Start-Process -FilePath $FilePath -Args $ProcessArgs -PassThru
-            $proc.WaitForExit()
-        }
-
-        Write-Host "Restart computer to complete teams uninstall"
-
-        Write-Host "Removing Bloatware"
-
-        foreach ($Bloat in $Bloatware) {
-            Get-AppxPackage "*$Bloat*" | Remove-AppxPackage -ErrorAction SilentlyContinue
-            Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like "*$Bloat*" | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
-            Write-Host "Trying to remove $Bloat."
-        }
-
-        Write-Host "Finished Removing Bloatware Apps"
-        Write-Host "Removing Bloatware Programs"
-        # Remove installed programs
-        $InstalledPrograms = Get-Package | Where-Object { $UninstallPrograms -contains $_.Name }
-        $InstalledPrograms | ForEach-Object {
-
-            Write-Host -Object "Attempting to uninstall: [$($_.Name)]..."
-
-            Try {
-                $Null = $_ | Uninstall-Package -AllVersions -Force -ErrorAction SilentlyContinue
-                Write-Host -Object "Successfully uninstalled: [$($_.Name)]"
-            }
-            Catch {
-                Write-Warning -Message "Failed to uninstall: [$($_.Name)]"
-            }
-        }
-        Write-Host "Finished Removing Bloatware Programs"
+        Write-Host "Debloating..."
+        Invoke-WinTweaks WPFEssTweaksDeBloat
         $WPFEssTweaksDeBloat.IsChecked = $false
     }
 
@@ -1531,20 +1396,23 @@ Invoke-Runspace -ScriptBlock {
         "dns"
     )
 
-    $ConfigsToLoad | ForEach-Object {
-        $sync.configs["$psitem"] = [System.Net.WebClient]::new().DownloadStringTaskAsync("https://raw.githubusercontent.com/ChrisTitusTech/winutil/$($Sync.BranchToUse)/config/$psitem.json")
+    if($sync.IsDev -eq $true){
+        $ConfigsToLoad | ForEach-Object {
+            $sync.configs["$psitem"] = Get-Content "$($sync.PSScriptRoot)\config\$PSItem.json" | ConvertFrom-Json
+        } 
+        $sync.ComputerInfo = Get-ComputerInfo
     }
-
-    $sync.ComputerInfo = Get-ComputerInfo
-
-    $ConfigsToLoad | ForEach-Object {
-        $sync.configs["$psitem"] = ConvertFrom-Json ($sync.configs["$psitem"].GetAwaiter().GetResult())
+    Else{
+        $ConfigsToLoad | ForEach-Object {
+            $sync.configs["$psitem"] = [System.Net.WebClient]::new().DownloadStringTaskAsync("https://raw.githubusercontent.com/ChrisTitusTech/winutil/$($Sync.BranchToUse)/config/$psitem.json")
+        }
+    
+        $sync.ComputerInfo = Get-ComputerInfo
+    
+        $ConfigsToLoad | ForEach-Object {
+            $sync.configs["$psitem"] = ConvertFrom-Json ($sync.configs["$psitem"].GetAwaiter().GetResult())
+        }
     }
-
-    #Uncomment to force local files
-    $ConfigsToLoad | ForEach-Object {
-        $sync.configs["$psitem"] = Get-Content "$($sync.PSScriptRoot)\config\$PSItem.json" | ConvertFrom-Json
-    } 
     
     $sync.ConfigLoaded = $True
 } | Out-Null
