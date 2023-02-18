a2f3ec6b1f8cc95964832803643b09ffa2c249a1
DeveloperDurp <developerdurp@durp.info>
2023-02-18 08:28:02 -0600

    WPFMiscTweaksDisplay

--- winutil.ps1
+++ winutil.ps1
@@ -806,17 +806,7 @@ $WPFtweaksbutton.Add_Click({
     }
     If ( $WPFMiscTweaksDisplay.IsChecked -eq $true ) {
         Write-Host "Adjusting visual effects for performance..."
-        Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "DragFullWindows" -Type String -Value 0
-        Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "MenuShowDelay" -Type String -Value 200
-        Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "UserPreferencesMask" -Type Binary -Value ([byte[]](144, 18, 3, 128, 16, 0, 0, 0))
-        Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\WindowMetrics" -Name "MinAnimate" -Type String -Value 0
-        Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardDelay" -Type DWord -Value 0
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ListviewAlphaSelect" -Type DWord -Value 0
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ListviewShadow" -Type DWord -Value 0
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAnimations" -Type DWord -Value 0
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "VisualFXSetting" -Type DWord -Value 3
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\DWM" -Name "EnableAeroPeek" -Type DWord -Value 0
-        Write-Host "Adjusted visual effects for performance"
+        Invoke-WinTweaks WPFMiscTweaksDisplay
         $WPFMiscTweaksDisplay.IsChecked = $false
     }
     If ( $WPFMiscTweaksDisableMouseAcceleration.IsChecked -eq $true ) {
