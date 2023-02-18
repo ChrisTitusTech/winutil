a636026e7c00eff158e841982860e82cd9bd9f74
DeveloperDurp <developerdurp@durp.info>
2023-02-18 07:58:26 -0600

    WPFEssTweaksWifi

--- winutil.ps1
+++ winutil.ps1
@@ -769,14 +769,7 @@ $WPFtweaksbutton.Add_Click({
     }
     If ( $WPFEssTweaksWifi.IsChecked -eq $true ) {
         Write-Host "Disabling Wi-Fi Sense..."
-        If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
-            New-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Force | Out-Null
-        }
-        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Name "Value" -Type DWord -Value 0
-        If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots")) {
-            New-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Force | Out-Null
-        }
-        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Name "Value" -Type DWord -Value 0
+        Invoke-WinTweaks WPFEssTweaksWifi
         $WPFEssTweaksWifi.IsChecked = $false
     }
     If ( $WPFMiscTweaksLapPower.IsChecked -eq $true ) {
