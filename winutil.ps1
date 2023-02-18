fdbb8a33a98932c230174ff42e09cc42e9e0e734
DeveloperDurp <developerdurp@durp.info>
2023-02-18 08:00:17 -0600

    WPFMiscTweaksLapPower

--- winutil.ps1
+++ winutil.ps1
@@ -773,10 +773,8 @@ $WPFtweaksbutton.Add_Click({
         $WPFEssTweaksWifi.IsChecked = $false
     }
     If ( $WPFMiscTweaksLapPower.IsChecked -eq $true ) {
-        If (Test-Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling") {
-            Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling" -Name "PowerThrottlingOff" -Type DWord -Value 00000000
-        }
-        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name "HiberbootEnabled" -Type DWord -Value 0000001
+        Write-Host "Enabling Power Throttling..."
+        Invoke-WinTweaks WPFMiscTweaksLapPower
         $WPFMiscTweaksLapPower.IsChecked = $false
     }
     If ( $WPFMiscTweaksLapNum.IsChecked -eq $true ) {
