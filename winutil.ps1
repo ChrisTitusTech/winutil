29fd3df63acbea53028128bcb2ad9992bb7e181f
DeveloperDurp <developerdurp@durp.info>
2023-02-18 05:05:43 -0600

    WPFMiscTweaksDisableNotifications

--- winutil.ps1
+++ winutil.ps1
@@ -661,9 +661,7 @@ $WPFtweaksbutton.Add_Click({
 
     If ( $WPFMiscTweaksDisableNotifications.IsChecked -eq $true ) {
         Write-Host "Disabling Notifications and Action Center..."
-        New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows" -Name "Explorer" -force
-        New-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -PropertyType "DWord" -Value 1
-        New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -PropertyType "DWord" -Value 0 -force
+        Invoke-WinTweaks WPFMiscTweaksDisableNotifications
         $WPFMiscTweaksDisableNotifications.IsChecked = $false
     }
 
