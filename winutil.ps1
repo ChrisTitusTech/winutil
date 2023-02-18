7e47e9dad79779764b50c8c642679b4fa8034d70
DeveloperDurp <developerdurp@durp.info>
2023-02-18 05:14:10 -0600

    WPFMiscTweaksRightClickMenu

--- winutil.ps1
+++ winutil.ps1
@@ -667,7 +667,7 @@ $WPFtweaksbutton.Add_Click({
 
     If ( $WPFMiscTweaksRightClickMenu.IsChecked -eq $true ) {
         Write-Host "Setting Classic Right-Click Menu..."
-        New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Name "InprocServer32" -force -value ""
+        Invoke-WinTweaks WPFMiscTweaksRightClickMenu
         $WPFMiscTweaksRightClickMenu.IsChecked = $false
     }
     If ( $WPFchangedns.text -eq 'Google' ) {
