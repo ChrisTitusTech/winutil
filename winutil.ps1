ff19a81eec963a0fdc580a6877102e5cf78fa22b
DeveloperDurp <developerdurp@durp.info>
2023-02-18 06:19:54 -0600

    WPFEssTweaksRP

--- winutil.ps1
+++ winutil.ps1
@@ -699,8 +699,7 @@ $WPFtweaksbutton.Add_Click({
     }
     If ( $WPFEssTweaksRP.IsChecked -eq $true ) {
         Write-Host "Creating Restore Point in case something bad happens"
-        Enable-ComputerRestore -Drive "$env:SystemDrive"
-        Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
+        Invoke-WinTweaks WPFEssTweaksRP
         $WPFEssTweaksRP.IsChecked = $false
     }
     If ( $WPFEssTweaksServices.IsChecked -eq $true ) {
