2b57ad2b87ad36e9904e8dcb27ab524206a25de8
DeveloperDurp <DeveloperDurp@durp.info>
2023-03-12 14:12:08 -0500

    more formating fixing

--- functions/public/Invoke-WPFundoall.ps1
+++ functions/public/Invoke-WPFundoall.ps1
@@ -15,31 +15,31 @@ function Invoke-WPFundoall {
     $Tweaks = Get-WinUtilCheckBoxes -Group "WPFTweaks"
 
     if ($tweaks.count -eq 0){
-    $msg = "Please check the tweaks you wish to undo."
-    [System.Windows.MessageBox]::Show($msg, "Winutil", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
-    return
+        $msg = "Please check the tweaks you wish to undo."
+        [System.Windows.MessageBox]::Show($msg, "Winutil", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
+        return
     }      
     
     Invoke-WPFRunspace -ArgumentList $Tweaks -ScriptBlock {
-    param($Tweaks)
+        param($Tweaks)
 
-    $sync.ProcessRunning = $true
+        $sync.ProcessRunning = $true
 
-    Foreach ($tweak in $tweaks){
-        Invoke-WinUtilTweaks $tweak -undo $true
-    }
+        Foreach ($tweak in $tweaks){
+            Invoke-WinUtilTweaks $tweak -undo $true
+        }
 
-    $sync.ProcessRunning = $false
-    Write-Host "=================================="
-    Write-Host "---  Undo Tweaks are Finished  ---"
-    Write-Host "=================================="
+        $sync.ProcessRunning = $false
+        Write-Host "=================================="
+        Write-Host "---  Undo Tweaks are Finished  ---"
+        Write-Host "=================================="
 
-    $ButtonType = [System.Windows.MessageBoxButton]::OK
-    $MessageboxTitle = "Tweaks are Finished "
-    $Messageboxbody = ("Done")
-    $MessageIcon = [System.Windows.MessageBoxImage]::Information
+        $ButtonType = [System.Windows.MessageBoxButton]::OK
+        $MessageboxTitle = "Tweaks are Finished "
+        $Messageboxbody = ("Done")
+        $MessageIcon = [System.Windows.MessageBoxImage]::Information
 
-    [System.Windows.MessageBox]::Show($Messageboxbody, $MessageboxTitle, $ButtonType, $MessageIcon)
+        [System.Windows.MessageBox]::Show($Messageboxbody, $MessageboxTitle, $ButtonType, $MessageIcon)
     }
 
 <#
