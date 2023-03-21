cb24267932dba09e105f87f87880889c9a9919ff
ChrisTitusTech <ChrisTitusTech@users.noreply.github.com>
2023-03-21 17:19:56 +0000

    Compile Winutil

--- winutil.ps1
+++ winutil.ps1
@@ -1162,21 +1162,27 @@ function Invoke-WPFTab {
 }
 function Invoke-WPFtweaksbutton {
   <#
-    
-        .DESCRIPTION
-        PlaceHolder
-    
-    #>
+  
+      .DESCRIPTION
+      PlaceHolder
+  
+  #>
 
   if($sync.ProcessRunning){
     $msg = "Install process is currently running."
     [System.Windows.MessageBox]::Show($msg, "Winutil", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
     return
-}
+  }
 
   $Tweaks = Get-WinUtilCheckBoxes -Group "WPFTweaks"
 
   Set-WinUtilDNS -DNSProvider $WPFchangedns.text
+
+  if ($tweaks.count -eq 0 -and  $WPFchangedns.text -eq "Default"){
+    $msg = "Please check the tweaks you wish to perform."
+    [System.Windows.MessageBox]::Show($msg, "Winutil", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
+    return
+  }
 
   Invoke-WPFRunspace -ArgumentList $Tweaks -ScriptBlock {
     param($Tweaks)
@@ -1243,30 +1249,36 @@ function Invoke-WPFundoall {
         [System.Windows.MessageBox]::Show($msg, "Winutil", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
         return
     }
+
+    $Tweaks = Get-WinUtilCheckBoxes -Group "WPFTweaks"
+
+    if ($tweaks.count -eq 0){
+        $msg = "Please check the tweaks you wish to undo."
+        [System.Windows.MessageBox]::Show($msg, "Winutil", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
+        return
+    }      
     
-      $Tweaks = Get-WinUtilCheckBoxes -Group "WPFTweaks"
-        
-      Invoke-WPFRunspace -ArgumentList $Tweaks -ScriptBlock {
+    Invoke-WPFRunspace -ArgumentList $Tweaks -ScriptBlock {
         param($Tweaks)
-    
+
         $sync.ProcessRunning = $true
-    
+
         Foreach ($tweak in $tweaks){
             Invoke-WinUtilTweaks $tweak -undo $true
         }
-    
+
         $sync.ProcessRunning = $false
         Write-Host "=================================="
         Write-Host "---  Undo Tweaks are Finished  ---"
         Write-Host "=================================="
-    
+
         $ButtonType = [System.Windows.MessageBoxButton]::OK
         $MessageboxTitle = "Tweaks are Finished "
         $Messageboxbody = ("Done")
         $MessageIcon = [System.Windows.MessageBoxImage]::Information
-    
+
         [System.Windows.MessageBox]::Show($Messageboxbody, $MessageboxTitle, $ButtonType, $MessageIcon)
-      }
+    }
 
 <#
 
