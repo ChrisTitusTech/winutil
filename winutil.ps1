de680572324b42c1f3cf49f5a4aef932751f063c
ChrisTitusTech <ChrisTitusTech@users.noreply.github.com>
2023-04-14 18:01:18 +0000

    Compile Winutil

--- winutil.ps1
+++ winutil.ps1
@@ -10,7 +10,7 @@
     Author         : Chris Titus @christitustech
     Runspace Author: @DeveloperDurp
     GitHub         : https://github.com/ChrisTitusTech
-    Version        : 23.03.27
+    Version        : 23.04.14
 #>
 
 Start-Transcript $ENV:TEMP\Winutil.log -Append
@@ -21,7 +21,7 @@ Add-Type -AssemblyName System.Windows.Forms
 # variable to sync between runspaces
 $sync = [Hashtable]::Synchronized(@{})
 $sync.PSScriptRoot = $PSScriptRoot
-$sync.version = "23.03.27"
+$sync.version = "23.04.14"
 $sync.configs = @{}
 $sync.ProcessRunning = $false
 Function Get-WinUtilCheckBoxes {
@@ -2619,7 +2619,7 @@ $sync.configs.applications = '{
     "choco": "nuget.commandline"
   },
   "WPFInstallwinrar": {
-    "winget": "RARLab.WinRAR",
+    "winget": "RARLab.WinRar",
     "choco": "winrar"
   }
 }' | convertfrom-json
