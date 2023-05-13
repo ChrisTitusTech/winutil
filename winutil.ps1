0c88d67b1ff6c8117e042b440ef4c31405e136d9
Chris Titus <contact@christitus.com>
2023-05-13 08:45:07 -0500

    Fix Wifi issue (#754)
    * Compile Winutil
    
    * fix wlansvc service (#749)
    
    * Compile Winutil
    
    ---------
    
    Co-authored-by: ChrisTitusTech <ChrisTitusTech@users.noreply.github.com>
    Co-authored-by: Padsala Tushal <57517785+padsalatushal@users.noreply.github.com>

--- winutil.ps1
+++ winutil.ps1
@@ -10,7 +10,7 @@
     Author         : Chris Titus @christitustech
     Runspace Author: @DeveloperDurp
     GitHub         : https://github.com/ChrisTitusTech
-    Version        : 23.05.12
+    Version        : 23.05.13
 #>
 
 Start-Transcript $ENV:TEMP\Winutil.log -Append
@@ -21,7 +21,7 @@ Add-Type -AssemblyName System.Windows.Forms
 # variable to sync between runspaces
 $sync = [Hashtable]::Synchronized(@{})
 $sync.PSScriptRoot = $PSScriptRoot
-$sync.version = "23.05.12"
+$sync.version = "23.05.13"
 $sync.configs = @{}
 $sync.ProcessRunning = $false
 Function Get-WinUtilCheckBoxes {
@@ -4027,11 +4027,7 @@ $sync.configs.tweaks = '{
       },
       {
         "Name": "WlanSvc",
-        "OriginalType": "Manual"
-      },
-      {
-        "Name": "Wlansvc",
-        "StartupType": "Manual"
+        "OriginalType": "Automatic"
       },
       {
         "Name": "WpcMonSvc",
@@ -5303,7 +5299,7 @@ catch [System.Management.Automation.MethodInvocationException] {
     }
 }
 catch {
-    # If it broke some other way <img draggable="false" role="img" class="emoji" alt="??" src="https://s0.wp.com/wp-content/mu-plugins/wpcom-smileys/twemoji/2/svg/1f600.svg">
+    # If it broke some other way <img draggable="false" role="img" class="emoji" alt="????" src="https://s0.wp.com/wp-content/mu-plugins/wpcom-smileys/twemoji/2/svg/1f600.svg">
     Write-Host "Unable to load Windows.Markup.XamlReader. Double-check syntax and ensure .net is installed."
 }
 
