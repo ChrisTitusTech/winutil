f14f18910e741c49c68bbb23f32f97eb54a3cbcf
David Dereje Mesganaw <DEREJEISACK@GMAIL.COM>
2023-05-24 00:23:04 +0300

    Update Set-WinUtilDNS.ps1

--- functions/private/Set-WinUtilDNS.ps1
+++ functions/private/Set-WinUtilDNS.ps1
@@ -1,32 +1,33 @@
 function Set-WinUtilDNS {
     <#
-    
         .DESCRIPTION
-        This function will set the DNS of all interfaces that are in the "Up" state. It will lookup the values from the DNS.Json file
+        This function sets the DNS of all interfaces in the "Up" state using values from the DNS.Json file.
 
         .EXAMPLE
-
         Set-WinUtilDNS -DNSProvider "google"
-    
     #>
     param($DNSProvider)
-    if($DNSProvider -eq "Default"){return}
-    Try{
-        $Adapters = Get-NetAdapter | Where-Object {$_.Status -eq "Up"}
-        Write-Host "Ensuring DNS is set to $DNSProvider on the following interfaces"
-        Write-Host $($Adapters | Out-String)
 
-        Foreach ($Adapter in $Adapters){
-            if($DNSProvider -eq "DHCP"){
+    if ($DNSProvider -eq "Default") { return }
+
+    try {
+        $Adapters = Get-NetAdapter | Where-Object { $_.Status -eq "Up" }
+        Write-Host "Setting DNS to $DNSProvider on the following interfaces:"
+        $Adapters | Format-Table
+
+        foreach ($Adapter in $Adapters) {
+            if ($DNSProvider -eq "DHCP") {
                 Set-DnsClientServerAddress -InterfaceIndex $Adapter.ifIndex -ResetServerAddresses
             }
-            Else{
-                Set-DnsClientServerAddress -InterfaceIndex $Adapter.ifIndex -ServerAddresses ("$($sync.configs.dns.$DNSProvider.Primary)", "$($sync.configs.dns.$DNSProvider.Secondary)")
+            else {
+                $PrimaryDNS = $sync.configs.dns.$DNSProvider.Primary
+                $SecondaryDNS = $sync.configs.dns.$DNSProvider.Secondary
+                Set-DnsClientServerAddress -InterfaceIndex $Adapter.ifIndex -ServerAddresses ($PrimaryDNS, $SecondaryDNS)
             }
         }
     }
-    Catch{
+    catch {
         Write-Warning "Unable to set DNS Provider due to an unhandled exception"
-        Write-Warning $psitem.Exception.StackTrace 
+        Write-Warning $Error[0].Exception.StackTrace
     }
 }
