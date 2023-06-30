9cf892311b5a9bc75b310cebb4de964abb120147
ChaseKnowlden <haroldknowlden@gmail.com>
2023-06-30 13:35:27 -0700

    Fix script not running

--- winutil.ps1
+++ winutil.ps1
@@ -10,7 +10,7 @@
     Author         : Chris Titus @christitustech
     Runspace Author: @DeveloperDurp
     GitHub         : https://github.com/ChrisTitusTech
-    Version        : 23.05.16
+    Version        : 23.06.30
 #>
 
 Start-Transcript $ENV:TEMP\Winutil.log -Append
@@ -21,7 +21,7 @@ Add-Type -AssemblyName System.Windows.Forms
 # variable to sync between runspaces
 $sync = [Hashtable]::Synchronized(@{})
 $sync.PSScriptRoot = $PSScriptRoot
-$sync.version = "23.05.16"
+$sync.version = "23.06.30"
 $sync.configs = @{}
 $sync.ProcessRunning = $false
 Function Get-WinUtilCheckBoxes {
@@ -5120,60 +5120,8 @@ $sync.configs.tweaks = '{
   },
   "WPFEssTweaksRemoveEdge": {
     "InvokeScript": [
-        "      
-        # Stop Edge Task
-        Stop-Process -Name \"msedge\" -Force -ErrorAction SilentlyContinue
-
-        # Uninstall - Edge
-        $edgePath = \"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\"
-        if (Test-Path $edgePath) {
-            $edgeVersions = Get-ChildItem $edgePath -Directory
-            foreach ($version in $edgeVersions) {
-                $installerPath = Join-Path $version.FullName \"Installer\"
-                if (Test-Path $installerPath) {
-                    Set-Location -Path $installerPath | Out-Null
-                    if (Test-Path \"setup.exe\") {
-                        Write-Host \"Removing Microsoft Edge\"
-                        Start-Process -Wait -FilePath \"setup.exe\" -ArgumentList \"--uninstall --system-level --force-uninstall\"
-                    }
-                }
-            }
-        }
-
-        # Uninstall - EdgeWebView
-        $edgeWebViewPath = \"C:\\Program Files (x86)\\Microsoft\\EdgeWebView\\Application\"
-        if (Test-Path $edgeWebViewPath) {
-            $edgeWebViewVersions = Get-ChildItem $edgeWebViewPath -Directory
-            foreach ($version in $edgeWebViewVersions) {
-                $installerPath = Join-Path $version.FullName \"Installer\"
-                if (Test-Path $installerPath) {
-                    Set-Location -Path $installerPath | Out-Null
-                    if (Test-Path \"setup.exe\") {
-                        Write-Host \"Removing EdgeWebView\"
-                        Start-Process -Wait -FilePath \"setup.exe\" -ArgumentList \"--uninstall --msedgewebview --system-level --force-uninstall\"
-                    }
-                }
-            }
-        }
-
-        # Delete Edge desktop icon, from all users
-        $users = Get-ChildItem -Path \"C:\\Users\" -Directory
-        foreach ($user in $users) {
-            $desktopPath = Join-Path -Path $user.FullName -ChildPath \"Desktop\"
-            Remove-Item -Path \"$desktopPath\\edge.lnk\" -Force -ErrorAction SilentlyContinue
-            Remove-Item -Path \"$desktopPath\\Microsoft Edge.lnk\" -Force -ErrorAction SilentlyContinue
-        }
-
-        # Delete additional files
-        if (Test-Path \"C:\\Windows\\System32\\MicrosoftEdgeCP.exe\") {
-            $edgeFiles = Get-ChildItem -Path \"C:\\Windows\\System32\" -Filter \"MicrosoftEdge*\" -File
-            foreach ($file in $edgeFiles) {
-                $filePath = Join-Path -Path $file.Directory.FullName -ChildPath $file.Name
-                takeown.exe /F \"$filePath\" > $null
-                icacls.exe \"$filePath\" /inheritance:e /grant \"$env:UserName:(OI)(CI)F\" /T /C > $null
-                Remove-Item -Path $filePath -Force -ErrorAction SilentlyContinue
-            }
-        }
+        "curl.exe -s \"https://raw.githubusercontent.com/ChaseKnowlden/winutil/remove-edge/removeedge.bat\" -o $ENV:temp\\removeedge.bat
+        cmd.exe /c %temp%\\removeedge.bat
         "
     ]
   },
@@ -5413,7 +5361,7 @@ catch [System.Management.Automation.MethodInvocationException] {
     }
 }
 catch {
-    # If it broke some other way <img draggable="false" role="img" class="emoji" alt="??" src="https://s0.wp.com/wp-content/mu-plugins/wpcom-smileys/twemoji/2/svg/1f600.svg">
+    # If it broke some other way <img draggable="false" role="img" class="emoji" alt="????" src="https://s0.wp.com/wp-content/mu-plugins/wpcom-smileys/twemoji/2/svg/1f600.svg">
     Write-Host "Unable to load Windows.Markup.XamlReader. Double-check syntax and ensure .net is installed."
 }
 
