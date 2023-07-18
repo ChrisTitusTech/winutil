fd599956cea781119d277c88305ae7967d2194e6
Chris Titus <contact@christitus.com>
2023-07-18 16:33:21 -0500

    Update edge.ps1

--- edge.ps1
+++ edge.ps1
@@ -11,16 +11,16 @@ if ($also_remove_webview -eq 1) {
 
 ## set useless policies
 foreach ($p in 'HKLM\SOFTWARE\Policies','HKLM\SOFTWARE') {
-  reg add "$p\Microsoft\EdgeUpdate" /f /v InstallDefault /d 0 /t reg_dword >$null 2>$null
-  reg add "$p\Microsoft\EdgeUpdate" /f /v Install{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062} /d 0 /t reg_dword >$null 2>$null
-  reg add "$p\Microsoft\EdgeUpdate" /f /v Install{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5} /d 1 /t reg_dword >$null 2>$null
-  reg add "$p\Microsoft\EdgeUpdate" /f /v DoNotUpdateToEdgeWithChromium /d 1 /t reg_dword >$null 2>$null
+    Set-ItemProperty -Path "$p\Microsoft\EdgeUpdate" -Name InstallDefault -Value 0 -Type DWord >$null 2>$null
+    Set-ItemProperty -Path "$p\Microsoft\EdgeUpdate" -Name Install{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062} -Value 0 -Type DWord >$null 2>$null
+    Set-ItemProperty -Path "$p\Microsoft\EdgeUpdate" -Name Install{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5} -Value 1 -Type DWord >$null 2>$null
+    Set-ItemProperty -Path "$p\Microsoft\EdgeUpdate" -Name DoNotUpdateToEdgeWithChromium -Value 1 -Type DWord >$null 2>$null
 }
 ## clear win32 uninstall block
 foreach ($hk in 'HKCU','HKLM') {
     foreach ($wow in '','\Wow6432Node') {
         foreach ($i in $remove_win32) {
-            reg delete "$hk\SOFTWARE${wow}\Microsoft\Windows\CurrentVersion\Uninstall\$i" /f /v NoRemove >$null 2>$null
+            Remove-ItemProperty -Path "$hk\SOFTWARE${wow}\Microsoft\Windows\CurrentVersion\Uninstall\$i" -Name NoRemove -ErrorAction SilentlyContinue
         }
     }
 }
@@ -29,7 +29,7 @@ $setup = @()
 $bho = @()
 $bho += "$env:ProgramData\ie_to_edge_stub.exe"
 $bho += "$env:Public\ie_to_edge_stub.exe"
-"LocalApplicationData","ProgramFilesX86","ProgramFiles" | foreach {
+"LocalApplicationData","ProgramFilesX86","ProgramFiles" | ForEach-Object {
     $setup += Get-ChildItem "$($([Environment]::GetFolderPath($_)))\Microsoft\Edge*\setup.exe" -Recurse -ErrorAction SilentlyContinue
     $bho += Get-ChildItem "$($([Environment]::GetFolderPath($_)))\Microsoft\Edge*\ie_to_edge_stub.exe" -Recurse -ErrorAction SilentlyContinue
 }
@@ -66,8 +66,8 @@ foreach ($choice in $remove_appx) {
     foreach ($appx in $($provisioned | Where-Object { $_.PackageName -like "*$choice*" })) {
         $PackageFamilyName = ($appxpackage | Where-Object { $_.Name -eq $appx.DisplayName }).PackageFamilyName
         $PackageFamilyName
-        reg add "$store_reg\Deprovisioned\$PackageFamilyName" /f >$null 2>$null
-        dism /online /remove-provisionedappxpackage /packagename:$($appx.PackageName) >$null 2>$null
+        Set-ItemProperty -Path "$store_reg\Deprovisioned\$PackageFamilyName" -Force >$null 2>$null
+        cmd /c "dism /online /remove-provisionedappxpackage /packagename:$($appx.PackageName) >nul 2>nul"
     }
     foreach ($appx in $($appxpackage | Where-Object { $_.PackageFullName -like "*$choice*" })) {
         $inbox = (Get-ItemProperty "$store\InboxApplications\*$($appx.Name)*").Path.PSChildName
@@ -75,16 +75,16 @@ foreach ($choice in $remove_appx) {
         $PackageFullName = $appx.PackageFullName
         $PackageFullName
         foreach ($app in $inbox) {
-            reg delete "$store_reg\InboxApplications\$app" /f >$null 2>$null
+            Remove-ItemProperty -Path "$store_reg\InboxApplications\$app" -Force >$null 2>$null
         }
-        reg add "$store_reg\Deprovisioned\$PackageFamilyName" /f >$null 2>$null
+        Set-ItemProperty -Path "$store_reg\Deprovisioned\$PackageFamilyName" -Force >$null 2>$null
         foreach ($sid in $users) {
-            reg add "$store_reg\EndOfLife\$sid\$PackageFullName" /f >$null 2>$null
+            Set-ItemProperty -Path "$store_reg\EndOfLife\$sid\$PackageFullName" -Force >$null 2>$null
         }
         dism /online /set-nonremovableapppolicy /packagefamily:$PackageFamilyName /nonremovable:0 >$null 2>$null
         remove-appxpackage -package "$PackageFullName" -AllUsers >$null 2>&1
         foreach ($sid in $users) {
-            reg delete "$store_reg\EndOfLife\$sid\$PackageFullName" /f >$null 2>$null
+            Remove-ItemProperty -Path "$store_reg\EndOfLife\$sid\$PackageFullName" -Force >$null 2>$null
         }
     }
 }
@@ -110,45 +110,85 @@ foreach ($s in $setup) {
     catch {
     }
 }
-## prevent latest cumulative update (LCU) failing due to non-matching EndOfLife Edge entries
-foreach ($i in $remove_appx) {
-    Get-ChildItem "$store\EndOfLife" -Recurse -ErrorAction SilentlyContinue | Where-Object { $_ -like "*${i}*" } | foreach {
-        reg delete "$($_.Name)" /f >$null 2>$null
-    }
-    Get-ChildItem "$store\Deleted\EndOfLife" -Recurse -ErrorAction SilentlyContinue | Where-Object { $_ -like "*${i}*" } | foreach {
-        reg delete "$($_.Name)" /f >$null 2>$null
-    }
-}
-## extra cleanup
-$desktop = [Environment]::GetFolderPath('Desktop')
-$appdata = [Environment]::GetFolderPath('ApplicationData')
-Remove-Item "$appdata\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Tombstones\Microsoft Edge.lnk" -Force -ErrorAction SilentlyContinue
-Remove-Item "$appdata\Microsoft\Internet Explorer\Quick Launch\Microsoft Edge.lnk" -Force -ErrorAction SilentlyContinue
-Remove-Item "$desktop\Microsoft Edge.lnk" -Force -ErrorAction SilentlyContinue
+## cleanup
+$desktop = $([Environment]::GetFolderPath('Desktop'))
+$appdata = $([Environment]::GetFolderPath('ApplicationData'))
+Remove-Item -Path "$appdata\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Tombstones\Microsoft Edge.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$appdata\Microsoft\Internet Explorer\Quick Launch\Microsoft Edge.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$desktop\Microsoft Edge.lnk" -Force -ErrorAction SilentlyContinue
 
 ## add OpenWebSearch to redirect microsoft-edge: anti-competitive links to the default browser
 $IFEO = 'HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options'
-$MSEP = ($env:ProgramFiles, ${env:ProgramFiles(x86)})[[Environment]::Is64BitOperatingSystem] + '\Microsoft\Edge\Application'
+$MSEP = ($env:ProgramFiles,${env:ProgramFiles(x86)})[[Environment]::Is64BitOperatingSystem] + '\Microsoft\Edge\Application'
 $MIN = ('--headless','--width 1 --height 1')[([environment]::OSVersion.Version.Build) -gt 25179]
 $CMD = "$env:systemroot\system32\conhost.exe $MIN" # AveYo: minimize prompt - see Terminal issue #13914
-reg add HKCR\microsoft-edge /f /ve /d URL:microsoft-edge >$null
-reg add HKCR\microsoft-edge /f /v "URL Protocol" /d "" >$null
-reg add HKCR\microsoft-edge /f /v NoOpenWith /d "" >$null
-reg add HKCR\microsoft-edge\shell\open\command /f /ve /d "$DIR\ie_to_edge_stub.exe %1" >$null
-reg add HKCR\MSEdgeHTM /f /v NoOpenWith /d "" >$null
-reg add HKCR\MSEdgeHTM\shell\open\command /f /ve /d "$DIR\ie_to_edge_stub.exe %1" >$null
-reg add "$IFEO\ie_to_edge_stub.exe" /f /v UseFilter /d 1 /t reg_dword >$null
-reg add "$IFEO\ie_to_edge_stub.exe\0" /f /v FilterFullPath /d "$DIR\ie_to_edge_stub.exe" >$null
-reg add "$IFEO\ie_to_edge_stub.exe\0" /f /v Debugger /d "$CMD $DIR\OpenWebSearch.cmd" >$null
-reg add "$IFEO\msedge.exe" /f /v UseFilter /d 1 /t reg_dword >$null
-reg add "$IFEO\msedge.exe\0" /f /v FilterFullPath /d "$MSEP\msedge.exe" >$null
-reg add "$IFEO\msedge.exe\0" /f /v Debugger /d "$CMD $DIR\OpenWebSearch.cmd" >$null
+Set-ItemProperty -Path HKCR\microsoft-edge -Force -Name '(default)' -Value 'URL:microsoft-edge' >$null
+Set-ItemProperty -Path HKCR\microsoft-edge -Force -Name 'URL Protocol' -Value '' >$null
+Set-ItemProperty -Path HKCR\microsoft-edge -Force -Name NoOpenWith -Value '' >$null
+Set-ItemProperty -Path 'HKCR\microsoft-edge\shell\open\command' -Force -Name '(default)' -Value "$DIR\ie_to_edge_stub.exe %1" >$null
+Set-ItemProperty -Path HKCR\MSEdgeHTM -Force -Name NoOpenWith -Value '' >$null
+Set-ItemProperty -Path 'HKCR\MSEdgeHTM\shell\open\command' -Force -Name '(default)' -Value "$DIR\ie_to_edge_stub.exe %1" >$null
+Set-ItemProperty -Path "$IFEO\ie_to_edge_stub.exe" -Force -Name UseFilter -Value 1 -Type DWord >$null
+Set-ItemProperty -Path "$IFEO\ie_to_edge_stub.exe\0" -Force -Name FilterFullPath -Value "$DIR\ie_to_edge_stub.exe" >$null
+Set-ItemProperty -Path "$IFEO\ie_to_edge_stub.exe\0" -Force -Name Debugger -Value "$CMD $DIR\OpenWebSearch.cmd" >$null
+Set-ItemProperty -Path "$IFEO\msedge.exe" -Force -Name UseFilter -Value 1 -Type DWord >$null
+Set-ItemProperty -Path "$IFEO\msedge.exe\0" -Force -Name FilterFullPath -Value "$MSEP\msedge.exe" >$null
+Set-ItemProperty -Path "$IFEO\msedge.exe\0" -Force -Name Debugger -Value "$CMD $DIR\OpenWebSearch.cmd" >$null
 
-[io.file]::WriteAllText("$DIR\OpenWebSearch.cmd", $OpenWebSearch) >$null
-## cleanup
-$cleanup = Get-ItemProperty 'Registry::HKEY_Users\S-1-5-21*\Volatile*' -Name Edge_Removal -ErrorAction SilentlyContinue
-if ($cleanup) {
-    Remove-ItemProperty $cleanup.PSPath Edge_Removal -Force -ErrorAction SilentlyContinue
-}
+$OpenWebSearch = @'
+@title OpenWebSearch Redux & echo off & set ?= open start menu web search, widgets links or help in your chosen browser
+for /f %%E in ('"prompt $E$S& for %%e in (1) do rem"') do echo;%%E[2t 2>nul & rem AveYo: minimize prompt
+call :reg_var "HKCU\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice" ProgID ProgID
+if /i "%ProgID%" equ "MSEdgeHTM" echo;Default browser is set to Edge! Change it or remove OpenWebSearch script. & pause & exit /b
+call :reg_var "HKCR\%ProgID%\shell\open\command" "" Browser
+set Choice=& for %%. in (%Browser%) do if not defined Choice set "Choice=%%~."
+call :reg_var "HKCR\MSEdgeMHT\shell\open\command" "" FallBack
+set "Edge=" & for %%. in (%FallBack%) do if not defined Edge set "Edge=%%~."
+set "URI=" & set "URL=" & set "NOOP=" & set "PassTrough=%Edge:msedge=edge%"
+set "CLI=%CMDCMDLINE:"=``% "
+if defined CLI set "CLI=%CLI:*ie_to_edge_stub.exe`` =%"
+if defined CLI set "CLI=%CLI:*ie_to_edge_stub.exe =%"
+if defined CLI set "CLI=%CLI:*msedge.exe`` =%"
+if defined CLI set "CLI=%CLI:*msedge.exe =%"
+set "FIX=%CLI:~-1%"
+if defined CLI if "%FIX%"==" " set "CLI=%CLI:~0,-1%"
+if defined CLI set "RED=%CLI:microsoft-edge=%"
+if defined CLI set "URL=%CLI:http=%"
+if defined CLI set "ARG=%CLI:``="%"
+if "%CLI%" equ "%RED%" (set NOOP=1) else if "%CLI%" equ "%URL%" (set NOOP=1)
+if defined NOOP if exist "%PassTrough%" start "" "%PassTrough%" %ARG%
+if defined NOOP exit /b
+set "URL=%CLI:*microsoft-edge=%"
+set "URL=http%URL:*http=%"
+set "FIX=%URL:~-2%"
+if defined URL if "%FIX%"=="``" set "URL=%URL:~0,-2%"
+call :dec_url
+start "" "%Choice%" "%URL%"
+exit
+
+:reg_var [USAGE] call :reg_var "HKCU\Volatile Environment" value-or-"" variable [extra options]
+set {var}=& set {reg}=reg query "%~1" /v %2 /z /se "," /f /e& if %2=="" set {reg}=reg query "%~1" /ve /z /se "," /f /e
+for /f "skip=2 tokens=* delims=" %%V in ('%{reg}% %4 %5 %6 %7 %8 %9 2^>nul') do set "{var}=%%V"
+if defined %~3 (set "%~3=%{var}:*REG_=%" & set "%~3=%{var}%")
+set "{var}=" & set "{reg}=" & exit /b
+
+:dec_url
+set "CLI=%URL:%%7E=~%"
+set "CLI=%CLI:%%26=&%"
+set "CLI=%CLI:%%2F=/%"
+set "CLI=%CLI:%%2E=.%"
+set "CLI=%CLI:%%3D==%"
+set "CLI=%CLI:%%3F=?%"
+set "CLI=%CLI:%%25=%%"
+set "CLI=%CLI:%%23=#%"
+set "CLI=%CLI:%%25=%%"
+set "URL=%CLI%"
+if /i "%URL%" neq "%CLI%" goto :dec_url
+exit /b
+
+:EOF
+'@
+
+$OpenWebSearch | Out-File "$DIR\OpenWebSearch.cmd" -Encoding ASCII -Force
 
 Write-Host -NoNewline -ForegroundColor Green -BackgroundColor Black "`n EDGE REMOVED!"
