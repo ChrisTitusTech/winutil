6b9be0920484d5fc7a5c35692e405dcadaa13ada
Chris Titus <contact@christitus.com>
2023-08-08 17:51:49 -0500

    winget fix

--- functions/private/Install-WinUtilWinget.ps1
+++ functions/private/Install-WinUtilWinget.ps1
@@ -38,62 +38,8 @@ function Install-WinUtilWinget {
         }
 
         Write-Host "Running Alternative Installer and Direct Installing"
-        $ErrorActionPreference = "Stop"
-        $apiLatestUrl = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-        # Hide the progress bar of Invoke-WebRequest
-        $oldProgressPreference = $ProgressPreference
-        $ProgressPreference = 'SilentlyContinue'
-
-        $desktopAppInstaller = @{
-        fileName = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-        url      = $(((Invoke-WebRequest $apiLatestUrl -UseBasicParsing | ConvertFrom-Json).assets | Where-Object { $_.name -match '^Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle$' }).browser_download_url)
-        hash     = $(Get-LatestHash)
-        }
-
-        $vcLibsUwp = @{
-        fileName = 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
-        url      = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
-        hash     = '6602159c341bafea747d0edf15669ac72df8817299fbfaa90469909e06794256'
-        }
-        $uiLibs = @{
-            nupkg = @{
-                fileName = 'microsoft.ui.xaml.2.7.0.nupkg'
-                url = 'https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.0'
-                hash = "422FD24B231E87A842C4DAEABC6A335112E0D35B86FAC91F5CE7CF327E36A591"
-            }
-            uwp = @{
-                fileName = 'Microsoft.UI.Xaml.2.7.appx'
-            }
-        }
-        $uiLibs.uwp.file = $PWD.Path + '\' + $uiLibs.uwp.fileName
-        $uiLibs.uwp.zipPath = '*/x64/*/' + $uiLibs.uwp.fileName
-
-        $dependencies = @($desktopAppInstaller, $vcLibsUwp, $uiLibs.nupkg)
-
-        foreach ($dependency in $dependencies) {
-        $dependency.file = $dependency.fileName
-        Invoke-WebRequest $dependency.url -OutFile $dependency.file
-        }
-
-        $uiLibs.nupkg.file = $PSScriptRoot + '\' + $uiLibs.nupkg.fileName
-        Add-Type -Assembly System.IO.Compression.FileSystem
-        $uiLibs.nupkg.zip = [IO.Compression.ZipFile]::OpenRead($uiLibs.nupkg.file)
-        $uiLibs.nupkg.zipUwp = $uiLibs.nupkg.zip.Entries | Where-Object { $_.FullName -like $uiLibs.uwp.zipPath }
-        [System.IO.Compression.ZipFileExtensions]::ExtractToFile($uiLibs.nupkg.zipUwp, $uiLibs.uwp.file, $true)
-        $uiLibs.nupkg.zip.Dispose()
-
-        Add-AppxPackage -Path $desktopAppInstaller.file -DependencyPath $vcLibsUwp.file,$uiLibs.uwp.file
-
-        Remove-Item $desktopAppInstaller.file
-        Remove-Item $vcLibsUwp.file
-        Remove-Item $uiLibs.nupkg.file
-        Remove-Item $uiLibs.uwp.file
-        Write-Host "WinGet installed!" -ForegroundColor Green
-        $ProgressPreference = $oldProgressPreference
-        Update-EnvironmentVariables
-
+        Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/winget.ps1 | iex"
+        
         Write-Host "Winget Installed"
     }
     Catch{
