7f5fe84efb04d85b3fdc76b773dbb6b5b00345c2
ChrisTitusTech <ChrisTitusTech@users.noreply.github.com>
2023-09-13 21:03:37 +0000

    Compile Winutil

--- winutil.ps1
+++ winutil.ps1
@@ -5624,7 +5624,10 @@ $sync.configs.tweaks = '{
         "
     ],
     "UndoScript": [
-      "winget install Microsoft.OneDrive"
+      "
+      Write-Host \"Install OneDrive\"
+      Start-Process -FilePath winget -ArgumentList \"install -e --accept-source-agreements --accept-package-agreements --silent Microsoft.OneDrive \" -NoNewWindow -Wait
+      "
     ]
   },
   "WPFMiscTweaksDisableNotifications": {
