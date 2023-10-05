beaf98b14183eb03a7a6dfd300297d864babc8a9
Carterpersall <carterpersall@gmail.com>
2023-10-05 14:24:50 -0500

    Description, Progress Bar, and nuke step 10
    - Added a full description of exactly what the function does
    - Replaced `Write-Host`s with multiple progress bars
      - Might've added too many, but I didn't want to add one for just a few of them as I want the user to be able to see all tasks that have been performed, just like with `Write-Host`
    - Removed Step 10
      - Only applies to Windows 8

--- functions/public/Invoke-WPFFixesUpdate.ps1
+++ functions/public/Invoke-WPFFixesUpdate.ps1
@@ -5,31 +5,71 @@ function Invoke-WPFFixesUpdate {
     .SYNOPSIS
         Performs various tasks in an attempt to repair Windows Update
 
+    .DESCRIPTION
+        1. Stops Windows Update Services
+        2. Remove the QMGR Data file, which stores BITS jobs
+        3. Renames the DataStore, Download, and CatRoot2 folders
+            DataStore - Contains the Windows Update History and Log Files
+            Download - Contains downloaded Windows Update files
+            CatRoot2 - Contains the Signatures for Windows Update Packages
+        4. Deletes the Windows Update Log
+        5. Resets the Security Descriptors on the Windows Update Services
+        6. Reregisters the BITS and Windows Update DLLs
+        7. Removes the WSUS client settings
+        8. Resets WinSock
+        9. Gets and deletes all BITS jobs
+        10. Starts Windows Update Services
+        11. Forces Windows Update to check for updates
+
     #>
 
-    # Reset Windows Update Script - reregister dlls, services, and remove registry entries
-    Write-Host "1. Stopping Windows Update Services..."
+    # Stop the Windows Update Services
+    Write-Progress -Id 0 -Activity "Repairing Windows Update" -Status "Stopping Windows Update Services..." -PercentComplete 0
+    # Wait for the first progress bar to show, otherwise the second one won't show
+    Start-Sleep -Milliseconds 200
+    Write-Progress -Id 1 -ParentId 0 -Activity "Stopping Services" -Status "Stopping BITS..." -PercentComplete 0
     Stop-Service -Name BITS
+    Write-Progress -Id 1 -ParentId 0 -Activity "Stopping Services" -Status "Stopping wuauserv..." -PercentComplete 20
     Stop-Service -Name wuauserv
+    Write-Progress -Id 1 -ParentId 0 -Activity "Stopping Services" -Status "Stopping appidsvc..." -PercentComplete 40
     Stop-Service -Name appidsvc
+    Write-Progress -Id 1 -ParentId 0 -Activity "Stopping Services" -Status "Stopping cryptsvc..." -PercentComplete 60
     Stop-Service -Name cryptsvc
+    Write-Progress -Id 1 -ParentId 0 -Activity "Stopping Services" -Status "Completed" -PercentComplete 100
 
-    Write-Host "2. Remove QMGR Data file..."
+
+    # Remove the QMGR Data file
+    Write-Progress -Id 0 -Activity "Repairing Windows Update" -Status "Renaming/Removing Files..." -PercentComplete 20
+    Write-Progress -Id 2 -ParentId 0 -Activity "Renaming/Removing Files" -Status "Removing QMGR Data files..." -PercentComplete 0
     Remove-Item "$env:allusersprofile\Application Data\Microsoft\Network\Downloader\qmgr*.dat" -ErrorAction SilentlyContinue
 
-    Write-Host "3. Renaming the Software Distribution and CatRoot Folder..."
-    Rename-Item $env:systemroot\SoftwareDistribution SoftwareDistribution.bak -ErrorAction SilentlyContinue
+
+    # Rename the Windows Update Log, Download, and Signature Folders
+    Write-Progress -Id 2 -ParentId 0 -Activity "Renaming/Removing Files" -Status "Renaming the Windows Update Log, Download, and Signature Folder..." -PercentComplete 20
+    Rename-Item $env:systemroot\SoftwareDistribution\DataStore DataStore.bak -ErrorAction SilentlyContinue
+    Rename-Item $env:systemroot\SoftwareDistribution\Download Download.bak -ErrorAction SilentlyContinue
     Rename-Item $env:systemroot\System32\Catroot2 catroot2.bak -ErrorAction SilentlyContinue
 
-    Write-Host "4. Removing old Windows Update log..."
+
+    # Delete the legacy Windows Update Log
+    Write-Progress -Id 2 -ParentId 0 -Activity "Renaming/Removing Files" -Status "Removing the old Windows Update log..." -PercentComplete 80
     Remove-Item $env:systemroot\WindowsUpdate.log -ErrorAction SilentlyContinue
+    Write-Progress -Id 2 -ParentId 0 -Activity "Renaming/Removing Files" -Status "Completed" -PercentComplete 100
 
-    Write-Host "5. Resetting the Windows Update Services to default settings..."
+
+    # Reset the Security Descriptors on the Windows Update Services
+    Write-Progress -Id 0 -Activity "Repairing Windows Update" -Status "Resetting the WU Service Security Descriptors..." -PercentComplete 25
+    Write-Progress -Id 3 -ParentId 0 -Activity "Resetting the WU Service Security Descriptors" -Status "Resetting the BITS Security Descriptor..." -PercentComplete 0
     Start-Process -NoNewWindow -FilePath "sc.exe" -ArgumentList "sdset", "bits", "D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)"
+    Write-Progress -Id 3 -ParentId 0 -Activity "Resetting the WU Service Security Descriptors" -Status "Resetting the wuauserv Security Descriptor..." -PercentComplete 50
     Start-Process -NoNewWindow -FilePath "sc.exe" -ArgumentList "sdset", "wuauserv", "D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)"
-    Set-Location $env:systemroot\system32
+    Write-Progress -Id 3 -ParentId 0 -Activity "Resetting the WU Service Security Descriptors" -Status "Completed" -PercentComplete 100
 
-    Write-Host "6. Registering some DLLs..."
+
+    # Reregister the BITS and Windows Update DLLs
+    Write-Progress -Id 0 -Activity "Repairing Windows Update" -Status "Reregistering DLLs..." -PercentComplete 40
+    Set-Location $env:systemroot\system32
+    $i = 0
     $DLLs = @(
         "atl.dll", "urlmon.dll", "mshtml.dll", "shdocvw.dll", "browseui.dll",
         "jscript.dll", "vbscript.dll", "scrrun.dll", "msxml.dll", "msxml3.dll",
@@ -40,43 +80,60 @@ function Invoke-WPFFixesUpdate {
         "wuweb.dll", "qmgr.dll", "qmgrprxy.dll", "wucltux.dll", "muweb.dll", "wuwebv.dll"
     )
     foreach ($dll in $DLLs) {
+        Write-Progress -Id 4 -ParentId 0 -Activity "Reregistering DLLs" -Status "Registering $dll..." -PercentComplete ($i / $DLLs.Count * 100)
+        $i++
+
         Start-Process -NoNewWindow -FilePath "regsvr32.exe" -ArgumentList "/s", $dll
     }
+    Write-Progress -Id 4 -ParentId 0 -Activity "Reregistering DLLs" -Status "Completed" -PercentComplete 100
 
-    Write-Host "7) Removing WSUS client settings..."
+
+    # Remove the WSUS client settings
     if (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate") {
+        Write-Progress -Id 0 -Activity "Repairing Windows Update" -Status "Removing WSUS client settings..." -PercentComplete 60
+        Write-Progress -Id 5 -ParentId 0 -Activity "Removing WSUS client settings" -PercentComplete 0
         Start-Process -NoNewWindow -FilePath "REG" -ArgumentList "DELETE", "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate", "/v", "AccountDomainSid", "/f"
         Start-Process -NoNewWindow -FilePath "REG" -ArgumentList "DELETE", "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate", "/v", "PingID", "/f"
         Start-Process -NoNewWindow -FilePath "REG" -ArgumentList "DELETE", "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate", "/v", "SusClientId", "/f"
+        Write-Progress -Id 5 -ParentId 0 -Activity "Removing WSUS client settings" -Status "Completed" -PercentComplete 100
     }
 
-    Write-Host "8) Resetting the WinSock..."
+
+    # Reset WinSock
+    Write-Progress -Id 0 -Activity "Repairing Windows Update" -Status "Resetting WinSock..." -PercentComplete 65
+    Write-Progress -Id 6 -ParentId 0 -Activity "Resetting WinSock" -Status "Resetting WinSock..." -PercentComplete 0
     Start-Process -NoNewWindow -FilePath "netsh" -ArgumentList "winsock", "reset"
     Start-Process -NoNewWindow -FilePath "netsh" -ArgumentList "winhttp", "reset", "proxy"
     Start-Process -NoNewWindow -FilePath "netsh" -ArgumentList "int", "ip", "reset"
+    Write-Progress -Id 6 -ParentId 0 -Activity "Resetting WinSock" -Status "Completed" -PercentComplete 100
 
-    Write-Host "9) Delete all BITS jobs..."
+
+    # Get and delete all BITS jobs
+    Write-Progress -Id 0 -Activity "Repairing Windows Update" -Status "Deleting BITS jobs..." -PercentComplete 75
+    Write-Progress -Id 7 -ParentId 0 -Activity "Deleting BITS jobs" -Status "Deleting BITS jobs..." -PercentComplete 0
     Get-BitsTransfer | Remove-BitsTransfer
+    Write-Progress -Id 7 -ParentId 0 -Activity "Deleting BITS jobs" -Status "Completed" -PercentComplete 100
 
-    Write-Host "10) Attempting to install the Windows Update Agent..."
-    If ([System.Environment]::Is64BitOperatingSystem) {
-        Start-Process -NoNewWindow -FilePath "wusa" -ArgumentList "Windows8-RT-KB2937636-x64", "/quiet"
-    }
-    else {
-        Start-Process -NoNewWindow -FilePath "wusa" -ArgumentList "Windows8-RT-KB2937636-x86", "/quiet"
-    }
 
-    Write-Host "11) Starting Windows Update Services..."
-    Start-Service -Name BITS
-    Start-Service -Name wuauserv
-    Start-Service -Name appidsvc
-    Start-Service -Name cryptsvc
+    # Restart the Windows Update Services
+    Write-Progress -Id 0 -Activity "Repairing Windows Update" -Status "Starting Windows Update Services..." -PercentComplete 90
+    Write-Progress -Id 8 -ParentId 0 -Activity "Starting Windows Update Services" -Status "Starting BITS..." -PercentComplete 0
+    Get-Service BITS | Set-Service -StartupType Manual -PassThru | Start-Service
+    Write-Progress -Id 8 -ParentId 0 -Activity "Starting Windows Update Services" -Status "Starting wuauserv..." -PercentComplete 25
+    Get-Service wuauserv | Set-Service -StartupType Manual -PassThru | Start-Service
+    Write-Progress -Id 8 -ParentId 0 -Activity "Starting Windows Update Services" -Status "Starting appidsvc..." -PercentComplete 50
+    Get-Service appidsvc | Set-Service -StartupType Manual -PassThru | Start-Service
+    Write-Progress -Id 8 -ParentId 0 -Activity "Starting Windows Update Services" -Status "Starting cryptsvc..." -PercentComplete 75
+    Get-Service cryptsvc | Set-Service -StartupType Automatic -PassThru | Start-Service
+    Write-Progress -Id 8 -ParentId 0 -Activity "Starting Windows Update Services" -Status "Completed" -PercentComplete 100
 
-    Write-Host "12) Forcing discovery..."
+
+    # Force Windows Update to check for updates
+    Write-Progress -Id 0 -Activity "Repairing Windows Update" -Status "Forcing discovery..." -PercentComplete 95
+    Write-Progress -Id 9 -ParentId 0 -Activity "Forcing discovery" -Status "Forcing discovery..." -PercentComplete 0
     Start-Process -NoNewWindow -FilePath "wuauclt" -ArgumentList "/resetauthorization", "/detectnow"
-
-
-    Write-Host "Process complete. Please reboot your computer."
+    Write-Progress -Id 9 -ParentId 0 -Activity "Forcing discovery" -Status "Completed" -PercentComplete 100
+    Write-Progress -Id 0 -Activity "Repairing Windows Update" -Status "Completed" -PercentComplete 100
 
     $ButtonType = [System.Windows.MessageBoxButton]::OK
     $MessageboxTitle = "Reset Windows Update "
@@ -87,4 +144,7 @@ function Invoke-WPFFixesUpdate {
     Write-Host "==============================================="
     Write-Host "-- Reset All Windows Update Settings to Stock -"
     Write-Host "==============================================="
+
+    # Remove the progress bar
+    Write-Progress -Id 0 -Activity "Repairing Windows Update" -Completed
 }
