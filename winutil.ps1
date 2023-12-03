eca7e02328ea8e021a54a0497174b47af0db9d81
AshlynOrSomethin <31773733+AshlynOrSomethin@users.noreply.github.com>
2023-12-03 17:26:58 -0600

    Add 2 Applications (#1222)
    * Add 3 Applications
    
    Spacedrive (An Alpha stage file manager)
    WSL Manager
    
    * Test somethin
    
    * Update winutil.ps1
    
    * Update inputXML.xaml
    
    * Update winutil.ps1

--- winutil.ps1
+++ winutil.ps1
@@ -4266,6 +4266,7 @@ $inputXML = '<Window x:Class="WinUtility.MainWindow"
                                 <CheckBox Name="WPFInstallsandboxie" Content="Sandboxie Plus" Margin="5,0"/>
                                 <CheckBox Name="WPFInstallshell" Content="Shell (Expanded Context Menu)" Margin="5,0"/>
                                 <CheckBox Name="WPFInstallsdio" Content="Snappy Driver Installer Origin" Margin="5,0"/>
+                                <CheckBox Name="WPFInstallspacedrive" Content="Spacedrive File Manager" Margin="5,0"/>
                                 <CheckBox Name="WPFInstallsuperf4" Content="SuperF4" Margin="5,0"/>
                                 <CheckBox Name="WPFInstallteamviewer" Content="TeamViewer" Margin="5,0"/>
                                 <CheckBox Name="WPFInstallttaskbar" Content="Translucent Taskbar" Margin="5,0"/>
@@ -4275,6 +4276,7 @@ $inputXML = '<Window x:Class="WinUtility.MainWindow"
                                 <CheckBox Name="WPFInstallwingetui" Content="WingetUI" Margin="5,0"/>
                                 <CheckBox Name="WPFInstallwiztree" Content="WizTree" Margin="5,0"/>
                                 <CheckBox Name="WPFInstallwinrar" Content="WinRAR" Margin="5,0"/>
+                                <CheckBox Name="WPFInstallwslmanager" Content="WSL Manager" Margin="5,0"/>
                                 <CheckBox Name="WPFInstallxdm" Content="Xtreme Download Manager" Margin="5,0"/>
                                 <CheckBox Name="WPFInstallzerotierone" Content="ZeroTier One" Margin="5,0"/>
                             </StackPanel>
@@ -5240,6 +5242,10 @@ $sync.configs.applications = '{
 		"winget": "StefanSundin.Superf4",
 		"choco": "superf4"
 	},
+	"WPFInstallspacedrive": {
+		"winget": "spacedrive.Spacedrive",
+		"choco": "na"
+	},
 	"WPFInstalltcpview": {
 		"winget": "Microsoft.Sysinternals.TCPView",
 		"choco": "tcpview"
@@ -5355,6 +5361,10 @@ $sync.configs.applications = '{
 	"WPFInstallwinrar": {
 		"winget": "RARLab.WinRAR",
 		"choco": "winrar"
+	},
+	"WPFIntallwslmanager": {
+		"Winget": "Bostrot.WSLManager",
+		"choco": "wsl2-distro-manager"
 	},
 	"WPFInstallwinscp": {
 		"winget": "WinSCP.WinSCP",
