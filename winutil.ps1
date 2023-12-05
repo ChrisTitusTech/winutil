ec22b617775632800c0fab26696afc079d834b12
ChrisTitusTech <ChrisTitusTech@users.noreply.github.com>
2023-12-05 03:42:33 +0000

    Compile Winutil

--- winutil.ps1
+++ winutil.ps1
@@ -10,7 +10,7 @@
     Author         : Chris Titus @christitustech
     Runspace Author: @DeveloperDurp
     GitHub         : https://github.com/ChrisTitusTech
-    Version        : 23.12.03
+    Version        : 23.12.05
 #>
 
 Start-Transcript $ENV:TEMP\Winutil.log -Append
@@ -22,7 +22,7 @@ Add-Type -AssemblyName System.Windows.Forms
 # Variable to sync between runspaces
 $sync = [Hashtable]::Synchronized(@{})
 $sync.PSScriptRoot = $PSScriptRoot
-$sync.version = "23.12.03"
+$sync.version = "23.12.05"
 $sync.configs = @{}
 $sync.ProcessRunning = $false
 
@@ -4683,7 +4683,7 @@ $sync.configs.applications = '{
 		"choco": "googlechrome"
 	},
 	"WPFInstallcopyq": {
-		"winget": "copyq",
+		"winget": "hluk.CopyQ",
 		"choco": "copyq"
 	},
 	"WPFInstallchromium": {
