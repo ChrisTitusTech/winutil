33279513292e75291bee44dc10be5342b2df820d
KonTy <KonTy@github.com>
2024-01-04 12:06:04 -0800

    Adding new Get-LocalizedYesNo based on choice.exe which is faster and more reliable, thank you @dtm-r for implementing it and testing it on English, German, Dutch, French, Italian, Spansich and Russian. Incredible work by @dtm-r, all cridit and props go to him. See this thread for details https://github.com/ChrisTitusTech/winutil/issues/1324

--- winutil.ps1
+++ winutil.ps1
@@ -121,6 +121,41 @@ function Copy-Files {
 function Get-LocalizedYesNo {
     <#
     .SYNOPSIS
+    This function runs choice.exe and captures its output to extract yes no in a localized Windows 
+    
+    .DESCRIPTION
+    The function retrieves the output of the command 'cmd /c "choice <nul 2>nul"' and converts the default output for Yes and No
+    in the localized format, such as "Yes=<first character>, No=<second character>".
+    
+    .EXAMPLE
+    $yesNoArray = Get-LocalizedYesNo
+    Write-Host "Yes=$($yesNoArray[0]), No=$($yesNoArray[1])"
+    #>
+  
+    # Run choice and capture its options as output
+    # The output shows the options for Yes and No as "[Y,N]?" in the (partitially) localized format.
+    # eg. English: [Y,N]?
+    # Dutch: [Y,N]?
+    # German: [J,N]?
+    # French: [O,N]?
+    # Spanish: [S,N]?
+    # Italian: [S,N]?
+    # Russian: [Y,N]?
+    
+    $line = cmd /c "choice <nul 2>nul"
+    $charactersArray = @()
+    $regexPattern = '([a-zA-Z])'
+    $charactersArray = [regex]::Matches($line, $regexPattern) | ForEach-Object { $_.Groups[1].Value }
+
+    Write-Debug "According to takeown.exe local Yes is $charactersArray[0]"
+    # Return the array of characters
+    return $charactersArray
+  }
+  
+
+function Get-LocalizedYesNoTakeown {
+    <#
+    .SYNOPSIS
     This function runs takeown.exe and captures its output to extract yes no in a localized Windows 
     
     .DESCRIPTION
@@ -3713,7 +3748,7 @@ $inputXML = '<Window x:Class="WinUtility.MainWindow"
         Background="{MainBackgroundColor}"
         WindowStartupLocation="CenterScreen"
         WindowStyle="None"
-        Title="Chris Titus Tech''s Windows Utility" Height="800" Width="1200">
+        Title="Chris Titus Tech''s Windows Utility" Height="800" Width="1280">
     <WindowChrome.WindowChrome>
         <WindowChrome CaptionHeight="0" CornerRadius="10"/>
     </WindowChrome.WindowChrome>
@@ -4596,7 +4631,7 @@ $inputXML = '<Window x:Class="WinUtility.MainWindow"
                 <Grid Width="Auto" Height="Auto">
                     <Grid.ColumnDefinitions>
                         <ColumnDefinition Width="*"/>
-                        <ColumnDefinition Width="4*"/>
+                        <ColumnDefinition Width="3*"/>
                     </Grid.ColumnDefinitions>
                     <Grid.RowDefinitions>
                         <RowDefinition Height="*" />
