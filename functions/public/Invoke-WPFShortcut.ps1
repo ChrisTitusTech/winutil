2b4610531fa6610d09b589d1c417b0b6fee329fd
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-03-31 00:38:57 +0100

    Update Invoke-WPFShortcut.ps1

--- functions/public/Invoke-WPFShortcut.ps1
+++ functions/public/Invoke-WPFShortcut.ps1
@@ -22,7 +22,7 @@ function Invoke-WPFShortcut {
             "WinUtil" {
                 $SourceExe = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
                 $IRM = 'irm https://christitus.com/win | iex'
-                $Powershell = '-ExecutionPolicy Bypass -Command "Start-Process powershell.exe -verb runas -ArgumentList'
+                $Powershell = '-ExecutionPolicy Bypass -c' # old: '-ExecutionPolicy Bypass -Command "Start-Process powershell.exe -verb runas -ArgumentList'
                 $ArgumentsToSourceExe = "$powershell '$IRM'"
                 $DestinationName = "WinUtil.lnk"
 
