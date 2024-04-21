6b54adf50934ee2a9f5b5ea7f2606a95dc410f1a
ChrisTitusTech <ChrisTitusTech@users.noreply.github.com>
2024-04-21 00:00:46 +0000

    Compile Winutil

--- winutil.ps1
+++ winutil.ps1
@@ -10,7 +10,7 @@
     Author         : Chris Titus @christitustech
     Runspace Author: @DeveloperDurp
     GitHub         : https://github.com/ChrisTitusTech
-    Version        : 24.04.20
+    Version        : 24.04.21
 #>
 param (
     [switch]$Debug,
@@ -47,7 +47,7 @@ Add-Type -AssemblyName System.Windows.Forms
 # Variable to sync between runspaces
 $sync = [Hashtable]::Synchronized(@{})
 $sync.PSScriptRoot = $PSScriptRoot
-$sync.version = "24.04.20"
+$sync.version = "24.04.21"
 $sync.configs = @{}
 $sync.ProcessRunning = $false
 
