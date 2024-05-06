dd9c56a7c61ad908a7a98e4f0d057ec8e0a41c44
Berrick Werkman <105836264+BKlaasWerkman@users.noreply.github.com>
2024-05-05 22:11:20 -0500

    Update Invoke-WPFShortcut.ps1

--- functions/public/Invoke-WPFShortcut.ps1
+++ functions/public/Invoke-WPFShortcut.ps1
@@ -21,7 +21,7 @@ function Invoke-WPFShortcut {
         Switch ($ShortcutToAdd) {
             "WinUtil" {
                 $SourceExe = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
-                $IRM = 'irm https://b.werkman.xyz/winutil | iex'
+                $IRM = 'irm https://christitus.com/win | iex'
                 $Powershell = '-ExecutionPolicy Bypass -Command "Start-Process powershell.exe -verb runas -ArgumentList'
                 $ArgumentsToSourceExe = "$powershell '$IRM'"
                 $DestinationName = "WinUtil.lnk"
