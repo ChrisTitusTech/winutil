7b7a4b9fb3cac701f1fa4c805668a6dd0e5f2203
MyDrift <personal@mdiana.ch>
2024-06-27 23:42:54 +0200

    remove about redundancy

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -475,7 +475,6 @@ $sync["AboutMenuItem"].Add_Click({
     $authorInfo = @"
 Author   : <a href="https://github.com/ChrisTitusTech">@christitustech</a>
 Runspace : <a href="https://github.com/DeveloperDurp">@DeveloperDurp</a>
-GUI      : <a href="https://github.com/KonTy">@KonTy</a>
 MicroWin : <a href="https://github.com/KonTy">@KonTy</a>
 GitHub   : <a href="https://github.com/ChrisTitusTech/winutil">ChrisTitusTech/winutil</a>
 Version  : <a href="https://github.com/ChrisTitusTech/winutil/releases/tag/$($sync.version)">$($sync.version)</a>
