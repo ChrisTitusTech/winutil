52d8e6178d5f9f46602a318055b3fa78a346d047
Marterich <47688561+Marterich@users.noreply.github.com>
2024-07-27 19:47:02 +0200

    Add docs

--- functions/private/Set-WinUtilProgressbar.ps1
+++ functions/private/Set-WinUtilProgressbar.ps1
@@ -1,9 +1,20 @@
-
 function Set-WinUtilProgressbar{
+    <#
+    .SYNOPSIS
+        This function is used to Update the Progress Bar displayed in the winutil GUI. 
+        It will be automatically hidden if the user clicks something and no process is running 
+    .PARAMETER Label
+        The Text to be overlayed onto the Progress Bar
+    .PARAMETER PERCENT
+        The percentage of the Progress Bar that should be filled (0-100) 
+    .PARAMETER Hide
+        If provided, the Progress Bar and the label will be hidden
+    #>
     param(
-        [string]$label,
-        [int]$percent,
-        $hide
+        [string]$Label,
+        [ValidateRange(0,100)]
+        [int]$Percent,
+        $Hide
     )
     if ($hide){
         $sync.form.Dispatcher.Invoke([action]{$sync.ProgressBarLabel.Visibility = "Collapsed"}) 
