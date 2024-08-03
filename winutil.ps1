e5504a6b9f7c8b23864de5420b0cf10abb3c694d
Linget0 <minecraftlover913@gmail.com>
2024-08-03 14:03:09 -0400

    Add files via upload

--- winutil.ps1
+++ winutil.ps1
@@ -11758,8 +11758,8 @@ $sync.configs.tweaks = '{
     "Order": "a029_",
     "InvokeScript": [
       "
-        Invoke-WebRequest -Uri \"https://raw.githubusercontent.com/Linget0/WPFEdgeRemoval/main/edgeremoval.bat\" -OutFile \"$ENV:TEMP\\edgeremoval.bat\"
-        Start-Process $ENV:temp\\edgeremoval.bat
+        Invoke-WebRequest -Uri \"https://raw.githubusercontent.com/Linget0/WPFEdgeRemoval/main/edgeremoval.ps1\" -OutFile \"$ENV:TEMP\\edgeremoval.ps1\"
+        Start-Process $ENV:temp\\edgeremoval.ps1
         "
     ],
     "UndoScript": [
