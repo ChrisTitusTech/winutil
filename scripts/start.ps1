75e617c16097386d19dac08bb4296b2104ed5ba2
Schooi <robloxianskp@gmail.com>
2024-08-08 23:26:59 +0300

    Update start.ps1

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -50,16 +50,21 @@ if (([Security.Principal.WindowsIdentity]::GetCurrent()).Owner.Value -ne "S-1-5-
     Write-Host "-- Scripts must be run as Administrator ---" -ForegroundColor Red
     Write-Host "-- Right-Click Start -> Terminal(Admin) ---" -ForegroundColor Red
     Write-Host "===========================================" -ForegroundColor Red
+    # Call the choice executable to ask the user if they want to elevate
     $choiceCmd = "$env:SystemRoot\System32\choice.exe /c YN /t 60 /D Y /N /M ""Would you like to restart as Administrator? (Y/N)"""
     Invoke-Expression $choiceCmd
     $choiceResult = $?
     if ($choiceResult) {
+        # Check if the build number of the operating system is 6000 or higher
+        # (Windows Vista/Server 2008 and later versions have build numbers of 6000 or higher)
         if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-        exit
+            # Re-run the current script with all the original arguments as admin
+            $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+            Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+            exit
         }
     }
+    exit
 }
 
 # Set PowerShell window title
