46d2a131e2df3380e1dee69da9dc70d8767b7122
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-08-14 14:39:01 +0200

    [MicroWin] Add try-catch block for AppX packages

--- functions/private/Invoke-WinUtilMicroWin-Helper.ps1
+++ functions/private/Invoke-WinUtilMicroWin-Helper.ps1
@@ -132,42 +132,49 @@ function Remove-ProvisionedPackages() {
         .EXAMPLE
         Remove-ProvisionedPackages
     #>
-    $appxProvisionedPackages = Get-AppxProvisionedPackage -Path "$($scratchDir)" | Where-Object    {
-            $_.PackageName -NotLike "*AppInstaller*" -AND
-            $_.PackageName -NotLike "*Store*" -and
-            $_.PackageName -NotLike "*dism*" -and
-            $_.PackageName -NotLike "*Foundation*" -and
-            $_.PackageName -NotLike "*FodMetadata*" -and
-            $_.PackageName -NotLike "*LanguageFeatures*" -and
-            $_.PackageName -NotLike "*Notepad*" -and
-            $_.PackageName -NotLike "*Printing*" -and
-            $_.PackageName -NotLike "*Wifi*" -and
-            $_.PackageName -NotLike "*Foundation*" -and
-            $_.PackageName -NotLike "*YourPhone*" -and
-            $_.PackageName -NotLike "*Xbox*" -and
-            $_.PackageName -NotLike "*WindowsTerminal*" -and
-            $_.PackageName -NotLike "*Calculator*" -and
-            $_.PackageName -NotLike "*Photos*" -and
-            $_.PackageName -NotLike "*VCLibs*" -and
-            $_.PackageName -NotLike "*Paint*" -and
-            $_.PackageName -NotLike "*Gaming*" -and
-            $_.PackageName -NotLike "*Extension*" -and
-            $_.PackageName -NotLike "*SecHealthUI*"
-
+    try
+    {
+        $appxProvisionedPackages = Get-AppxProvisionedPackage -Path "$($scratchDir)" | Where-Object {
+                $_.PackageName -NotLike "*AppInstaller*" -AND
+                $_.PackageName -NotLike "*Store*" -and
+                $_.PackageName -NotLike "*dism*" -and
+                $_.PackageName -NotLike "*Foundation*" -and
+                $_.PackageName -NotLike "*FodMetadata*" -and
+                $_.PackageName -NotLike "*LanguageFeatures*" -and
+                $_.PackageName -NotLike "*Notepad*" -and
+                $_.PackageName -NotLike "*Printing*" -and
+                $_.PackageName -NotLike "*Wifi*" -and
+                $_.PackageName -NotLike "*Foundation*" -and
+                $_.PackageName -NotLike "*YourPhone*" -and
+                $_.PackageName -NotLike "*Xbox*" -and
+                $_.PackageName -NotLike "*WindowsTerminal*" -and
+                $_.PackageName -NotLike "*Calculator*" -and
+                $_.PackageName -NotLike "*Photos*" -and
+                $_.PackageName -NotLike "*VCLibs*" -and
+                $_.PackageName -NotLike "*Paint*" -and
+                $_.PackageName -NotLike "*Gaming*" -and
+                $_.PackageName -NotLike "*Extension*" -and
+                $_.PackageName -NotLike "*SecHealthUI*"
         }
 
-    $counter = 0
-    foreach ($appx in $appxProvisionedPackages) {
-        $status = "Removing Provisioned $($appx.PackageName)"
-        Write-Progress -Activity "Removing Provisioned Apps" -Status $status -PercentComplete ($counter++/$appxProvisionedPackages.Count*100)
-        try {
-            Remove-AppxProvisionedPackage -Path "$scratchDir" -PackageName $appx.PackageName -ErrorAction SilentlyContinue
-        } catch {
-            Write-Host "Application $($appx.PackageName) could not be removed"
-            continue
+        $counter = 0
+        foreach ($appx in $appxProvisionedPackages) {
+            $status = "Removing Provisioned $($appx.PackageName)"
+            Write-Progress -Activity "Removing Provisioned Apps" -Status $status -PercentComplete ($counter++/$appxProvisionedPackages.Count*100)
+            try {
+                Remove-AppxProvisionedPackage -Path "$scratchDir" -PackageName $appx.PackageName -ErrorAction SilentlyContinue
+            } catch {
+                Write-Host "Application $($appx.PackageName) could not be removed"
+                continue
+            }
         }
+        Write-Progress -Activity "Removing Provisioned Apps" -Status "Ready" -Completed
     }
-    Write-Progress -Activity "Removing Provisioned Apps" -Status "Ready" -Completed
+    catch
+    {
+        # This can happen if getting AppX packages fails
+        Write-Host "Unable to get information about the AppX packages. MicroWin processing will continue, but AppX packages will not be processed"
+    }
 }
 
 function Copy-ToUSB([string]$fileToCopy) {
