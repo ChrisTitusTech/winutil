75b466778b4e2cfe040b1d9f810a281e30e4c61c
Marterich <47688561+Marterich@users.noreply.github.com>
2024-09-05 22:08:42 +0200

    Revert Admin Elevation

--- windev.ps1
+++ windev.ps1
@@ -12,18 +12,6 @@
     Run in Admin Powershell >  ./windev.ps1
 #>
 
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
-
-    $script = if ($MyInvocation.MyCommand.Path) { "& '" + $MyInvocation.MyCommand.Path + "'" } else { "irm 'https://github.com/ChrisTitusTech/winutil/raw/main/windev.ps1' | iex"}
-    $powershellcmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
-    $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { $powershellcmd }
-
-    Start-Process $processCmd -ArgumentList "$powershellcmd -ExecutionPolicy Bypass -NoProfile -Command $script" -Verb RunAs
-
-    break
-}
-
 # Function to fetch the latest release tag from the GitHub API
 function Get-LatestRelease {
     try {
