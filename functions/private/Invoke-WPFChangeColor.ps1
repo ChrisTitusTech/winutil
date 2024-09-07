2deb92c96f25a063471799a12f38408e825a9f59
Marterich <47688561+Marterich@users.noreply.github.com>
2024-09-07 11:19:58 +0200

    Working Example for Background

--- functions/private/Invoke-WPFChangeColor.ps1
+++ functions/private/Invoke-WPFChangeColor.ps1
@@ -1,3 +1,13 @@
 function Invoke-WPFChangeColor {
-    Write-Host "It works"
+    $random = New-Object System.Random
+    function Get-RandomColor {
+        $r = $random.Next(0, 256)
+        $g = $random.Next(0, 256)
+        $b = $random.Next(0, 256)
+        
+        # Return a new SolidColorBrush with random RGB values
+        return [Windows.Media.SolidColorBrush]::new([Windows.Media.Color]::FromRgb($r, $g, $b))
+    }
+    $newBackground = Get-RandomColor
+    $sync.Form.Resources["DMainBackgroundColor"] = $newBackground
 }
