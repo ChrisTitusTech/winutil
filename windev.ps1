d3ef94f175b8d27b90be8fc98e9cf04b6fdf341a
Chris Titus Tech <dfm.titus@gmail.com>
2024-09-10 14:18:51 -0500

    Update windev.ps1

--- windev.ps1
+++ windev.ps1
@@ -34,15 +34,15 @@ function RedirectToLatestPreRelease {
         Write-Host "Using latest Full Release"
         $url = "https://github.com/ChrisTitusTech/winutil/releases/latest/download/winutil.ps1"
     }
-    
-    $script = Invoke-RestMethod $url 
-    # Elevate Shell if necessary 
+
+    $script = Invoke-RestMethod $url
+    # Elevate Shell if necessary
     if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
         Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
 
         $powershellcmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
         $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { $powershellcmd }
-    
+
         Start-Process $processCmd -ArgumentList "$powershellcmd -ExecutionPolicy Bypass -NoProfile -Command $(Invoke-Expression $script)" -Verb RunAs
     }
     else{
