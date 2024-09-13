8244755d77476846214950a3b8d01c7504cb7b3a
MyDrift <personal@mdiana.ch>
2024-09-13 11:41:49 +0200

    undo Set-WinUtilTweaks (fix will be done in seperate PR)

---
+++ functions/private/Set-WinUtilService.ps1
@@ -0,0 +1,36 @@
+Function Set-WinUtilService {
+    <#
+
+    .SYNOPSIS
+        Changes the startup type of the given service
+
+    .PARAMETER Name
+        The name of the service to modify
+
+    .PARAMETER StartupType
+        The startup type to set the service to
+
+    .EXAMPLE
+        Set-WinUtilService -Name "HomeGroupListener" -StartupType "Manual"
+
+    #>
+    param (
+        $Name,
+        $StartupType
+    )
+    try {
+        Write-Host "Setting Service $Name to $StartupType"
+
+        # Check if the service exists
+        $service = Get-Service -Name $Name -ErrorAction Stop
+
+        # Service exists, proceed with changing properties
+        $service | Set-Service -StartupType $StartupType -ErrorAction Stop
+    } catch [System.ServiceProcess.ServiceNotFoundException] {
+        Write-Warning "Service $Name was not found"
+    } catch {
+        Write-Warning "Unable to set $Name due to unhandled exception"
+        Write-Warning $_.Exception.Message
+    }
+
+}
