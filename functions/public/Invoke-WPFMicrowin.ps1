358695d2965aeba57a2537df58c542a0cdb7dac9
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-10-02 22:58:44 +0200

    Change policy for News and Interests
    Avoid showing "Access denied" errors for this. This is still broken - News and Interests is still there. [louder]Linus Torvalds curse word here[/louder]
    
    Anyway, if someone wants to give setting this up a shot, send me suggestions on how to do this

--- functions/public/Invoke-WPFMicrowin.ps1
+++ functions/public/Invoke-WPFMicrowin.ps1
@@ -302,7 +302,7 @@ public class PowerManagement {
             Write-Host "Disabling Search Highlights and News and Interests..."
             reg add "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds\DSB" /v "ShowDynamicContent" /t REG_DWORD /d 0 /f
             reg add "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\SearchSettings" /v "IsDynamicSearchBoxEnabled" /t REG_DWORD /d 0 /f
-            reg add "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" /v "ShellFeedsTaskbarViewMode" /t REG_DWORD /d 2 /f
+            reg add "HKLM\zSOFTWARE\Policies\Microsoft\Dsh" /v "AllowNewsAndInterests" /t REG_DWORD /d 0 /f
             reg add "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "TraySearchBoxVisible" /t REG_DWORD /d 1 /f
         }
 
