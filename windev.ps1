6e1d69c0eac8309610c26696b4ec16701bfe3fc3
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-13 23:22:24 +0200

    Update windev.ps1

--- windev.ps1
+++ windev.ps1
@@ -12,44 +12,12 @@
     Run in Admin Powershell >  ./windev.ps1
 #>
 
-# Function to fetch the latest release tag from the GitHub API
-function Get-LatestRelease {
-    try {
-        $releases = Invoke-RestMethod -Uri 'https://api.github.com/repos/ChrisTitusTech/winutil/releases'
-        $latestRelease = $releases | Where-Object {$_.prerelease -eq $true} | Select-Object -First 1
-        return $latestRelease.tag_name
-    } catch {
-        Write-Host "Error fetching release data: $_" -ForegroundColor Red
-        return $latestRelease.tag_name
-    }
-}
+Invoke-WebRequest "https://github.com/ChrisTitusTech/winutil/archive/refs/heads/main.zip" -OutFile winutil.zip
 
-# Function to redirect to the latest pre-release version
-function RedirectToLatestPreRelease {
-    $latestRelease = Get-LatestRelease
-    if ($latestRelease) {
-        $url = "https://github.com/ChrisTitusTech/winutil/releases/download/$latestRelease/winutil.ps1"
-    } else {
-        Write-Host 'No pre-release version found. This is most likely because the latest release is a full release and no newer pre-release exists.' -ForegroundColor Yellow
-        Write-Host "Using latest Full Release"
-        $url = "https://github.com/ChrisTitusTech/winutil/releases/latest/download/winutil.ps1"
-    }
+Expand-Archive .\winutil.zip
+Remove-Item .\winutil.zip
 
-    $script = Invoke-RestMethod $url
-    # Elevate Shell if necessary
-    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-        Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
+Set-Location .\winutil\winutil-main
 
-        $powershellcmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
-        $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { $powershellcmd }
-
-        Start-Process $processCmd -ArgumentList "$powershellcmd -ExecutionPolicy Bypass -NoProfile -Command $(Invoke-Expression $script)" -Verb RunAs
-    }
-    else{
-        Invoke-Expression $script
-    }
-}
-
-# Call the redirect function
-
-RedirectToLatestPreRelease
+Invoke-Expression .\Complie.ps1
+Invoke-Expression .\Winutil.ps1
