b3cf4f71531b0901d6e96fb38f025cc3302c9c98
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-11-16 23:18:41 +0200

    Update windev.ps1

--- windev.ps1
+++ windev.ps1
@@ -39,11 +39,7 @@ function RedirectToLatestPreRelease {
     # Elevate Shell if necessary
     if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
         Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
-
-        $powershellcmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
-        $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { $powershellcmd }
-
-        Start-Process $processCmd -ArgumentList "$powershellcmd -ExecutionPolicy Bypass -NoProfile -Command $(Invoke-Expression $script)" -Verb RunAs
+        Start-Process powershell -ArgumentList "powershell -ExecutionPolicy Bypass -NoProfile -Command $(Invoke-Expression $script)" -Verb RunAs
     }
     else{
         Invoke-Expression $script
