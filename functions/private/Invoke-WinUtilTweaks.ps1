ce99e5097ae43708ea227ca7f848626d5a7f9230
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2025-11-25 11:07:13 +0100

    Fix #3736

--- functions/private/Invoke-WinUtilTweaks.ps1
+++ functions/private/Invoke-WinUtilTweaks.ps1
@@ -21,10 +21,6 @@ function Invoke-WinUtilTweaks {
         $KeepServiceStartup = $true
     )
 
-    if ($Checkbox -like "*Toggle*") {
-        $CheckBox = $sync.configs.tweaks.$CheckBox
-    }
-
     Write-Debug "Tweaks: $($CheckBox)"
     if($undo) {
         $Values = @{
