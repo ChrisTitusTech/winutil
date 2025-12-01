6b755c500b5362bef0103e863ba673845e6b29df
Chris Titus <contact@christitus.com>
2025-12-01 14:28:24 -0600

    Clean up comments in Set-WinUtilService.ps1
    Removed comments explaining the handling of auto delayed start for PWSH 5.

--- functions/private/Set-WinUtilService.ps1
+++ functions/private/Set-WinUtilService.ps1
@@ -26,14 +26,6 @@ Function Set-WinUtilService {
 
         # Service exists, proceed with changing properties -- while handling auto delayed start for PWSH 5
         if (($PSVersionTable.PSVersion.Major -lt 7) -and ($StartupType -eq "AutomaticDelayedStart")) {
-            # Auto delayed start doesn't work with PWSH 5. That startup type is a combination of both the Automatic startup type,
-            # and an additional DWORD value for delayed start. That's how the SCM defines it. For this, we'll go with sc
-            #  
-            # PWSH 5 uses a built-in enum for service start types: System.ServiceProcess.ServiceStartMode (https://learn.microsoft.com/en-us/dotnet/api/system.serviceprocess.servicestartmode?view=net-10.0-pp)
-            # PWSH 7 uses a custom enum for service start types: Microsoft.PowerShell.Commands.ServiceStartupType (https://learn.microsoft.com/en-us/dotnet/api/microsoft.powershell.commands.servicestartuptype?view=powershellsdk-7.4.0)
-            #
-            # ---- That's why this doesn't work on the former!
-            
             sc.exe config $Name start=delayed-auto
         } else {
             $service | Set-Service -StartupType $StartupType -ErrorAction Stop
