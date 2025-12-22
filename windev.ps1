d57af402bd5b84ae2e8a12fb46fc4c91fc5d21d0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-23 01:19:14 +0200

    Update windev.ps1

--- windev.ps1
+++ windev.ps1
@@ -15,19 +15,7 @@ $latestRelease = $releases | Where-Object { $_.prerelease -eq $true } | Select-O
 $latestTag = $latestRelease.tag_name
 
 if ($latestTag) {
-    $url = "https://github.com/ChrisTitusTech/winutil/releases/download/$latestTag/winutil.ps1"
+    Invoke-RestMethod "https://github.com/ChrisTitusTech/winutil/releases/download/$latestTag/winutil.ps1" | Invoke-Expression
 } else {
-    $url = "https://github.com/ChrisTitusTech/winutil/releases/latest/download/winutil.ps1"
-}
-
-$script = Invoke-RestMethod $url
-
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-
-    $powershellcmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
-    $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { $powershellcmd }
-
-    Start-Process $processCmd -ArgumentList "$powershellcmd -ExecutionPolicy Bypass -NoProfile -Command $(Invoke-Expression $script)" -Verb RunAs
-} else {
-    Invoke-Expression $script
+    Invoke-RestMethod "https://github.com/ChrisTitusTech/winutil/releases/latest/download/winutil.ps1" | Invoke-Expression
 }
