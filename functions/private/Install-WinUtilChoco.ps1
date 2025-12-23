77fbb1b6ac86ae225cdc34bca698e84ffd16e85a
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-23 20:07:45 +0200

    Update Install-WinUtilChoco.ps1

--- functions/private/Install-WinUtilChoco.ps1
+++ functions/private/Install-WinUtilChoco.ps1
@@ -6,21 +6,10 @@ function Install-WinUtilChoco {
         Installs Chocolatey if it is not already installed
 
     #>
-
-    try {
-        Write-Host "Checking if Chocolatey is Installed..."
-
-        if ((Test-WinUtilPackageManager -choco) -eq "installed") {
-            return
-        }
-        
-        Write-Host "Chocolatey is not installed, installing now."
-        Invoke-WebRequest https://community.chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
-
-    } catch {
-        Write-Host "===========================================" -Foregroundcolor Red
-        Write-Host "--     Chocolatey failed to install     ---" -Foregroundcolor Red
-        Write-Host "===========================================" -Foregroundcolor Red
+    if ((Test-WinUtilPackageManager -choco) -eq "installed") {
+        return
     }
-
+        
+    Write-Host "Chocolatey is not installed, installing now."
+    Invoke-WebRequest https://community.chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 }
