ebaf79530a432d0a1b32e58462652e67903a635e
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-12-24 13:00:29 +0200

    Update Invoke-WinUtilUninstallPSProfile.ps1

--- functions/private/Invoke-WinUtilUninstallPSProfile.ps1
+++ functions/private/Invoke-WinUtilUninstallPSProfile.ps1
@@ -1,5 +1,6 @@
 function Invoke-WinUtilUninstallPSProfile {
     if (Test-Path ($Profile + '.bak')) {
+        Remove-Item $Profile
         Rename-Item ($Profile + '.bak') -NewName $Profile
     }
     else {
