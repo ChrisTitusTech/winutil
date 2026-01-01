e5defb9ce339bce7b1908659df20d33614d33ff4
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-01-02 01:26:25 +0200

    Update Install-WinUtilWinget.ps1

--- functions/private/Install-WinUtilWinget.ps1
+++ functions/private/Install-WinUtilWinget.ps1
@@ -12,8 +12,10 @@ function Install-WinUtilWinget {
     }
 
     Write-Host "Winget is not Installed. Installing." -ForegroundColor Red
-        
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    
     Install-PackageProvider -Name NuGet -Force
-    Install-Module "Microsoft.WinGet.Client" -Force
+    Install-Module Microsoft.WinGet.Client -Force
+    Import-Module Microsoft.WinGet.Client
     Repair-WinGetPackageManager
 }
