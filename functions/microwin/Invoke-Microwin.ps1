cbf856c0f48884c1fc5b4b51589c41a844e0b22c
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-01-01 22:46:00 +0100

    Rework stub path reg value set
    This works on both Windows 11 and Windows 10. It didn't work on Win10 before; it should work now

--- functions/microwin/Invoke-Microwin.ps1
+++ functions/microwin/Invoke-Microwin.ps1
@@ -190,7 +190,7 @@ public class PowerManagement {
             reg load HKLM\zSOFTWARE "$($scratchDir)\Windows\System32\config\SOFTWARE"
             reg add "HKLM\zSOFTWARE\Microsoft\Active Setup\Installed Components\CMP_NoFla" /f
             reg add "HKLM\zSOFTWARE\Microsoft\Active Setup\Installed Components\CMP_NoFla" /f /ve /t REG_SZ /d "Stop First Logon Animation Process"
-            reg add "HKLM\zSOFTWARE\Microsoft\Active Setup\Installed Components\CMP_NoFla" /f /v StubPath /t REG_EXPAND_SZ /d '""%WINDIR%\System32\cmd.exe"" /C ""taskkill /f /im firstlogonanim.exe""'
+            reg add "HKLM\zSOFTWARE\Microsoft\Active Setup\Installed Components\CMP_NoFla" /f /v StubPath /t REG_EXPAND_SZ /d '\"%WINDIR%\System32\cmd.exe\" /C \"taskkill /f /im firstlogonanim.exe\"'
             reg unload HKLM\zSOFTWARE
         }
 
