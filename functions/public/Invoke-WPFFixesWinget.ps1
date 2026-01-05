dada9ab576efadb6b08ee7aff66bfb2bf19d2064
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-05 22:46:41 +0200

    Update functions/public/Invoke-WPFFixesWinget.ps1
    Co-authored-by: CodingWonders <101426328+CodingWonders@users.noreply.github.com>

--- functions/public/Invoke-WPFFixesWinget.ps1
+++ functions/public/Invoke-WPFFixesWinget.ps1
@@ -2,7 +2,9 @@ function Invoke-WPFFixesWinget {
     Set-WinUtilTaskbaritem -state "Indeterminate" -overlay "logo"
     
     Install-PackageProvider -Name NuGet -Force
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     Install-Module "Microsoft.WinGet.Client" -Force
+    Import-Module Microsoft.WinGet.Client
     Repair-WinGetPackageManager
 
     Write-Host "==> Finished Winget Repair"
