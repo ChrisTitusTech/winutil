1ee20919b78282335db7a48711d085120b7058d2
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-10 05:23:07 +0200

    Update Invoke-WPFUltimatePerformance.ps1

--- functions/public/Invoke-WPFUltimatePerformance.ps1
+++ functions/public/Invoke-WPFUltimatePerformance.ps1
@@ -15,15 +15,16 @@ Function Invoke-WPFUltimatePerformance {
 
     switch ($State) {
         "Enable" {
-            # Duplicate and activate Ultimate Performance plan
-            $guid = (powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String "Power Scheme GUID").Line.Split()[3]
-            
-            powercfg /setactive $guid
-            Write-Host "Activated Ultimate Performance power plan" -ForegroundColor Green
-        }
-
+            foreach ($line in powercfg /list) {
+                if (-not ($line -like '*Ultimate Performance*')) {
+                    powercfg /setactive (powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String 'Power Scheme GUID').Line.Split()[3]
+                    Write-Host 'Activated Ultimate Performance power plan' -ForegroundColor Green
+                }
+                else {
+                    Write-Host 'Ultimate Performance power plan is already enabled' -ForegroundColor Red
+                }
+            }
         "Disable" {
-            # Get current plan (the ultimate plan) and switch to Balanced, then remove Ultimate Performance
             powercfg /setactive 381b4222-f694-41f0-9685-ff5bb260df2e
 
             foreach ($line in powercfg /list) {
