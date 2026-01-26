ab4a9cee916da875b499a3aa44284d2f5e6b7d4c
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-26 15:55:10 +0200

    Update Invoke-WinUtilRemoveEdge.ps1

--- functions/public/Invoke-WinUtilRemoveEdge.ps1
+++ functions/public/Invoke-WinUtilRemoveEdge.ps1
@@ -1,8 +1,7 @@
 function Invoke-WinUtilRemoveEdge {
-  $Path = (Get-ChildItem "C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe")[0].FullName
-  
   Write-Host "Unlocking The Offical Edge Uninstaller And Removing Microsoft Edge..."
-  
+
+  $Path = (Get-ChildItem "C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe")[0].FullName
   New-Item "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe" -Force
   Start-Process $Path -ArgumentList '--uninstall --system-level --force-uninstall --delete-profile'
 }
