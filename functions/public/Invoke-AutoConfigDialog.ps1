4efdcf07787bc53527c7704cced9bfacfdbf8d62
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-06 11:06:09 +0200

    Update Invoke-AutoConfigDialog.ps1

--- functions/public/Invoke-AutoConfigDialog.ps1
+++ functions/public/Invoke-AutoConfigDialog.ps1
@@ -1,10 +1,8 @@
 function Invoke-AutoConfigDialog {
 
     <#
-
         .SYNOPSIS
             Sets the automatic configuration file based on a specified JSON file
-
     #>
 
     [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
@@ -12,13 +10,11 @@ function Invoke-AutoConfigDialog {
     $OFD.Filter = "JSON Files (*.json)|*.json"
     $OFD.ShowDialog()
 
-    if (($OFD.FileName -eq "") -and ($sync.MicrowinAutoConfigBox.Text -eq ""))
+    if ($OFD.FileName -eq "")
     {
         Write-Host "No automatic config file has been selected. Continuing without one..."
         return
     }
-    elseif ($OFD.FileName -ne "")
-    {
-        $sync.MicrowinAutoConfigBox.Text = "$($OFD.FileName)"
-    }
+
+    return $OFD.FileName
 }
