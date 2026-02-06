45636672235d0e8632b5c8970b0d4eeeda33af72
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-06 10:36:22 +0200

    Update Invoke-WinUtilInstallPSProfile.ps1

--- functions/private/Invoke-WinUtilInstallPSProfile.ps1
+++ functions/private/Invoke-WinUtilInstallPSProfile.ps1
@@ -1,8 +1,15 @@
 function Invoke-WinUtilInstallPSProfile {
 
     if (Test-Path $Profile) {
-        Rename-Item $Profile -NewName ($Profile + '.bak')
+        Rename-Item $Profile -NewName ($Profile + '.bak') -Force
     }
 
-    Start-Process powershell -ArgumentList '-Command "irm https://github.com/ChrisTitusTech/powershell-profile/raw/main/setup.ps1 | iex"'
+    try {
+        Start-Process pwsh -ArgumentList '-Command "irm https://github.com/ChrisTitusTech/powershell-profile/raw/main/setup.ps1 | iex"'
+    }
+    catch {
+        Write-Host "Installing pwsh"
+        winget install pwsh --source winget
+        Start-Process pwsh -ArgumentList '-Command "irm https://github.com/ChrisTitusTech/powershell-profile/raw/main/setup.ps1 | iex"'
+    }
 }
