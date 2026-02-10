a28474155ca6bd1995824a0dcb016cd416917abb
Chris Titus <contact@christitus.com>
2026-02-10 13:32:53 -0600

    Revert "Fix Get Installed toggle detection and multiple config bugs (#3959)"
    This reverts commit 414cd139e2499272ada8bb04dc7d4ed3dec8eb9c.

--- functions/private/Invoke-WinUtilCurrentSystem.ps1
+++ functions/private/Invoke-WinUtilCurrentSystem.ps1
@@ -41,73 +41,47 @@ Function Invoke-WinUtilCurrentSystem {
         }
     }
 
-    if ($CheckBox -eq "tweaks") {
+    if($CheckBox -eq "tweaks") {
 
-        if (!(Test-Path 'HKU:\')) {$null = (New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS)}
+        if(!(Test-Path 'HKU:\')) {$null = (New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS)}
         $ScheduledTasks = Get-ScheduledTask
 
         $sync.configs.tweaks | Get-Member -MemberType NoteProperty | ForEach-Object {
 
             $Config = $psitem.Name
             #WPFEssTweaksTele
-            $entry = $sync.configs.tweaks.$Config
-            $registryKeys = $entry.registry
-            $scheduledtaskKeys = $entry.scheduledtask
-            $serviceKeys = $entry.service
-            $appxKeys = $entry.appx
-            $invokeScript = $entry.InvokeScript
-            $entryType = $entry.Type
+            $registryKeys = $sync.configs.tweaks.$Config.registry
+            $scheduledtaskKeys = $sync.configs.tweaks.$Config.scheduledtask
+            $serviceKeys = $sync.configs.tweaks.$Config.service
 
-            if ($registryKeys -or $scheduledtaskKeys -or $serviceKeys) {
+            if($registryKeys -or $scheduledtaskKeys -or $serviceKeys) {
                 $Values = @()
 
-                if ($entryType -eq "Toggle") {
-                    if (-not (Get-WinUtilToggleStatus $Config)) {
-                        $values += $False
-                    }
-                } else {
-                    $registryMatchCount = 0
-                    $registryTotal = 0
 
-                    Foreach ($tweaks in $registryKeys) {
-                        Foreach ($tweak in $tweaks) {
-                            $registryTotal++
-                            $regstate = $null
+                Foreach ($tweaks in $registryKeys) {
+                    Foreach($tweak in $tweaks) {
 
-                            if (Test-Path $tweak.Path) {
-                                $regstate = Get-ItemProperty -Name $tweak.Name -Path $tweak.Path -ErrorAction SilentlyContinue | Select-Object -ExpandProperty $($tweak.Name)
+                        if(test-path $tweak.Path) {
+                            $actualValue = Get-ItemProperty -Name $tweak.Name -Path $tweak.Path -ErrorAction SilentlyContinue | Select-Object -ExpandProperty $($tweak.Name)
+                            $expectedValue = $tweak.Value
+                            if ($expectedValue -eq "<RemoveEntry>") {
+                              if ($null -ne $actualValue) {
+                                $values += $False
+                              }
+                            } elseif ($expectedValue -notlike $actualValue) {
+                                $values += $False
                             }
-
-                            if ($null -eq $regstate) {
-                                switch ($tweak.DefaultState) {
-                                    "true" {
-                                        $regstate = $tweak.Value
-                                    }
-                                    "false" {
-                                        $regstate = $tweak.OriginalValue
-                                    }
-                                    default {
-                                        $regstate = $tweak.OriginalValue
-                                    }
-                                }
-                            }
-
-                            if ($regstate -eq $tweak.Value) {
-                                $registryMatchCount++
-                            }
+                        } else {
+                            $values += $False
                         }
-                    }
-
-                    if ($registryTotal -gt 0 -and $registryMatchCount -ne $registryTotal) {
-                        $values += $False
                     }
                 }
 
                 Foreach ($tweaks in $scheduledtaskKeys) {
-                    Foreach ($tweak in $tweaks) {
+                    Foreach($tweak in $tweaks) {
                         $task = $ScheduledTasks | Where-Object {$($psitem.TaskPath + $psitem.TaskName) -like "\$($tweak.name)"}
 
-                        if ($task) {
+                        if($task) {
                             $actualValue = $task.State
                             $expectedValue = $tweak.State
                             if ($expectedValue -ne $actualValue) {
@@ -118,10 +92,10 @@ Function Invoke-WinUtilCurrentSystem {
                 }
 
                 Foreach ($tweaks in $serviceKeys) {
-                    Foreach ($tweak in $tweaks) {
+                    Foreach($tweak in $tweaks) {
                         $Service = Get-Service -Name $tweak.Name
 
-                        if ($Service) {
+                        if($Service) {
                             $actualValue = $Service.StartType
                             $expectedValue = $tweak.StartupType
                             if ($expectedValue -ne $actualValue) {
@@ -131,12 +105,8 @@ Function Invoke-WinUtilCurrentSystem {
                     }
                 }
 
-                if ($values -notcontains $false) {
+                if($values -notcontains $false) {
                     Write-Output $Config
-                }
-            } else {
-                if ($invokeScript -or $appxKeys) {
-                    Write-Debug "Skipping $Config in Get Installed: no detectable registry, scheduled task, or service state."
                 }
             }
         }
