f5dc2001c9b312b3a8ceeb3184269c05bb2f92a3
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-03-11 12:48:32 +0200

    Update Invoke-WPFOOSU.ps1

--- functions/public/Invoke-WPFOOSU.ps1
+++ functions/public/Invoke-WPFOOSU.ps1
@@ -1,6 +1,5 @@
 function Invoke-WPFOOSU {
-    $Initial_ProgressPreference = $ProgressPreference
-    $ProgressPreference = "SilentlyContinue" # Disables the Progress Bar to drasticly speed up Invoke-WebRequest
+    $global:ProgressPreference = "SilentlyContinue"
 
     if (-not ((Test-NetConnection).PingSucceeded)) {
         Write-Host "You must have an internet connection to download ooshutup10!" -ForegroundColor Red
@@ -9,6 +8,6 @@ function Invoke-WPFOOSU {
 
     Invoke-WebRequest -Uri https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe -OutFile $Env:Temp\OOSU10.exe
     Start-Process $Env:Temp\OOSU10.exe
-
-    $ProgressPreference = $Initial_ProgressPreference
+    
+    $global:ProgressPreference = "Continue"
 }
