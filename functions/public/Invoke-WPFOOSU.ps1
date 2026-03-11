535e07e82fb00797ab7bbe4c00f118879d5f043f
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-03-11 12:33:48 +0200

    Update Invoke-WPFOOSU.ps1

--- functions/public/Invoke-WPFOOSU.ps1
+++ functions/public/Invoke-WPFOOSU.ps1
@@ -1,10 +1,9 @@
 function Invoke-WPFOOSU {
-    if (-not ((Test-NetConnection).PingSucceeded) {
-        Write-Host "You must have a internet connect to download ooshutup10!" -ForegroundColor Red
+    if (-not ((Test-NetConnection).PingSucceeded)) {
+        Write-Host "You must have an internet connection to download ooshutup10!" -ForegroundColor Red
         return
     }
 
     Invoke-WebRequest -Uri https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe -OutFile $Env:Temp\OOSU10.exe
     Start-Process $Env:Temp\OOSU10.exe
-    }
 }
