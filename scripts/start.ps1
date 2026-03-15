7aef7d4bb40854527dec40b1a0ac448255fdb180
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-03-16 00:36:04 +0200

    Rework auto elvation

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -33,37 +33,23 @@ if ($Offline) {
     $PARAM_OFFLINE = $true
 }
 
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
+    $command = if ($PSCommandPath) { "$PSCommandPath" "$args" } else { "irm https://christitus.com/win | iex" }
 
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
-    $argList = @()
-
-    $PSBoundParameters.GetEnumerator() | ForEach-Object {
-        $argList += if ($_.Value -is [switch] -and $_.Value) {
-            "-$($_.Key)"
-        } elseif ($_.Value -is [array]) {
-            "-$($_.Key) $($_.Value -join ',')"
-        } elseif ($_.Value) {
-            "-$($_.Key) '$($_.Value)'"
+    if (Get-Command wt -ErrorAction SilentlyContinue) {
+        if (Get-Command pwsh -ErrorAction SilentlyContinue) {
+            Start-Process wt -Verb RunAs -ArgumentList "new-tab pwsh -Command $command"
+        } else {
+            Start-Process wt -Verb RunAs -ArgumentList "new-tab powershell -Command $command"
+        }
+    } else {
+        if (Get-Command pwsh -ErrorAction SilentlyContinue) {
+            Start-Process pwsh -Verb RunAs -ArgumentList "-Command $command"
+        } else {
+            Start-Process powershell -Verb RunAs -ArgumentList "-Command $command"
         }
     }
-
-    $script = if ($PSCommandPath) {
-        "& { & `'$($PSCommandPath)`' $($argList -join ' ') }"
-    } else {
-        "&([ScriptBlock]::Create((irm https://github.com/ChrisTitusTech/winutil/releases/latest/download/winutil.ps1))) $($argList -join ' ')"
-    }
-
-    $powershellCmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
-    $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { "$powershellCmd" }
-
-    if ($processCmd -eq "wt.exe") {
-        Start-Process $processCmd -ArgumentList "$powershellCmd -ExecutionPolicy Bypass -NoProfile -Command `"$script`"" -Verb RunAs
-    } else {
-        Start-Process $processCmd -ArgumentList "-ExecutionPolicy Bypass -NoProfile -Command `"$script`"" -Verb RunAs
-    }
-
-    break
+    exit
 }
 
 # Load DLLs
