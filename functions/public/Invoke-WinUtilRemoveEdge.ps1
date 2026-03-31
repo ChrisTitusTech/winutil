fe13c31bbf7bdbdf727aeb1eed71716b6c58b0f1
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-03-31 16:18:12 +0300

    Update Invoke-WinUtilRemoveEdge.ps1

--- functions/public/Invoke-WinUtilRemoveEdge.ps1
+++ functions/public/Invoke-WinUtilRemoveEdge.ps1
@@ -1,5 +1,5 @@
 function Invoke-WinUtilRemoveEdge {
-  $Path = Get-ChildItem -Path "$Env:ProgramFiles (x86)\Microsoft\Edge\Application\*\Installer\setup.exe" | Sort-Object FullName -Descending | Select-Object -First 1
+  $Path = Get-ChildItem -Path "$Env:ProgramFiles (x86)\Microsoft\Edge\Application\*\Installer\setup.exe" | Select-Object -First 1
   
   New-Item -Path "$Env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe" -Force
   Start-Process -FilePath $Path -ArgumentList '--uninstall --system-level --force-uninstall --delete-profile' -Wait
