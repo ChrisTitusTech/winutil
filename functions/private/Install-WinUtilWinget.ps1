c0e47a02810a125f8396bf5f11472a40bded4b8c
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-04-03 05:21:01 -0700

    Update Install-WinUtilWinget.ps1

--- functions/private/Install-WinUtilWinget.ps1
+++ functions/private/Install-WinUtilWinget.ps1
@@ -15,6 +15,5 @@ function Install-WinUtilWinget {
 
     Install-PackageProvider -Name NuGet -Force
     Install-Module -Name Microsoft.WinGet.Client -Force
-    Import-Module -Name Microsoft.WinGet.Client
     Repair-WinGetPackageManager -AllUsers
 }
