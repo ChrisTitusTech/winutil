8e2dc2182389bb088c7fca3e4bfe2f2c5835c0ae
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-04-14 19:03:07 +0300

    Update `Invoke-WinUtilRemoveEdge.ps1` (#4358)
    * Replce Get-ChildItem with Resolve-Path inside of Invoke-WinUtilRemoveEdge.ps1
    
    * Update Invoke-WinUtilRemoveEdge.ps1
    
    * Update Invoke-WinUtilRemoveEdge.ps1

--- functions/public/Invoke-WinUtilRemoveEdge.ps1
+++ functions/public/Invoke-WinUtilRemoveEdge.ps1
@@ -1,7 +1,7 @@
 function Invoke-WinUtilRemoveEdge {
-  $Path = Get-ChildItem -Path "$Env:ProgramFiles (x86)\Microsoft\Edge\Application\*\Installer\setup.exe" | Select-Object -First 1
-
   New-Item -Path "$Env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe" -Force
+
+  $Path = Resolve-Path -Path "$Env:ProgramFiles (x86)\Microsoft\Edge\Application\*\Installer\setup.exe" | Select-Object -Last 1
   Start-Process -FilePath $Path -ArgumentList '--uninstall --system-level --force-uninstall --delete-profile' -Wait
 
   Write-Host "Microsoft Edge was removed" -ForegroundColor Green
