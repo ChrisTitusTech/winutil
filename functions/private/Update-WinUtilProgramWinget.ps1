b03a2f07b2e926399b7ade27a0cddf30df5c987b
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-04-23 11:21:56 +0300

    Delete functions/private/Update-WinUtilProgramWinget.ps1

--- functions/private/Update-WinUtilProgramWinget.ps1
+++
@@ -1,3 +0,0 @@
-Function Update-WinUtilProgramWinget {
-    Start-Process -FilePath winget.exe -ArgumentList 'upgrade --all --silent --include-unknown --accept-source-agreements --accept-package-agreements' -Wait -NoNewWindow
-}
