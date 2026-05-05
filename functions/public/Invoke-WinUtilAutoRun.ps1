55ce6258fdf629b102bb25efe77baffae662e6ca
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-05 09:37:28 +0300

    Update Invoke-WinUtilAutoRun.ps1

--- functions/public/Invoke-WinUtilAutoRun.ps1
+++ functions/public/Invoke-WinUtilAutoRun.ps1
@@ -6,37 +6,15 @@ function Invoke-WinUtilAutoRun {
     #>
 
     function BusyWait {
-        Start-Sleep -Seconds 1
+        Start-Sleep -Milliseconds 100
         while ($sync.ProcessRunning) {
-            Start-Sleep -Seconds 1
+            Start-Sleep -Milliseconds 100
         }
     }
-
-    BusyWait
 
     if ($sync.selectedTweaks.Count -gt 0) {
         Write-Host "Applying tweaks..."
         Invoke-WPFtweaksbutton
-        BusyWait
-    }
-
-    if ($sync.selectedToggles.Count -gt 0) {
-        Write-Host "Applying toggles..."
-        $handle = Invoke-WPFRunspace -ScriptBlock {
-            $Toggles = $sync.selectedToggles
-            Write-Debug "Inside Number of toggles to process: $($Toggles.Count)"
-
-            $sync.ProcessRunning = $true
-
-            for ($i = 0; $i -lt $Toggles.Count; $i++) {
-                Invoke-WinUtilTweaks $Toggles[$i]
-            }
-
-            $sync.ProcessRunning = $false
-            Write-Host "================================="
-            Write-Host "--     Toggles are Finished    ---"
-            Write-Host "================================="
-        }
         BusyWait
     }
 
