07cb9e73a5f09c799d0be16a321f1815d2886c1c
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-05 10:19:36 +0300

    Update Invoke-WPFtweaksbutton.ps1

--- functions/public/Invoke-WPFtweaksbutton.ps1
+++ functions/public/Invoke-WPFtweaksbutton.ps1
@@ -14,7 +14,9 @@ function Invoke-WPFtweaksbutton {
 
   $Tweaks = $sync.selectedTweaks
   $dnsProvider = $sync["WPFchangedns"].text
-  if ([string]::IsNullOrWhiteSpace($dnsProvider)) { $dnsProvider = "Default" }
+  if (-not ($dnsProvider)) {
+    $dnsProvider = "Default"
+  }
   $restorePointTweak = "WPFTweaksRestorePoint"
   $restorePointSelected = $Tweaks -contains $restorePointTweak
   $tweaksToRun = @($Tweaks | Where-Object { $_ -ne $restorePointTweak })
