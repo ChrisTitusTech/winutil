0a0369849750b005f537806e8db0b567ac31244b
GabiNun2 <gabinun6969@gmail.com>
2026-05-05 09:35:00 +0300

    Skip empty no-UI autorun sections

--- functions/public/Invoke-WinUtilAutoRun.ps1
+++ functions/public/Invoke-WinUtilAutoRun.ps1
@@ -6,43 +6,51 @@ function Invoke-WinUtilAutoRun {
     #>
 
     function BusyWait {
-        Start-Sleep -Seconds 5
+        Start-Sleep -Seconds 1
         while ($sync.ProcessRunning) {
-                Start-Sleep -Seconds 5
-            }
-    }
-
-    BusyWait
-
-    Write-Host "Applying tweaks..."
-    Invoke-WPFtweaksbutton
-    BusyWait
-
-    Write-Host "Applying toggles..."
-    $handle = Invoke-WPFRunspace -ScriptBlock {
-        $Toggles = $sync.selectedToggles
-        Write-Debug "Inside Number of toggles to process: $($Toggles.Count)"
-
-        $sync.ProcessRunning = $true
-
-        for ($i = 0; $i -lt $Toggles.Count; $i++) {
-            Invoke-WinUtilTweaks $Toggles[$i]
+            Start-Sleep -Seconds 1
         }
-
-        $sync.ProcessRunning = $false
-        Write-Host "================================="
-        Write-Host "--     Toggles are Finished    ---"
-        Write-Host "================================="
     }
+
     BusyWait
 
-    Write-Host "Applying features..."
-    Invoke-WPFFeatureInstall
-    BusyWait
+    if ($sync.selectedTweaks.Count -gt 0) {
+        Write-Host "Applying tweaks..."
+        Invoke-WPFtweaksbutton
+        BusyWait
+    }
 
-    Write-Host "Installing applications..."
-    Invoke-WPFInstall
-    BusyWait
+    if ($sync.selectedToggles.Count -gt 0) {
+        Write-Host "Applying toggles..."
+        $handle = Invoke-WPFRunspace -ScriptBlock {
+            $Toggles = $sync.selectedToggles
+            Write-Debug "Inside Number of toggles to process: $($Toggles.Count)"
+
+            $sync.ProcessRunning = $true
+
+            for ($i = 0; $i -lt $Toggles.Count; $i++) {
+                Invoke-WinUtilTweaks $Toggles[$i]
+            }
+
+            $sync.ProcessRunning = $false
+            Write-Host "================================="
+            Write-Host "--     Toggles are Finished    ---"
+            Write-Host "================================="
+        }
+        BusyWait
+    }
+
+    if ($sync.selectedFeatures.Count -gt 0) {
+        Write-Host "Applying features..."
+        Invoke-WPFFeatureInstall
+        BusyWait
+    }
+
+    if ($sync.selectedApps.Count -gt 0) {
+        Write-Host "Installing applications..."
+        Invoke-WPFInstall
+        BusyWait
+    }
 
     Write-Host "Done."
 }
