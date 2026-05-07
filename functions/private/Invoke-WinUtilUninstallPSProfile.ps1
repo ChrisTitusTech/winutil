3d6c54bd4ab0fdf0aac2fc4a404de0e5ebc21d44
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-07 13:32:18 -0700

    Update Invoke-WinUtilUninstallPSProfile.ps1

--- functions/private/Invoke-WinUtilUninstallPSProfile.ps1
+++ functions/private/Invoke-WinUtilUninstallPSProfile.ps1
@@ -1,4 +1,10 @@
 function Invoke-WinUtilUninstallPSProfile {
-    Remove-Item -Path $Profile
+
+    if (Test-Path ($Profile + ".bak")) {
+        Move-Item -Path ($Profile + ".bak") -Destination $Profile
+    } else {
+        Remove-Item -Path $Profile
+    }
+
     Write-Host "Successfully uninstalled CTT PowerShell Profile." -ForegroundColor Green
 }
