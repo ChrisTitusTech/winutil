acdefaf0429902c9635b663d01bc9e9bc363d9b8
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-07 13:28:16 -0700

    Update Invoke-WinUtilInstallPSProfile.ps1

--- functions/private/Invoke-WinUtilInstallPSProfile.ps1
+++ functions/private/Invoke-WinUtilInstallPSProfile.ps1
@@ -1,3 +1,15 @@
 function Invoke-WinUtilInstallPSProfile {
+    if (-not (Get-Command wt)) {
+        Write-Host "Windows Terminal not found installing..."
+        Install-WinUtilWinget
+        winget install Microsoft.WindowsTerminal --source winget --silent
+    }
+
+    if (-not (Get-Command pwsh)) {
+        Write-Host "Powershell 7 not found installing..."
+        Install-WinUtilWinget
+        winget install Microsoft.PowerShell --source winget --silent
+    }
+
     wt new-tab pwsh -NoExit -Command "irm https://github.com/ChrisTitusTech/powershell-profile/raw/main/setup.ps1 | iex"
 }
