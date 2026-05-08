74d9b511a560726bd401d42991d0bf4db3293d32
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-08 02:55:11 -0700

    Update Invoke-WPFUltimatePerformance.ps1

--- functions/public/Invoke-WPFUltimatePerformance.ps1
+++ functions/public/Invoke-WPFUltimatePerformance.ps1
@@ -9,6 +9,7 @@ function Invoke-WPFUltimatePerformance {
         Write-Host "Ultimate Power Plan plan installed and activated." -ForegroundColor Green
     } else {
         powercfg /setactive SCHEME_BALANCED
+        powercfg /delete e9a42b02-d5df-448d-aa00-03f14749eb61
         Write-Host "Ultimate Power Plan plan was removed." -ForegroundColor Red
     }
 }
