1e03a8f7866a450d13551a8dc770487eeb2407a0
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-13 13:15:07 -0700

    Update Compile.ps1

--- Compile.ps1
+++ Compile.ps1
@@ -20,7 +20,7 @@ $script.Add((Get-ChildItem functions -Recurse -File | Get-Content))
 Get-ChildItem config -Filter *.json | ForEach-Object {
     $obj = Get-Content $_.FullName | ConvertFrom-Json
 
-    if ($_.Name -eq applications.json) {
+    if ($_.Name -eq "applications.json") {
         $fixed = [ordered]@{}
         foreach ($p in $obj.PSObject.Properties) {
             $fixed["WPFInstall$($p.Name)"] = $p.Value
