5e5038282071e734cc3e5ce446fb6206f6fe85c4
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-15 05:33:13 -0700

    Update Compile.ps1

--- Compile.ps1
+++ Compile.ps1
@@ -12,13 +12,13 @@ $sync.configs = @{}
 $script = [System.Collections.Generic.List[string]]::new()
 
 $script.Add(
-    ((Get-Content scripts\start.ps1) -replace '#{replaceme}', (Get-Date -Format 'yy.MM.dd'))
+    ((Get-Content -Path scripts\start.ps1) -replace '#{replaceme}', (Get-Date -Format 'yy.MM.dd'))
 )
 
-$script.Add((Get-ChildItem functions -Recurse -File | Get-Content))
+$script.Add((Get-ChildItem -Path functions -Recurse -File | Get-Content))
 
 Get-ChildItem config | ForEach-Object {
-    $obj = Get-Content $_.FullName | ConvertFrom-Json
+    $obj = Get-Content -Path $_.FullName | ConvertFrom-Json
 
     if ($_.Name -eq "applications.json") {
         $fixed = [ordered]@{}
@@ -35,13 +35,13 @@ Get-ChildItem config | ForEach-Object {
 }
 
 # Read the entire XAML file as a single string, preserving line breaks
-$xaml = Get-Content xaml\inputXML.xaml
+$xaml = Get-Content -Path xaml\inputXML.xaml
 $script.Add('$inputXML = @''' + "`n" + $xaml + "`n" + '''@')
 
-$autounattendXml = Get-Content tools\autounattend.xml
+$autounattendXml = Get-Content -Path tools\autounattend.xml
 $script.Add("`$WinUtilAutounattendXml = @'`r`n$autounattendXml`r`n'@")
 
-$script.Add((Get-Content scripts\main.ps1))
+$script.Add((Get-Content -Path scripts\main.ps1))
 
 Set-Content -Path winutil.ps1 -Value $script
 
