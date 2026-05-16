2799cae18e081d392d06e469d48f48fee2e232d0
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-16 07:43:26 -0700

    Update Invoke-WPFUpdatesdefault.ps1

--- functions/public/Invoke-WPFUpdatesdefault.ps1
+++ functions/public/Invoke-WPFUpdatesdefault.ps1
@@ -1,59 +1,17 @@
 function Invoke-WPFUpdatesdefault {
-    <#
-
-    .SYNOPSIS
-        Resets Windows Update settings to default
-
-    #>
-    $ErrorActionPreference = 'SilentlyContinue'
-
-    Write-Host "Removing Windows Update policy settings..." -ForegroundColor Green
-
-    Remove-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Recurse -Force
-    Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization" -Recurse -Force
-    Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" -Recurse -Force
-    Remove-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Device Metadata" -Recurse -Force
-    Remove-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching" -Recurse -Force
-    Remove-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Recurse -Force
 
     Write-Host "Showing Windows Updates in settings"
     Remove-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer -Name SettingsPageVisibility
 
-    Write-Host "Reenabling Windows Update Services..." -ForegroundColor Green
-
-    Write-Host "Restored BITS to Manual"
-    Set-Service -Name BITS -StartupType Manual
-
-    Write-Host "Restored wuauserv to Manual"
     Set-Service -Name wuauserv -StartupType Manual
+    Start-Service -Name wuauserv
+    Write-Host "Restored wuauserv to Manual"
 
-    Write-Host "Restored UsoSvc to Automatic"
-    Start-Service -Name UsoSvc
     Set-Service -Name UsoSvc -StartupType Automatic
-
-    Write-Host "Restored WaaSMedicSvc to Manual"
-    Set-Service -Name WaaSMedicSvc -StartupType Manual
-
-    Write-Host "Enabling update related scheduled tasks..." -ForegroundColor Green
-
-    $Tasks =
-        '\Microsoft\Windows\InstallService\*',
-        '\Microsoft\Windows\UpdateOrchestrator\*',
-        '\Microsoft\Windows\UpdateAssistant\*',
-        '\Microsoft\Windows\WaaSMedic\*',
-        '\Microsoft\Windows\WindowsUpdate\*',
-        '\Microsoft\WindowsUpdate\*'
-
-    foreach ($Task in $Tasks) {
-        Get-ScheduledTask -TaskPath $Task | Enable-ScheduledTask -ErrorAction SilentlyContinue
-    }
-
-    Write-Host "Windows Local Policies Reset to Default"
-    secedit /configure /cfg "$Env:SystemRoot\inf\defltbase.inf" /db defltbase.sdb
+    Start-Service -Name UsoSvc
+    Write-Host "Restored UsoSvc to Automatic"
 
     Write-Host "===================================================" -ForegroundColor Green
     Write-Host "---  Windows Update Settings Reset to Default   ---" -ForegroundColor Green
     Write-Host "===================================================" -ForegroundColor Green
-
-    Write-Host "Note: You must restart your system in order for all changes to take effect." -ForegroundColor Yellow
 }
