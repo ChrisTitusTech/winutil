cf966cecb1c9e4baf90ce4a0af38fef7c193903a
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-16 07:07:00 -0700

    Changes windev.ps1's syntax

--- windev.ps1
+++ windev.ps1
@@ -9,5 +9,5 @@
     Run in Admin Powershell >  ./windev.ps1
 #>
 
-$latestTag = (Invoke-RestMethod "https://api.github.com/repos/ChrisTitusTech/winutil/tags")[0].name
-Invoke-RestMethod "https://github.com/ChrisTitusTech/winutil/releases/download/$latestTag/winutil.ps1" | Invoke-Expression
+$latestTag = (Invoke-RestMethod https://api.github.com/repos/ChrisTitusTech/winutil/tags).Name | Select-Object -First 1
+Invoke-RestMethod -Uri https://github.com/ChrisTitusTech/winutil/releases/download/$latestTag/winutil.ps1 | Invoke-Expression
