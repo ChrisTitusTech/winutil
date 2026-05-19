24aaf9a3cf351769da45ac4883438fbeecf90250
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-19 21:07:10 +0300

    Cleanup Get-WinUtilSelectedPackages.ps1 and remove annoying messages (#4496)
    * Cleanup Get-WinUtilSelectedPackages.ps1 and remove annoying messages
    
    * Update Get-WinUtilSelectedPackages.ps1
    
    * Update Get-WinUtilSelectedPackages.ps1
    
    * Update Get-WinUtilSelectedPackages.ps1
    
    * Merge branch 'main' into patch-8
    
    * Update Get-WinUtilSelectedPackages.ps1
    
    * Update Get-WinUtilSelectedPackages.ps1
    
    * Update Get-WinUtilSelectedPackages.ps1
    
    * Update Get-WinUtilSelectedPackages.ps1

--- functions/private/Get-WinUtilSelectedPackages.ps1
+++ functions/private/Get-WinUtilSelectedPackages.ps1
@@ -1,18 +1,12 @@
-function Get-WinUtilSelectedPackages
-{
-     <#
-    .SYNOPSIS
-        Sorts given packages based on installer preference and availability.
+function Get-WinUtilSelectedPackages {
 
-    .OUTPUTS
-        Hashtable. Key = Package Manager, Value = ArrayList of packages to install
-    #>
-    param (
-        [Parameter(Mandatory=$true)]
-        $PackageList,
-        [Parameter(Mandatory=$true)]
-        [PackageManagers]$Preference
-    )
+     param(
+         [Parameter(Mandatory = $true)]
+         [object] $PackageList,
+     
+         [Parameter(Mandatory = $true)]
+         [PackageManagers] $Preference
+     )
 
     if ($PackageList.count -eq 1) {
         Invoke-WPFUIThread -ScriptBlock { Set-WinUtilTaskbaritem -state "Indeterminate" -value 0.01 -overlay "logo" }
@@ -23,34 +17,21 @@ function Get-WinUtilSelectedPackages
     $packages = [System.Collections.Hashtable]::new()
     $packagesWinget = [System.Collections.ArrayList]::new()
     $packagesChoco = [System.Collections.ArrayList]::new()
+
     $packages[[PackageManagers]::Winget] = $packagesWinget
     $packages[[PackageManagers]::Choco] = $packagesChoco
-
-    Write-Debug "Checking packages using Preference '$($Preference)'"
 
     foreach ($package in $PackageList) {
         switch ($Preference) {
             "Choco" {
                 if ($package.choco -eq "na") {
-                    Write-Debug "$($package.content) has no Choco value."
-                    $null = $packagesWinget.add($($package.winget))
-                    Write-Host "Queueing $($package.winget) for WinGet..."
+                    $null = $packagesWinget.add($package.winget)
                 } else {
                     $null = $packagesChoco.add($package.choco)
-                    Write-Host "Queueing $($package.choco) for Chocolatey..."
                 }
-                break
             }
             "Winget" {
-                if ($package.winget -eq "na") {
-                    Write-Debug "$($package.content) has no WinGet value."
-                    $null = $packagesChoco.add($package.choco)
-                    Write-Host "Queueing $($package.choco) for Chocolatey..."
-                } else {
-                    $null = $packagesWinget.add($($package.winget))
-                    Write-Host "Queueing $($package.winget) for WinGet..."
-                }
-                break
+                $null = $packagesWinget.add($package.winget)
             }
         }
     }
