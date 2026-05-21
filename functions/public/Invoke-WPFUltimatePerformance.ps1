bcef185a98b69feb97e25874299d65f04d32d1de
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-21 10:09:11 +0300

    Update Invoke-WPFUltimatePerformance.ps1

--- functions/public/Invoke-WPFUltimatePerformance.ps1
+++ functions/public/Invoke-WPFUltimatePerformance.ps1
@@ -4,7 +4,7 @@ function Invoke-WPFUltimatePerformance {
     )
 
     if ($Enable) {
-        powercfg /setactive (powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String -Pattern '[A-Fa-f0-9-]{36}').Matches.Value
+        powercfg /setactive (powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String -Pattern '[A-Fa-f0-9-]{36}').Matches.Value
         Write-Host "Ultimate Power Plan plan installed and activated." -ForegroundColor Green
     } else {
         powercfg /restoredefaultschemes
