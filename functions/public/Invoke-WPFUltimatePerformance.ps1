d6be8c3f1fa212b8667f4129ea5d8ff0f9658e53
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-24 05:53:29 -0700

    Update Invoke-WPFUltimatePerformance.ps1

--- functions/public/Invoke-WPFUltimatePerformance.ps1
+++ functions/public/Invoke-WPFUltimatePerformance.ps1
@@ -1,8 +1,4 @@
-function Invoke-WPFUltimatePerformance {
-    param(
-        [switch]$Enable
-    )
-
+function Invoke-WPFUltimatePerformance ([switch]$Enable) {
     if ($Enable) {
         powercfg /setactive (powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String -Pattern '[A-Fa-f0-9-]{36}').Matches.Value
         [System.Windows.MessageBox]::Show("Ultimate Power Plan plan installed and activated.","Success","OK","Information")
