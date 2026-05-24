8e9b71ef86a08ef47f018502debdeb2542544f52
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-24 05:41:40 -0700

    Update Invoke-WPFUltimatePerformance.ps1

--- functions/public/Invoke-WPFUltimatePerformance.ps1
+++ functions/public/Invoke-WPFUltimatePerformance.ps1
@@ -5,9 +5,9 @@ function Invoke-WPFUltimatePerformance {
 
     if ($Enable) {
         powercfg /setactive (powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String -Pattern '[A-Fa-f0-9-]{36}').Matches.Value
-        Write-Host "Ultimate Power Plan plan installed and activated." -ForegroundColor Green
+        [System.Windows.MessageBox]::Show("Ultimate Power Plan plan installed and activated.","Success","OK","Information")
     } else {
         powercfg /restoredefaultschemes
-        Write-Host "Power Plan was reset to defaults" -ForegroundColor Red
+        [System.Windows.MessageBox]::Show("Power Plan was reset to defaults.","Success","OK","Information")
     }
 }
