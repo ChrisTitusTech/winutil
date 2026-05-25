1219aa1e03262a3a6988328f10ebc36299335643
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-25 05:19:23 -0700

    Update Invoke-WinUtilISO.ps1

--- functions/private/Invoke-WinUtilISO.ps1
+++ functions/private/Invoke-WinUtilISO.ps1
@@ -32,6 +32,7 @@ Function Invoke-WinUtilISO {
 
     Remove-Item -Path "Sources", "oscdimg.exe" -Recurse -Force
     Dismount-DiskImage -ImagePath $dialog.FileName
-    
+
+    return "$path\Win11Creator.iso"
     Write-Host "Done! iso file located at $path\Win11Creator.iso" -ForegroundColor Green
 }
