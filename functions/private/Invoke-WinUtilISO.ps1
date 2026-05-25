e9f31d3fe09d036cfa4a45ba872e7d317dd36dc6
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-25 04:32:04 -0700

    Update Invoke-WinUtilISO.ps1

--- functions/private/Invoke-WinUtilISO.ps1
+++ functions/private/Invoke-WinUtilISO.ps1
@@ -2,28 +2,34 @@ Function Invoke-WinUtilISO {
     $dialog = New-Object System.Windows.Forms.OpenFileDialog
     $dialog.Filter = "ISO files (*.iso)|*.iso"
     $dialog.Title = "Select Windows ISO"
-    $dialog.ShowDialog() | Out-Null
+    $dialog.ShowDialog()
     
     if (-not $dialog.FileName) { return }
-    
-    Mount-DiskImage -ImagePath $dialog.FileName | Out-Null
+
+    Write-Host "Mounting iso file..."
+
+    Mount-DiskImage -ImagePath $dialog.FileName
     $Drive = ($Drive = Get-CimInstance Win32_CDROMDrive).Drive
     
     Write-Host "Copying files..."
     
-    New-Item -Path "Sources" -ItemType Directory | Out-Null
+    New-Item -Path "Sources" -ItemType Directory
     Copy-Item -Path "$Drive\*" -Destination "Sources" -Recurse -Force
-    
+
+    Write-Host "Downloading oscdimg and injecting autounattend.xml..."
+
     Invoke-WebRequest -Uri https://github.com/GabiNun2/test/raw/main/autounattend.xml -OutFile "Sources\autounattend.xml"
     Invoke-WebRequest -Uri https://msdl.microsoft.com/download/symbols/oscdimg.exe/688CABB065000/oscdimg.exe -OutFile "oscdimg.exe"
     
     Write-Host "Packing files into a iso file..."
     
     $path = Split-Path -Path $dialog.FileName
-    .\oscdimg.exe -u2 -b"Sources\efi\microsoft\boot\efisys.bin" "Sources" "$path\Win11Creator.iso" | Out-Null
-    
+    .\oscdimg.exe -u2 -b"Sources\efi\microsoft\boot\efisys.bin" "Sources" "$path\Win11Creator.iso"
+
+    Write-Host "Cleaning up..."
+
     Remove-Item -Path "Sources", "oscdimg.exe" -Recurse -Force
-    Dismount-DiskImage -ImagePath $dialog.FileName | Out-Null
+    Dismount-DiskImage -ImagePath $dialog.FileName
     
     Write-Host "Done! iso file located at $path\Win11Creator.iso"
 }
