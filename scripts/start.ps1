c5faf3fdf07fb475241888c82da8a7a83321f19b
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-27 03:44:17 -0700

    Update start.ps1

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -34,7 +34,7 @@ if ($ExecutionContext.SessionState.LanguageMode -ne 'FullLanguage') {
 }
 
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Output "Winutil needs to be ran as Administrator. Attempting to relaunch."
+    Write-Host "Winutil needs to be ran as Administrator. Attempting to relaunch."
 
     $argList = @(
         if ($Config) { "-Config $Config" }
