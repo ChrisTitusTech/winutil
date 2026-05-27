e6e8871298a2014925e0cb95742e799aef50f7ab
Tester <tester@example.com>
2026-05-27 22:47:35 +0530

    feat: add argument forwarding and winutil-dev to CTT PS profile installer

--- functions/private/Invoke-WinUtilInstallPSProfile.ps1
+++ functions/private/Invoke-WinUtilInstallPSProfile.ps1
@@ -11,5 +11,5 @@ function Invoke-WinUtilInstallPSProfile {
         winget install Microsoft.PowerShell --source winget --silent
     }
 
-    wt new-tab pwsh -NoExit -Command "irm https://github.com/ChrisTitusTech/powershell-profile/raw/main/setup.ps1 | iex"
+    wt new-tab pwsh -NoExit -Command "irm https://github.com/ChrisTitusTech/powershell-profile/raw/main/setup.ps1 | iex \; if (Test-Path `$PROFILE) { (Get-Content `$PROFILE -Raw) -replace 'Invoke-RestMethod https://christitus.com/win \| Invoke-Expression', '& ([ScriptBlock]::Create((Invoke-RestMethod https://christitus.com/win))) `@args' -replace 'Invoke-RestMethod https://christitus.com/windev \| Invoke-Expression', '& ([ScriptBlock]::Create((Invoke-RestMethod https://christitus.com/windev))) `@args }; function winutil-dev { & ([ScriptBlock]::Create((Invoke-RestMethod https://christitus.com/windev))) `@args' | Set-Content `$PROFILE }"
 }
