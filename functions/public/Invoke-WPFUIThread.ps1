2bf9c556c911b30aec668dc1e0bd29ff67e089c0
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-29 14:58:18 +0300

    Update Invoke-WPFUIThread.ps1

--- functions/public/Invoke-WPFUIThread.ps1
+++ functions/public/Invoke-WPFUIThread.ps1
@@ -1,17 +1,3 @@
-function Invoke-WPFUIThread {
-    <#
-
-    .SYNOPSIS
-        Creates and runs a task on Winutil's WPF Forms thread.
-
-    .PARAMETER ScriptBlock
-        The scriptblock to invoke in the thread
-    #>
-
-    [CmdletBinding()]
-    Param (
-        $ScriptBlock
-    )
-
+function Invoke-WPFUIThread ($ScriptBlock) {
     $sync.form.Dispatcher.Invoke([action]$ScriptBlock)
 }
