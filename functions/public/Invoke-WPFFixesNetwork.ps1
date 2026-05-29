cea7021c2a1c579899c8353fc1ee33ebcc9087d6
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-29 14:18:11 +0300

    Update Invoke-WPFFixesNetwork.ps1

--- functions/public/Invoke-WPFFixesNetwork.ps1
+++ functions/public/Invoke-WPFFixesNetwork.ps1
@@ -1,36 +1,7 @@
 function Invoke-WPFFixesNetwork {
-    <#
+    netsh winsock reset
+    netsh winhttp reset proxy
+    netsh int ip reset
 
-    .SYNOPSIS
-        Resets various network configurations
-
-    #>
-
-    Write-Host "Resetting Network with netsh"
-
-    Set-WinUtilTaskbaritem -state "Normal" -value 0.01 -overlay "logo"
-    # Reset WinSock catalog to a clean state
-    Start-Process -NoNewWindow -FilePath "netsh" -ArgumentList "winsock", "reset"
-
-    Set-WinUtilTaskbaritem -state "Normal" -value 0.35 -overlay "logo"
-    # Resets WinHTTP proxy setting to DIRECT
-    Start-Process -NoNewWindow -FilePath "netsh" -ArgumentList "winhttp", "reset", "proxy"
-
-    Set-WinUtilTaskbaritem -state "Normal" -value 0.7 -overlay "logo"
-    # Removes all user configured IP settings
-    Start-Process -NoNewWindow -FilePath "netsh" -ArgumentList "int", "ip", "reset"
-
-    Set-WinUtilTaskbaritem -state "None" -overlay "checkmark"
-
-    Write-Host "Process complete. Please reboot your computer."
-
-    $ButtonType = [System.Windows.MessageBoxButton]::OK
-    $MessageboxTitle = "Network Reset "
-    $Messageboxbody = ("Stock settings loaded.`n Please reboot your computer")
-    $MessageIcon = [System.Windows.MessageBoxImage]::Information
-
-    [System.Windows.MessageBox]::Show($Messageboxbody, $MessageboxTitle, $ButtonType, $MessageIcon)
-    Write-Host "=========================================="
-    Write-Host "-- Network Configuration has been Reset --"
-    Write-Host "=========================================="
+    Write-Host "Network Configuration has been Reset Please restart your computer."
 }
