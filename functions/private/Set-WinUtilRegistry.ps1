9c1158e8de3f08b01f42100796eb7971125f134b
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-29 14:46:26 +0300

    Update Set-WinUtilRegistry.ps1

--- functions/private/Set-WinUtilRegistry.ps1
+++ functions/private/Set-WinUtilRegistry.ps1
@@ -1,56 +1,18 @@
-function Set-WinUtilRegistry {
-    <#
+function Set-WinUtilRegistry ($Name, $Path, $Type, $Value {
+    if (-not (Get-PSDrive -Name HKU)) {
+        New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS | Out-Null
+    }
 
-    .SYNOPSIS
-        Modifies the registry based on the given inputs
+    if (-not (Test-Path $Path)) {
+        Write-Host "$Path was not found. Creating..."
+        New-Item -Path $Path -Force -ErrorAction Stop | Out-Null
+    }
 
-    .PARAMETER Name
-        The name of the key to modify
-
-    .PARAMETER Path
-        The path to the key
-
-    .PARAMETER Type
-        The type of value to set the key to
-
-    .PARAMETER Value
-        The value to set the key to
-
-    .EXAMPLE
-        Set-WinUtilRegistry -Name "PublishUserActivities" -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Type "DWord" -Value "0"
-
-    #>
-    param (
-        $Name,
-        $Path,
-        $Type,
-        $Value
-    )
-
-    try {
-        if(!(Test-Path 'HKU:\')) {New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS}
-
-        If (!(Test-Path $Path)) {
-            Write-Host "$Path was not found. Creating..."
-            New-Item -Path $Path -Force -ErrorAction Stop | Out-Null
-        }
-
-        if ($Value -ne "<RemoveEntry>") {
-            Write-Host "Set $Path\$Name to $Value"
-            Set-ItemProperty -Path $Path -Name $Name -Type $Type -Value $Value -Force -ErrorAction Stop | Out-Null
-        }
-        else{
-            Write-Host "Remove $Path\$Name"
-            Remove-ItemProperty -Path $Path -Name $Name -Force -ErrorAction Stop | Out-Null
-        }
-    } catch [System.Security.SecurityException] {
-        Write-Warning "Unable to set $Path\$Name to $Value due to a Security Exception."
-    } catch [System.Management.Automation.ItemNotFoundException] {
-        Write-Warning $psitem.Exception.ErrorRecord
-    } catch [System.UnauthorizedAccessException] {
-       Write-Warning $psitem.Exception.Message
-    } catch {
-        Write-Warning "Unable to set $Name due to unhandled exception."
-        Write-Warning $psitem.Exception.StackTrace
+    if ($Value -ne "<RemoveEntry>") {
+        Write-Host "Set $Path\$Name to $Value"
+        Set-ItemProperty -Path $Path -Name $Name -Type $Type -Value $Value -Force -ErrorAction Stop | Out-Null
+    } else {
+        Write-Host "Remove $Path\$Name"
+        Remove-ItemProperty -Path $Path -Name $Name -Force -ErrorAction Stop | Out-Null
     }
 }
