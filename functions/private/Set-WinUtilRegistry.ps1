42ded73b24731a8a2b53039563bb5a028e30832f
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-29 18:46:48 +0300

    Update Set-WinUtilRegistry.ps1

--- functions/private/Set-WinUtilRegistry.ps1
+++ functions/private/Set-WinUtilRegistry.ps1
@@ -1,4 +1,4 @@
-function Set-WinUtilRegistry ($Name, $Path, $Type, $Value {
+function Set-WinUtilRegistry ($Name, $Path, $Type, $Value) {
     if (-not (Get-PSDrive -Name HKU)) {
         New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS | Out-Null
     }
@@ -8,11 +8,16 @@ function Set-WinUtilRegistry ($Name, $Path, $Type, $Value {
         New-Item -Path $Path -Force -ErrorAction Stop | Out-Null
     }
 
-    if ($Value -ne "<RemoveEntry>") {
-        Write-Host "Set $Path\$Name to $Value"
-        Set-ItemProperty -Path $Path -Name $Name -Type $Type -Value $Value -Force -ErrorAction Stop | Out-Null
-    } else {
-        Write-Host "Remove $Path\$Name"
-        Remove-ItemProperty -Path $Path -Name $Name -Force -ErrorAction Stop | Out-Null
+    try {
+        if ($Value -ne "<RemoveEntry>") {
+            Write-Host "Set $Path\$Name to $Value"
+            Set-ItemProperty -Path $Path -Name $Name -Type $Type -Value $Value -Force -ErrorAction Stop | Out-Null
+        } else {
+            Write-Host "Remove $Path\$Name"
+            Remove-ItemProperty -Path $Path -Name $Name -Force -ErrorAction Stop | Out-Null
+        }
+    } catch {
+        Write-Warning "Unable to set $Name due to unhandled exception."
+        Write-Warning $psitem.Exception.StackTrace
     }
 }
