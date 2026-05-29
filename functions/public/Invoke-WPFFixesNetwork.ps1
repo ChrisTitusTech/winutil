9a9579da8370a3def1e19227e5bc20491c876e50
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-29 14:19:33 +0300

    Update Invoke-WPFFixesNetwork.ps1

--- functions/public/Invoke-WPFFixesNetwork.ps1
+++ functions/public/Invoke-WPFFixesNetwork.ps1
@@ -1,7 +1,5 @@
 function Invoke-WPFFixesNetwork {
     netsh winsock reset
-    netsh winhttp reset proxy
     netsh int ip reset
-
     Write-Host "Network Configuration has been Reset Please restart your computer."
 }
