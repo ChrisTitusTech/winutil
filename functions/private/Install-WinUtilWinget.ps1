d159a4d0425db315d80c782e96459454da529eb5
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-31 13:52:26 +0300

    Update Install-WinUtilWinget.ps1

--- functions/private/Install-WinUtilWinget.ps1
+++ functions/private/Install-WinUtilWinget.ps1
@@ -6,7 +6,10 @@ function Install-WinUtilWinget {
 
     Write-Host "WinGet is not installed. Installing..." -ForegroundColor Red
 
-    Install-PackageProvider -Name NuGet -Force
-    Install-Module -Name Microsoft.WinGet.Client -Force
-    Repair-WinGetPackageManager
+    Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile "winget.msixbundle"
+    Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/latest/download/DesktopAppInstaller_Dependencies.zip -OutFile "winget.zip"
+    
+    Expand-Archive "winget.zip"
+    Add-AppxPackage -Path "winget.msixbundle" -DependencyPath "winget\x64\*"
+    Remove-Item -Path "winget*" -Recurse
 }
