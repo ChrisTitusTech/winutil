f2a36da0b2f1d4d14b663c92d56ea35a2f11ce94
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-31 13:51:12 +0300

    Update Install-WinUtilChoco.ps1

--- functions/private/Install-WinUtilChoco.ps1
+++ functions/private/Install-WinUtilChoco.ps1
@@ -4,6 +4,18 @@ function Install-WinUtilChoco {
         return
     }
 
-    Write-Host "Chocolatey is not installed. Installing..."
-    Invoke-RestMethod -Uri https://community.chocolatey.org/install.ps1 | Invoke-Expression
+    Invoke-WebRequest -Uri https://community.chocolatey.org/api/v2/package/chocolatey -OutFile "chocolatey.nupkg"
+    Expand-Archive -Path "chocolatey.nupkg"
+
+    New-Item -Path $Env:ProgramData\chocolatey\lib\chocolatey -ItemType Directory  -Force
+
+    Move-Item -Path "chocolatey\tools\chocolateyInstall\*" -Destination $Env:ProgramData\chocolatey
+    Move-Item -Path "chocolatey.nupkg" -Destination $Env:ProgramData\chocolatey\lib\chocolatey\chocolatey.nupkg
+
+    $Path = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
+
+    [Environment]::SetEnvironmentVariable("PATH", $Path + ";$Env:ProgramData\chocolatey", "Machine")
+    $Env:Path = $Path
+
+    Remove-Item -Path "chocolatey" -Recurse
 }
