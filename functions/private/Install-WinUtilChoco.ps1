a8c6af052142162dfc7ec0dc5aa5a168add57d70
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-31 14:01:53 +0300

    Update Install-WinUtilChoco.ps1

--- functions/private/Install-WinUtilChoco.ps1
+++ functions/private/Install-WinUtilChoco.ps1
@@ -6,11 +6,12 @@ function Install-WinUtilChoco {
 
     Invoke-WebRequest -Uri https://community.chocolatey.org/api/v2/package/chocolatey -OutFile "chocolatey.nupkg"
     Expand-Archive -Path "chocolatey.nupkg"
+    Rename-Item -Path "chocolatey.nupkg" -NewName "chocolatey.zip"
 
     New-Item -Path $Env:ProgramData\chocolatey\lib\chocolatey -ItemType Directory  -Force
 
     Move-Item -Path "chocolatey\tools\chocolateyInstall\*" -Destination $Env:ProgramData\chocolatey
-    Move-Item -Path "chocolatey.nupkg" -Destination $Env:ProgramData\chocolatey\lib\chocolatey\chocolatey.nupkg
+    Move-Item -Path "chocolatey.zip" -Destination $Env:ProgramData\chocolatey\lib\chocolatey\chocolatey.nupkg
 
     $Path = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
 
