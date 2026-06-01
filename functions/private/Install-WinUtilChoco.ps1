118feaed37a53552ffb6bc827c3ed2c12c406513
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-01 14:21:49 +0300

    Update Install-WinUtilChoco.ps1

--- functions/private/Install-WinUtilChoco.ps1
+++ functions/private/Install-WinUtilChoco.ps1
@@ -1,20 +1,17 @@
 function Install-WinUtilChoco {
+    if (-not (Get-Command -Name choco)) {
+        Invoke-WebRequest -Uri https://community.chocolatey.org/api/v2/package/chocolatey -OutFile "chocolatey.zip"
+        Expand-Archive -Path "chocolatey.zip"
 
-    if (Get-Command -Name choco) {
-        return
+        New-Item -Path $Env:ProgramData\chocolatey\lib\chocolatey -ItemType Directory  -Force
+
+        Move-Item -Path "chocolatey\tools\chocolateyInstall\*" -Destination $Env:ProgramData\chocolatey
+        Move-Item -Path "chocolatey.zip" -Destination $Env:ProgramData\chocolatey\lib\chocolatey\chocolatey.nupkg
+
+        $Path = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
+        [Environment]::SetEnvironmentVariable("PATH", $Path + ";$Env:ProgramData\chocolatey", "Machine")
+        $Env:Path = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
+
+        Remove-Item -Path "chocolatey" -Recurse
     }
-
-    Invoke-WebRequest -Uri https://community.chocolatey.org/api/v2/package/chocolatey -OutFile "chocolatey.zip"
-    Expand-Archive -Path "chocolatey.zip"
-
-    New-Item -Path $Env:ProgramData\chocolatey\lib\chocolatey -ItemType Directory  -Force
-
-    Move-Item -Path "chocolatey\tools\chocolateyInstall\*" -Destination $Env:ProgramData\chocolatey
-    Move-Item -Path "chocolatey.zip" -Destination $Env:ProgramData\chocolatey\lib\chocolatey\chocolatey.nupkg
-
-    $Path = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
-    [Environment]::SetEnvironmentVariable("PATH", $Path + ";$Env:ProgramData\chocolatey", "Machine")
-    $Env:Path = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
-
-    Remove-Item -Path "chocolatey" -Recurse
 }
