9caa1b1722b205248d4bbf4875614d8ee3e14871
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-03 17:42:18 +0300

    Delete functions/private/Set-Preferences.ps1

--- functions/private/Set-Preferences.ps1
+++
@@ -1,7 +0,0 @@
-function Set-Preferences {
-    if ($sync.preferences.packagemanager -eq "Choco") {
-        $sync.preferences.packagemanager = [PackageManagers]::Choco
-    } elseif ($sync.preferences.packagemanager -eq "Winget") {
-        $sync.preferences.packagemanager = [PackageManagers]::Winget
-    }
-}
