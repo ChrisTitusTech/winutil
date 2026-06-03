a104c475f48132c1357f00bde57fc4b84a650f05
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-03 17:36:59 +0300

    Update Set-Preferences.ps1

--- functions/private/Set-Preferences.ps1
+++ functions/private/Set-Preferences.ps1
@@ -1,17 +1,7 @@
 function Set-Preferences {
-
-    if ($null -eq $sync.preferences.theme) {
-        $sync.preferences.theme = "Auto"
-    }
-
-    if ($null -eq $sync.preferences.packagemanager) {
-        $sync.preferences.packagemanager = "Winget"
-    }
-
     if ($sync.preferences.packagemanager -eq "Choco") {
         $sync.preferences.packagemanager = [PackageManagers]::Choco
-    }
-    elseif ($sync.preferences.packagemanager -eq "Winget") {
+    } elseif ($sync.preferences.packagemanager -eq "Winget") {
         $sync.preferences.packagemanager = [PackageManagers]::Winget
     }
 }
