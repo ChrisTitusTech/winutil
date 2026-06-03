55b8c860ea4cbf3f18a6b7cae0f61ef8f715b77e
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-03 14:58:45 +0300

    Update Invoke-WinUtilISO.ps1

--- functions/private/Invoke-WinUtilISO.ps1
+++ functions/private/Invoke-WinUtilISO.ps1
@@ -335,19 +335,12 @@ function Invoke-WinUtilISOExport {
     public class ISOFile {
         public unsafe static void Create(string path, object stream, int blockSize, int totalBlocks) {
             int bytes = 0;
+            byte[] buf = new byte[blockSize];
             var ptr = (System.IntPtr)(&bytes);
+            var o = System.IO.File.OpenWrite(path);
             var i = stream as System.Runtime.InteropServices.ComTypes.IStream;
-            using (var o = new System.IO.FileStream(path, System.IO.FileMode.Create, System.IO.FileAccess.Write, System.IO.FileShare.None, 8 * 1024 * 1024, true)) {
-                byte[] buf = new byte[8 * 1024 * 1024];
-                int blocksPerChunk = buf.Length / blockSize;
-                while (totalBlocks > 0) {
-                    int chunk = System.Math.Min(blocksPerChunk, totalBlocks);
-                    int toRead = chunk * blockSize;
-                    i.Read(buf, toRead, ptr);
-                    o.Write(buf, 0, bytes);
-                    totalBlocks -= chunk;
-                }
-            }
+            while (totalBlocks-- > 0) { i.Read(buf, blockSize, ptr); o.Write(buf, 0, bytes); }
+            o.Flush(); o.Close();
         }
     }
 "@
