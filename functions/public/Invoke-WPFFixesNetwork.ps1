9dc3f60891c4845296b6ca2eeb002d3b29a17d03
Vyas Devgna <141345367+vyas-devgna@users.noreply.github.com>
2026-06-08 18:04:30 +0530

    Update Invoke-WPFFixesNetwork.ps1

--- functions/public/Invoke-WPFFixesNetwork.ps1
+++ functions/public/Invoke-WPFFixesNetwork.ps1
@@ -1,7 +1,6 @@
 function Invoke-WPFFixesNetwork {
     netsh winsock reset
     netsh int ip reset
-    # Flush DNS cache
     ipconfig /flushdns
     Write-Host "Network Configuration has been Reset Please restart your computer."
 }
