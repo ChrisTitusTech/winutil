3bdd701220e1f6c92b2acace8a343f453d336a02
Vyas Devgna <141345367+vyas-devgna@users.noreply.github.com>
2026-06-08 07:43:38 +0530

    Update Invoke-WPFtweaksbutton.ps1

--- functions/public/Invoke-WPFtweaksbutton.ps1
+++ functions/public/Invoke-WPFtweaksbutton.ps1
@@ -82,6 +82,6 @@ function Invoke-WPFtweaksbutton {
     Write-Host "================================="
     Write-Host "--     Tweaks are Finished    ---"
     Write-Host "================================="
-    @($tweaks | Where-Object { $sync.configs.tweaks.$_.registry -or $sync.configs.tweaks.$_.service }) | ConvertTo-Json | Out-File "...lastrun.json" -Force
+    @($tweaks | Where-Object { $sync.configs.tweaks.$_.registry -or $sync.configs.tweaks.$_.service }) | ConvertTo-Json | Out-File "$env:LocalAppData\winutil\lastrun.json" -Force
   }
 }
