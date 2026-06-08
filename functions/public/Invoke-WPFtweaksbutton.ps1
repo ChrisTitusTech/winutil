cb385c9b6688407b6171c52711d0469da991d116
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-08 16:43:31 +0300

    Update Invoke-WPFtweaksbutton.ps1

--- functions/public/Invoke-WPFtweaksbutton.ps1
+++ functions/public/Invoke-WPFtweaksbutton.ps1
@@ -67,7 +67,9 @@ function Invoke-WPFtweaksbutton {
       }
     }
 
-    Set-WinUtilDNS -DNSProvider $dnsProvider
+    if ($DNSProvider -ne "Default") {
+      Set-WinUtilDNS -DNSProvider $dnsProvider
+    }
 
     for ($i = 0; $i -lt $tweaks.Count; $i++) {
       Set-WinUtilProgressBar -Label "Applying $($tweaks[$i])" -Percent ($completedSteps / $totalSteps * 100)
