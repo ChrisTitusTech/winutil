04aaca6de7023540e5e733f05e7e18c8516f4599
Dev <dev@localhost>
2026-06-08 17:59:44 +0530

    Add DNS flush to network reset

--- functions/public/Invoke-WPFFixesNetwork.ps1
+++ functions/public/Invoke-WPFFixesNetwork.ps1
@@ -1,5 +1,7 @@
 function Invoke-WPFFixesNetwork {
     netsh winsock reset
     netsh int ip reset
+    # Flush DNS cache
+    ipconfig /flushdns
     Write-Host "Network Configuration has been Reset Please restart your computer."
 }
