faf6d5db112f81d4548f855f9b9bf68650fc2a69
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-08 16:47:02 +0300

    Update Set-WinUtilDNS.ps1

--- functions/private/Set-WinUtilDNS.ps1
+++ functions/private/Set-WinUtilDNS.ps1
@@ -2,9 +2,11 @@ function Set-WinUtilDNS ($DNSProvider) {
     foreach ($Adapter in Get-NetAdapter | Where-Object Status -eq "Up") {
         if ($DNSProvider -eq "DHCP") {
             Set-DnsClientServerAddress -InterfaceIndex $Adapter.ifIndex -ResetServerAddresses
+            Write-Host "DNS set to default"
         } else {
             $dns = $sync.configs.dns.$DNSProvider
             Set-DnsClientServerAddress -InterfaceIndex $Adapter.ifIndex -ServerAddresses $dns.Primary, $dns.Secondary, $dns.Primary6, $dns.Secondary6
+            Write-Host "DNS set to $DNSProvider"
         }
     }
 }
