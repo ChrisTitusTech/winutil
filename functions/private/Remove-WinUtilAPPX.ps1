92e774c4deef1685dc40aa9ec24ddfe2a041378c
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-09 12:50:49 +0000

    Update Remove-WinUtilAPPX.ps1

--- functions/private/Remove-WinUtilAPPX.ps1
+++ functions/private/Remove-WinUtilAPPX.ps1
@@ -1,4 +1,4 @@
 function Remove-WinUtilAPPX ($Name) {
     Write-Host "Removing $Name"
-    Get-AppxPackage -Package $Name -AllUsers | Remove-AppxPackage -AllUsers
+    Get-AppxPackage -Name $Name -AllUsers | Remove-AppxPackage -AllUsers
 }
