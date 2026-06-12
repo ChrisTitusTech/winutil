011558153c0740dc59d5bae925234c46fb2b9919
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-12 13:43:32 +0300

    Update Invoke-WinUtilTweaks.ps1

--- functions/private/Invoke-WinUtilTweaks.ps1
+++ functions/private/Invoke-WinUtilTweaks.ps1
@@ -1,80 +1,29 @@
-function Invoke-WinUtilTweaks {
-    <#
+function Invoke-WinUtilTweaks ($CheckBox, $undo) {
+    $tweak = $sync.configs.tweaks.$CheckBox
 
-    .SYNOPSIS
-        Invokes the function associated with each provided checkbox
-
-    .PARAMETER CheckBox
-        The checkbox to invoke
-
-    .PARAMETER undo
-        Indicates whether to undo the operation contained in the checkbox
-
-    .PARAMETER KeepServiceStartup
-        Indicates whether to override the startup of a service with the one given from WinUtil,
-        or to keep the startup of said service, if it was changed by the user, or another program, from its default value.
-    #>
-
-    param(
-        $CheckBox,
-        $undo = $false,
-        $KeepServiceStartup = $true
-    )
-
-    if ($undo) {
-        $Values = @{
-            Registry = "OriginalValue"
-            Service = "OriginalType"
-            ScriptType = "UndoScript"
-        }
-
+    $keys = if ($undo) {
+        @{ Registry = "OriginalValue"; Service = "OriginalType"; ScriptType = "UndoScript" }
     } else {
-        $Values = @{
-            Registry = "Value"
-            Service = "StartupType"
-            OriginalService = "OriginalType"
-            ScriptType = "InvokeScript"
-        }
-    }
-    if ($sync.configs.tweaks.$CheckBox.service) {
-        $sync.configs.tweaks.$CheckBox.service | ForEach-Object {
-            $changeservice = $true
-
-        # The check for !($undo) is required, without it the script will throw an error for accessing unavailable member, which's the 'OriginalService' Property
-            if ($KeepServiceStartup -AND !($undo)) {
-                try {
-                    # Check if the service exists
-                    $service = Get-Service -Name $psitem.Name -ErrorAction Stop
-                    if(!($service.StartType.ToString() -eq $psitem.$($values.OriginalService))) {
-                        $changeservice = $false
-                    }
-                } catch [System.ServiceProcess.ServiceNotFoundException] {
-                    Write-Warning "Service $($psitem.Name) was not found."
-                }
-            }
-
-            if ($changeservice) {
-                Set-WinUtilService -Name $psitem.Name -StartupType $psitem.$($values.Service)
-            }
-        }
-    }
-    if ($sync.configs.tweaks.$CheckBox.registry) {
-        $sync.configs.tweaks.$CheckBox.registry | ForEach-Object {
-            Set-WinUtilRegistry -Name $psitem.Name -Path $psitem.Path -Type $psitem.Type -Value $psitem.$($values.registry)
-        }
-    }
-    if ($sync.configs.tweaks.$CheckBox.$($values.ScriptType)) {
-        $sync.configs.tweaks.$CheckBox.$($values.ScriptType) | ForEach-Object {
-            $Scriptblock = [scriptblock]::Create($psitem)
-            Invoke-WinUtilScript -ScriptBlock $scriptblock -Name $CheckBox
-        }
+        @{ Registry = "Value"; Service = "StartupType"; OriginalService = "OriginalType"; ScriptType = "InvokeScript" }
     }
 
-    if (!$undo) {
-        if($sync.configs.tweaks.$CheckBox.appx) {
-            $sync.configs.tweaks.$CheckBox.appx | ForEach-Object {
-                Remove-WinUtilAPPX -Name $psitem
-            }
+    foreach ($svc in $tweak.service) {
+        Write-Host "Setting Service $svc.Name to $svc.($keys.Service)"
+        Set-Service -Name $svc.Name -StartupType $svc.($keys.Service)
+    }
+
+    foreach ($reg in $tweak.registry) {
+        Set-WinUtilRegistry -Name $reg.Name -Path $reg.Path -Type $reg.Type -Value $reg.($keys.Registry)
+    }
+
+    foreach ($script in $tweak.($keys.ScriptType)) {
+        Invoke-WinUtilScript -ScriptBlock ([scriptblock]::Create($script)) -Name $CheckBox
+    }
+
+    if (-not $undo) {
+        foreach ($app in $tweak.appx) {
+            Write-Host "Removing $app"
+            Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
         }
     }
 }
