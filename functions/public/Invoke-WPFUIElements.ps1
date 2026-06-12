65336bc04616f3c78e8a47be317b7a51a4c74a44
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-12 15:28:53 +0300

    Update Invoke-WPFUIElements.ps1

--- functions/public/Invoke-WPFUIElements.ps1
+++ functions/public/Invoke-WPFUIElements.ps1
@@ -69,7 +69,7 @@ function Invoke-WPFUIElements {
         # Create an object for the application
         $entryObject = [PSCustomObject]@{
             Name        = $entry
-            if ($entryInfo.Category) { $entryInfo.Category } else { "Uncategorized" }
+            Category    = if ($entryInfo.Category) { $entryInfo.Category } else { "Uncategorized" }
             Content     = $entryInfo.Content
             Panel       = if ($entryInfo.Panel) { $entryInfo.Panel } else { "0" }
             Link        = $entryInfo.link
