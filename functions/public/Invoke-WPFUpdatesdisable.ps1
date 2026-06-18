9ae366b9638f4718639fc240c829c3c113c24298
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-18 12:30:16 +0300

    Update Invoke-WPFUpdatesdisable.ps1

--- functions/public/Invoke-WPFUpdatesdisable.ps1
+++ functions/public/Invoke-WPFUpdatesdisable.ps1
@@ -1,53 +1,19 @@
 function Invoke-WPFUpdatesdisable {
-    <#
-
-    .SYNOPSIS
-        Disables Windows Update
-
-    .NOTES
-        Disabling Windows Update is not recommended. This is only for advanced users who know what they are doing.
-
-    #>
-    $ErrorActionPreference = 'SilentlyContinue'
-
-    Write-Host "Configuring registry settings..." -ForegroundColor Yellow
-    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Force
-
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "NoAutoUpdate" -Type DWord -Value 1
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUOptions" -Type DWord -Value 1
-
-    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" -Force
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" -Name "DODownloadMode" -Type DWord -Value 0
-
-    Write-Host "Hiding Windows Updates from settings"
     Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer -Name SettingsPageVisibility -Value hide:windowsupdate
+    Write-Host "Windows Update will not be hidden from settings"
 
-    Write-Host "Disabled BITS Service"
-    Set-Service -Name BITS -StartupType Disabled
+    takeown /f $Env:SystemRoot\System32\usosvc.dll | Out-Null
+    icacls $Env:SystemRoot\System32\usosvc.dll /grant Everyone:F | Out-Null
+    Rename-Item -Path $Env:SystemRoot\System32\usosvc.dll -NewName usosvc.dlle
 
-    Write-Host "Disabled wuauserv Service"
-    Set-Service -Name wuauserv -StartupType Disabled
+    takeown /f $Env:SystemRoot\System32\wuaueng.dll | Out-Null
+    icacls $Env:SystemRoot\System32\wuaueng.dll /grant Everyone:F | Out-Null
+    Rename-Item -Path $Env:SystemRoot\System32\wuaueng.dll -NewName wuaueng.dlle
 
-    Write-Host "Disabled UsoSvc Service"
-    Stop-Service -Name UsoSvc -Force
-    Set-Service -Name UsoSvc -StartupType Disabled
+    Write-Host "Windows Update dlls renamed."
 
     Remove-Item "C:\Windows\SoftwareDistribution\*" -Recurse -Force
     Write-Host "Cleared SoftwareDistribution folder"
-
-    Write-Host "Disabling update related scheduled tasks..." -ForegroundColor Yellow
-
-    $Tasks =
-        '\Microsoft\Windows\InstallService\*',
-        '\Microsoft\Windows\UpdateOrchestrator\*',
-        '\Microsoft\Windows\UpdateAssistant\*',
-        '\Microsoft\Windows\WaaSMedic\*',
-        '\Microsoft\Windows\WindowsUpdate\*',
-        '\Microsoft\WindowsUpdate\*'
-
-    foreach ($Task in $Tasks) {
-        Get-ScheduledTask -TaskPath $Task | Disable-ScheduledTask -ErrorAction SilentlyContinue
-    }
 
     Write-Host "=================================" -ForegroundColor Green
     Write-Host "---   Updates Are Disabled    ---" -ForegroundColor Green
