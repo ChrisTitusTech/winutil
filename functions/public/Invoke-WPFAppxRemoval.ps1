b81b208a9fcc5b2667c66c94c9e9da6043587f5b
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-21 09:27:11 -0700

    Update Invoke-WPFAppxRemoval.ps1

--- functions/public/Invoke-WPFAppxRemoval.ps1
+++ functions/public/Invoke-WPFAppxRemoval.ps1
@@ -1,14 +1,30 @@
 function Invoke-WPFAppxRemoval {
+    if ($sync.ProcessRunning) {
+        [System.Windows.MessageBox]::Show("A process is currently running.", "Winutil", "OK", "Warning")
+        return
+    }
+
+    $selected = @($sync.selectedAppx)
+    if (-not $selected) {
+        [System.Windows.MessageBox]::Show("Select AppX packages to remove.", "Winutil", "OK", "Warning")
+        return
+    }
+
     $apps = $sync.configs.appxHashtable
 
     Invoke-WPFRunspace -ParameterList @(("selected", $selected),("apps", $apps)) -ScriptBlock {
         param($selected, $apps)
 
+        $sync.ProcessRunning = $true
+
         foreach ($key in $selected) {
             $package = $apps[$key].PackageId
+
             Write-Host "Removing $package"
             Get-AppxPackage -Name $package -AllUsers | Remove-AppxPackage -AllUsers
         }
+
+        $sync.ProcessRunning = $false
 
         Write-Host "================================="
         Write-Host "--   AppX Removal Finished   ---"
