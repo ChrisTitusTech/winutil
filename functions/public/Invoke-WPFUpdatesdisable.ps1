f9869171fd115bd051150f913e471799371b8868
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-22 11:21:28 -0700

    Update Invoke-WPFUpdatesdisable.ps1

--- functions/public/Invoke-WPFUpdatesdisable.ps1
+++ functions/public/Invoke-WPFUpdatesdisable.ps1
@@ -1,12 +1,15 @@
 function Invoke-WPFUpdatesdisable {
     Write-Host "Note: This will prevent you from using the Microsoft Store." -ForegroundColor Yellow
 
+    Write-Host "Blocking execution of windows update dlls"
+
     takeown /f $Env:SystemRoot\System32\usosvc.dll
     icacls $Env:SystemRoot\System32\usosvc.dll /deny Everyone:F
 
     takeown /f $Env:SystemRoot\System32\wuaueng.dll
     icacls $Env:SystemRoot\System32\wuaueng.dll /deny Everyone:F
 
+    Write-Host "Clearing SoftwareDistribution folder"
     Remove-Item -Path $Env:SystemRootSoftwareDistribution\* -Recurse -Force
 
     Write-Host "=================================" -ForegroundColor Green
