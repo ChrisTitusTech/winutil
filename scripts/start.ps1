ac8b5aa1999836f588b26cf2331ce2a3d6235b6b
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-23 23:44:55 -0700

    Update start.ps1

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -81,10 +81,6 @@ $dateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
 $winutildir = "$env:LocalAppData\winutil"
 New-Item $winutildir -ItemType Directory -Force | Out-Null
 
-$logdir = "$winutildir\logs"
-New-Item $logdir -ItemType Directory -Force | Out-Null
-Start-Transcript -Path "$logdir\winutil_$dateTime.log" -Append -NoClobber | Out-Null
-
 # Set PowerShell window title
 $Host.UI.RawUI.WindowTitle = "WinUtil (Admin)"
 clear-host
