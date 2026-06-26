28951962b5142340cd0db3367c1d9b6b02c232fa
LKmXr <113763590+LKmXr@users.noreply.github.com>
2026-06-26 20:09:30 +0700

    Add status messages to Invoke-WPFOOSU.ps1

--- functions/public/Invoke-WPFOOSU.ps1
+++ functions/public/Invoke-WPFOOSU.ps1
@@ -1,11 +1,14 @@
 function Invoke-WPFOOSU {
     try {
-        $ProgressPreference = 'SilentlyContinue'
-
+        # $ProgressPreference = 'SilentlyContinue'
+        # SlientlyContinue may make users think the script is frozen
+        Write-Host "Downloading O&O ShutUp10..."
         Invoke-WebRequest -Uri https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe -OutFile "$winutildir\ooshutup10.exe"
+        Write-Host "Starting O&O ShutUp10..."
         Start-Process -FilePath "$winutildir\ooshutup10.exe"
 
-        $ProgressPreference = 'Continue'
+        # $ProgressPreference = 'Continue'
+        # no need to reset this var
     } catch {
         Write-Error "Couldn't download O&O ShutUp10. Please make sure you have an active Internet connection."
     }
