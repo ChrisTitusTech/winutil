0d2052df956e9fa68edc3e570f998b009269ced8
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-06-30 08:11:13 +0300

    Update Invoke-WPFUpdatesdisable.ps1

--- functions/public/Invoke-WPFUpdatesdisable.ps1
+++ functions/public/Invoke-WPFUpdatesdisable.ps1
@@ -1,6 +1,4 @@
 function Invoke-WPFUpdatesdisable {
-    Write-Host "Note: This will prevent you from using the Microsoft Store." -ForegroundColor Yellow
-
     Write-Host "Disabling windows update services"
 
     Set-Service -Name UsoSvc -StartupType Disabled
@@ -12,16 +10,10 @@ function Invoke-WPFUpdatesdisable {
     icacls $Env:SystemRoot\System32\usosvc.dll /grant Everyone:F
     Rename-Item -Path $Env:SystemRoot\System32\usosvc.dll -NewName usosvc.dlle
 
-    takeown /f $Env:SystemRoot\System32\wuaueng.dll
-    icacls $Env:SystemRoot\System32\wuaueng.dll /grant Everyone:F
-    Rename-Item -Path $Env:SystemRoot\System32\wuaueng.dll -NewName wuaueng.dlle
-
     Write-Host "Clearing SoftwareDistribution folder"
     Remove-Item -Path $Env:SystemRoot\SoftwareDistribution\* -Recurse -Force
 
     Write-Host "=================================" -ForegroundColor Green
     Write-Host "---   Updates Are Disabled    ---" -ForegroundColor Green
     Write-Host "=================================" -ForegroundColor Green
-
-    Write-Host "Note: You must restart your system in order for all changes to take effect." -ForegroundColor Yellow
 }
