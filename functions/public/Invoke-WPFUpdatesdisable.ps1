f86fcb92510aea0168a987ce9a798096b9893cf0
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-07-03 07:49:20 +0300

    Update Invoke-WPFUpdatesdisable.ps1

--- functions/public/Invoke-WPFUpdatesdisable.ps1
+++ functions/public/Invoke-WPFUpdatesdisable.ps1
@@ -1,13 +1,4 @@
 function Invoke-WPFUpdatesdisable {
-    Write-WinUtilLog -Component "Updates" -Message "Configuring Windows Update registry policy values for disable mode."
-    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Force
-
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "NoAutoUpdate" -Type DWord -Value 1
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUOptions" -Type DWord -Value 1
-
-    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" -Force
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" -Name "DODownloadMode" -Type DWord -Value 0
-
     Write-WinUtilLog -Component "Updates" -Message "Disabling wuauserv service."
     Set-Service -Name wuauserv -StartupType Disabled
 
