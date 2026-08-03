40a2f49cbf770a3772cbd7d570696fff38b95c61
Omar <90123670+mewclouds@users.noreply.github.com>
2026-08-03 16:20:38 -0400

    Support restricted PowerShell in windev

--- windev.ps1
+++ windev.ps1
@@ -4,5 +4,7 @@ $latestTag = (Invoke-RestMethod https://api.github.com/repos/ChrisTitusTech/winu
 $uri = "https://github.com/ChrisTitusTech/winutil/releases/download/$latestTag/winutil.ps1"
 $scriptPath = Join-Path $env:TEMP "winutil-$latestTag.ps1"
 
-Invoke-WebRequest -Uri $uri -OutFile $scriptPath -ErrorAction Stop
-& $scriptPath
+Invoke-WebRequest -Uri $uri -OutFile $scriptPath -UseBasicParsing -ErrorAction Stop
+
+$powerShellCmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
+& $powerShellCmd -ExecutionPolicy Bypass -NoProfile -File $scriptPath
